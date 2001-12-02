2b6ad59234877f59d6f3417e8c9137a291e52372
Erik Ouchterlony <ejlo@users.sf.net>
2001-12-02 13:19:15 +0000

    Bugfix: delete all query

--- messages.php
+++ messages.php
@@ -38,7 +38,7 @@ require( "include/std_functions.php" );
       if( $del == 'all' )
       {
          $result = mysql_query("UPDATE Messages SET Flags=CONCAT_WS(',',Flags,'DELETED') " .
-                               "WHERE Flags NOT ( Flags LIKE '%NEW%' OR " .
+                               "WHERE NOT ( Flags LIKE '%NEW%' OR " .
                                "Flags LIKE '%REPLY REQUIRED%' )");
       }
       else
