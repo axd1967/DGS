195122ba7ef7f69dd2da06462103d023fcd7bf8d
Erik Ouchterlony <ejlo@users.sf.net>
2002-01-26 21:01:04 +0000

    Updated copyright years

--- edit_profile.php
+++ edit_profile.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Dragon Go Server
-Copyright (C) 2001  Erik Ouchterlony
+Copyright (C) 2001-2002  Erik Ouchterlony
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
