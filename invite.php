d153d01fa9cabb142a9b4a8e9bdf055ba3e2c862
Ragnar Ouchterlony <ragnar@users.sf.net>
2002-03-08 16:22:33 +0000

    * Changed to use form_functions.php * Radiobuttons is now situated to the right of the description (for byoyomi   that is).

--- invite.php
+++ invite.php
@@ -19,6 +19,7 @@ Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
 require( "include/std_functions.php" );
+include( "include/form_functions.php" );
 
 {
    connect2mysql();
@@ -37,7 +38,7 @@ require( "include/std_functions.php" );
    if( $uid and $uid != $player_row["ID"] )
    {
       $result = mysql_query( "SELECT Handle FROM Players WHERE ID=$uid" );
-      
+
       if( mysql_num_rows( $result ) == 1 )
       {
          extract(mysql_fetch_array($result));
@@ -46,188 +47,74 @@ require( "include/std_functions.php" );
 
 
    start_page("Send Invitation", true, $logged_in, $player_row );
+
+   echo "<center>\n";
+   echo "<B><font size=+1>Invitation message:</font></B>\n<HR>\n";
+   echo form_start( 'inviteform', 'send_message.php', 'POST' );
+
+   echo form_insert_row( 'DESCRIPTION', 'To (userid)',
+                         'TEXTINPUT', 'to', 50, 80, $Handle );
+
+   echo form_insert_row( 'DESCRIPTION', 'Message',
+                         'TEXTAREA', 'message', 50, 8, "" );
+
+   $value_array=array();
+   for( $bs = 5; $bs <= 25; $bs++ )
+     $value_array[$bs]=$bs;
+
+   echo form_insert_row( 'DESCRIPTION', 'Board size',
+                         'SELECTBOX', 'size', 1, $value_array, 19, false );
+
+   $value_array=array( 'White' => 'White', 'Black' => 'Black' );
+   echo form_insert_row( 'DESCRIPTION', 'My color',
+                         'SELECTBOX', 'color', 1, $value_array, 'White', false );
+
+   $value_array=array( 0 => 0 );
+   for( $bs = 2; $bs <= 20; $bs++ )
+     $value_array[$bs]=$bs;
+
+   echo form_insert_row( 'DESCRIPTION', 'Handicap',
+                         'SELECTBOX', 'handicap', 1, $value_array, 0, false );
+
+   echo form_insert_row( 'DESCRIPTION', 'Komi',
+                         'TEXTINPUT', 'komi', 5, 5, '6.5' );
+
+   $value_array=array( 'hours' => 'hours', 'days' => 'days', 'months' => 'months' );
+   echo form_insert_row( 'DESCRIPTION', 'Main time',
+                         'TEXTINPUT', 'timevalue', 5, 5, 3,
+                         'SELECTBOX', 'timeunit', 1, $value_array, 'months', false );
+
+   echo form_insert_row( 'DESCRIPTION', 'Japanese byo-yomi',
+                         'RADIOBUTTONS', 'byoyomitype', array( 'JAP' => '' ), 'JAP',
+                         'TEXTINPUT', 'byotimevalue_jap', 5, 5, 1,
+                         'SELECTBOX', 'timeunit_jap', 1, $value_array, 'days', false,
+                         'TEXT', 'with&nbsp;',
+                         'TEXTINPUT', 'byoperiods_jap', 5, 5, 10,
+                         'TEXT', 'extra periods.' );
+
+   echo form_insert_row( 'DESCRIPTION', 'Canadian byo-yomi',
+                         'RADIOBUTTONS', 'byoyomitype', array( 'CAN' => '' ), 'CAN',
+                         'TEXTINPUT', 'byotimevalue_can', 5, 5, 15,
+                         'SELECTBOX', 'timeunit_can', 1, $value_array, 'days', false,
+                         'TEXT', 'for&nbsp;',
+                         'TEXTINPUT', 'byoperiods_can', 5, 5, 15,
+                         'TEXT', 'stones.' );
+
+   echo form_insert_row( 'DESCRIPTION', 'Fischer time',
+                         'RADIOBUTTONS', 'byoyomitype', array( 'FIS' => '' ), 'FIS',
+                         'TEXTINPUT', 'byotimevalue_fis', 5, 5, 1,
+                         'SELECTBOX', 'timeunit_fis', 1, $value_array, 'days', false,
+                         'TEXT', 'extra&nbsp;per move.' );
+
+   echo form_insert_row( 'DESCRIPTION', 'Clock runs on weekends',
+                         'CHECKBOX', 'weekendclock', 'Y', "", true );
+   echo form_insert_row( 'DESCRIPTION', 'Rated',
+                         'CHECKBOX', 'rated', 'Y', "", true );
+   echo form_insert_row( 'HIDDEN', 'type', 'INVITATION' );
+   echo form_insert_row( 'SUBMITBUTTON', 'send', 'Send Invitation' );
+
+   echo "</center>\n";
+
+   end_page();
 }
-?>
-
-<FORM name="loginform" action="send_message.php" method="POST">
-      
-      
-      <center><B><font size=+1>New Message:</font></B></center>
-      <HR>
-      <TABLE align="center">
-        
-          <TR>
-            <TD align=right>To (userid):</TD>
-            <TD align=left> <input type="text" name="to" size="50" value="<?php echo $Handle ?>" maxlength="80"></TD>
-          </TR>
-
-          <TR>
-            <TD align=right>Message:</TD>
-            <TD align=left>  
-              <textarea name="message" cols="50" rows="8" wrap="virtual"></textarea></TD>
-          </TR>
-
-
-          <TR>
-            <TD align=right>Board size:</TD>
-            <TD align=left>  
-          <select name="size">
-            <option>5</option>
-            <option>6</option>
-            <option>7</option>
-            <option>8</option>
-            <option>9</option>
-            <option>10</option>
-            <option>11</option>
-            <option>12</option>
-            <option>13</option>
-            <option>14</option>
-            <option>15</option>
-            <option>16</option>
-            <option>17</option>
-            <option>18</option>
-            <option selected>19</option>
-            <option>20</option>
-            <option>21</option>
-            <option>22</option>
-            <option>23</option>
-            <option>24</option>
-            <option>25</option>
-          </select>
-      </TR>
-      
-      <TR>
-        <TD align=right>My color:</TD>
-        <TD align=left>  
-          <select name="color">
-            <option selected>White</option>
-            <option>Black</option>
-          </select>
-      </TR>
-
-      <TR>
-        <TD align=right>Handicap:</TD>
-        <TD align=left>  
-          <select name="handicap">
-            <option selected>0</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>6</option>
-            <option>7</option>
-            <option>8</option>
-            <option>9</option>
-            <option>10</option>
-            <option>11</option>
-            <option>12</option>
-            <option>13</option>
-            <option>14</option>
-            <option>15</option>
-            <option>16</option>
-            <option>17</option>
-            <option>18</option>
-            <option>19</option>
-            <option>20</option>
-          </select>
-      </TR>
- 
-      <TR>
-        <TD align=right>Komi:</TD>
-        <TD align=left>  
-          <input type="text" value="6.5" name="komi" size="5" maxlength="5">
-      </TR>
-
-      <TR>
-        <TD align=right>Main time:</TD>
-        <TD align=left>  
-          <input type="text" value="3" name="timevalue" size="5" maxlength="5">
-              <select name="timeunit">
-              <option>hours</option>
-              <option>days</option>
-              <option selected>months</option>
-          </select>
-        </TD>
-      </TR>
-
-
-      <TR>
-        <TD align=right>
-          <input type="radio" name="byoyomitype" value="JAP" checked>
-            Japanese byo-yomi:</TD>
-        <TD align=left>  
-
-            <input type="text" value="1" name="byotimevalue_jap" size="5" maxlength="5">
-              <select name="timeunit_jap">
-                <option>hours</option>
-                <option selected>days</option>
-                <option>months</option>
-              </select>
-
-            with &nbsp;
-            <input type="text" value="10" name="byoperiods_jap" size="5" maxlength="5">
-              extra periods.
-        </TD>
-      </TR>
-
-      <TR>
-        <TD align=right>
-          <input type="radio" name="byoyomitype" value="CAN">
-            Canadian byo-yomi:</TD>
-        <TD align=left>  
-
-            <input type="text" value="15" name="byotimevalue_can" size="5" maxlength="5">
-              <select name="timeunit_can">
-                <option>hours</option>
-                <option selected>days</option>
-                <option>months</option>
-              </select>
-
-            for&nbsp;
-            <input type="text" value="15" name="byostones_can" size="5" maxlength="5">
-              stones.
-        </TD>
-      </TR>
-
-      <TR>
-        <TD align=right>
-          <input type="radio" name="byoyomitype" value="FIS">
-            Fischer time:</TD>
-        <TD align=left>  
-
-            <input type="text" value="1" name="byotimevalue_fis" size="5" maxlength="5">
-              <select name="timeunit_fis">
-                <option>hours</option>
-                <option selected>days</option>
-                <option>months</option>
-              </select>
-
-            extra&nbsp;per move.
-        </TD>
-      </TR>
-
-      <TR>
-        <TD align=right>Clock runs on weekends:</TD>
-        <TD align=left>  <input type="checkbox" name="weekendclock" value="Y" checked></TD>
-
-      </TR>
-
-      <TR>
-        <TD align=right>Rated:</TD>
-        <TD align=left>  <input type="checkbox" name="rated" value="Y" checked></TD>
-
-      </TR>
-
-      <TR>
-        <TD></TD>
-        <TD>
-            <input type=hidden name="type" value="INVITATION">
-            <input type=submit name="send" value="Send invitation"></TD>
-      </TR>
-
-    </TABLE>
-</FORM>
-
-<?php
-end_page();
 ?>
