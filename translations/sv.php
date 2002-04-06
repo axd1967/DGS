da9afb0c56372058b3f9d6be9531165d9fb78965
Ragnar Ouchterlony <ragnar@users.sf.net>
2002-04-06 11:31:02 +0000

    * Added some translations I used for test purposes.

--- translations/sv.php
+++ translations/sv.php
@@ -54,12 +54,49 @@ class sv_Language extends Language
                "Forgot password?" =>
                "Glömt lösenordet?",
 
+               /* From edit_profile.php */
+
+               "Edit profile" =>
+               "Ändra din profil",
+
+               "Personal settings" =>
+               "Personliga inställningar",
+
+               "Board graphics" =>
+               "Brädgrafik",
+
+               /* From error.php */
+
+               "Sorry, you have to be logged in to do that.\n" .
+               "<p>\n" .
+               "The reasons for this problem could be any of the following:\n" .
+               "<ul>\n" .
+               "<li> You haven't got an <a href=\"%1\$s/register.php\">account</a>, or haven't <a href=\"%1\$s/index.php\">logged in</a> yet.\n" .
+               "<li> Your cookies have expired. This happens once a week.\n" .
+               "<li> You haven't enabled cookies in your browser.\n" .
+               "</ul>\n" =>
+
+               "Tyvärr, men du måste vara inloggad för att göra detta.\n" .
+               "<p>\n" .
+               "Anledningen till detta problem kan vara ett av följande:\n" .
+               "<ul>\n" .
+               "<li> Du har inget <a href=\"%1\$s/register.php\">konto</a>, eller så har du inte <a href=\"%1\$s/index.php\">loggat in</a> än.\n" .
+               "<li> Dina kakor har blivit ogiltiga. Detta händer en gång i veckan.\n" .
+               "<li> Du har inte slagit på kakor i din webbläsare.\n" .
+               "</ul>\n",
+
                /* Other things */
                "Userid" =>
                "Användaridentitet",
 
                "Password" =>
                "Lösenord",
+
+               "Full name" =>
+               "Hela namnet",
+
+               "Email" =>
+               "Epost",
 
                "Yes" =>
                "Ja",
