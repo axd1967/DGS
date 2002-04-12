0db6a9cf35f5a2bdba683c7547835e2b1a1f73d9
Ragnar Ouchterlony <ragnar@users.sf.net>
2002-04-12 13:15:57 +0000

    * Improved linking. * Made translatable

--- docs.php
+++ docs.php
@@ -18,42 +18,38 @@ along with this program; if not, write to the Free Software Foundation,
 Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-require( "include/std_functions.php" );
+ require( "include/std_functions.php" );
 
-connect2mysql();
+{
+  connect2mysql();
 
-$logged_in = is_logged_in($handle, $sessioncode, $player_row);
+  $logged_in = is_logged_in($handle, $sessioncode, $player_row);
 
-start_page("Links", true, $logged_in, $player_row );
+  start_page(T_("Docs"), true, $logged_in, $player_row );
 
-?>
+  echo "<table align=center><tr><td>\n";
+  echo "<center><h3><font color=$h3_color>" .
+    T_('Documentaion') . "</font></h3></center>\n";
 
-<table align=center><tr><td>
+  add_link_page_link('introduction.php', T_('Introduction to Dragon'));
+  add_link_page_link('site_map.php', T_('Site map'));
+  add_link_page_link("phorum/list.php?f=3",
+                     T_('Frequently Asked Questions'), T_('with answers'));
+  add_link_page_link("links.php", T_('Links'));
+  add_link_page_link("todo.php", T_('To do list'), T_('plans for future improvements'));
+  add_link_page_link("install.php", T_('Installation instructions'),
+                     T_('if you want your own dragon'));
+  add_link_page_link("snapshot", T_('Download dragon sources'),
+                     T_('daily snapshot of the cvs'));
+  add_link_page_link("http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/dragongoserver/DragonGoServer/",
+                     T_('Browse Dragon source code'));
+  add_link_page_link("http://sourceforge.net/projects/dragongoserver",
+                     T_('Dragon project page at sourceforge'));
+  add_link_page_link("licence.php", T_('Licence'), 'GPL');
 
-<center><h3><font color=<?php echo $h3_color; ?>>Documentaion</font></h3></center>
+  echo "<br>&nbsp;\n</td></tr></table>\n";
 
+  end_page();
+}
 
-<p><a href="introduction.php">Introduction to Dragon</a>
-
-<p><a href="site_map.php">Site map</a>
-
-<p><a href="phorum/list.php?f=3">Frequently Asked Questions</a> -- with answers
-
-<p><a href="links.php">Links</a>
-
-<p><a href="todo.php">To do list</a> --- plans for future improvements
-
-<p><a href="install.php">Installation instructions</a> --- if you want your own dragon
-
-<p><a href="snapshot">Download dragon sources</a> --- daily shapshot of the cvs
-
-<p><a href="http://sourceforge.net/projects/dragongoserver">Dragon project page at sourceforge</a>
-
-<p><a href="licence.php">Licence</a> --- GPL
-
-<br>&nbsp;
-</td></tr></table>
-
-<?php
-end_page();
 ?>
--- links.php
+++ links.php
@@ -20,111 +20,115 @@ Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 require( "include/std_functions.php" );
 
-connect2mysql();
+{
+  connect2mysql();
 
-$logged_in = is_logged_in($handle, $sessioncode, $player_row);
+  $logged_in = is_logged_in($handle, $sessioncode, $player_row);
 
-start_page("Links", true, $logged_in, $player_row );
+  start_page(T_("Links"), true, $logged_in, $player_row );
 
-?>
+  echo "<table align=center width=\"85%\"><tr><td>\n";
+  echo "<h3 align=left><a name=\"general\"></a><font color=$h3_color>" .
+    T_('General Info') . "</font></h3>\n";
 
-
-<table align=center width="85%"><tr><td>
-
-<h3 align=left><a name="general"></a><font color=<?php echo $h3_color; ?>>General Info</font> </h3>
-
-
-
-<p><a href="http://gobase.org">Jan van der Steens Pages</a> --- Lots of info on go
-
-<p><a href="http://www.igoweb.org/~pahle/go-faq/index.html">Go FAQ</a> --- Frequently asked questions about go for the rec.games.go newsgroup
-
-<p><a href="http://www.msoworld.com/mindzine/news/orient/go/go.html">Mind Sport Zine</a> --- An excellent, but unfortunately no longer updated site.
-
-<p><a href="http://home.san.rr.com/rafgo/go.html">Rafael's Go Page</a> --- Another go site with lots of useful info.
-
-<p><a href="http://senseis.xmp.net">Sensei's Library</a> --- A collaboration web site. Read and contribute!
-
-<p><a href="http://www.kyoto.zaq.ne.jp/momoyama/news/news.html">Go News</a> --- News and games from the professional scene
-
-<p><a href="http://finance.baylor.edu/rich/go/goguild.html">Turn-based go guild</a> --- Meet other turn-based go players
-
-<p><a href="http://www.toriyamaworld.com/hikago">Hikaru no Go</a> --- A manga about go. Recommended!
-
-
-
-<p>&nbsp;
-
-<h3 align=left><a name="rules"></a><font color=<?php echo $h3_color; ?>>Rules</font></h3>
-
-<p><a href="http://playgo.to/interactive/index.html">An Interactive
-    Introduction</a> --- This is a very nice site to learn with.
-
-<p><a href="http://www.britgo.org/intro/intro1.html">Introduction</a> --- Very well written introduction by the British Go Association.
+  add_link_page_link("http://gobase.org",
+                     T_('Jan van der Steens Pages'), T_('Lots of info on go'));
+  add_link_page_link("http://www.igoweb.org/~pahle/go-faq/index.html",
+                     T_('Go FAQ'),
+                     T_('Frequently asked questions about go for the rec.games.go newsgroup'));
+  add_link_page_link("http://www.msoworld.com/mindzine/news/orient/go/go.html",
+                     T_('Mind Sport Zine'),
+                     T_('An excellent, but unfortunately no longer updated site.'));
+  add_link_page_link("http://home.san.rr.com/rafgo/go.html",
+                     T_('Rafael\'s Go Page'),
+                     T_('Another go site with lots of useful info.'));
+  add_link_page_link("http://senseis.xmp.net",
+                     T_('Sensei\'s Library'),
+                     T_('A collaboration web site. Read and contribute!'));
+  add_link_page_link("http://www.kyoto.zaq.ne.jp/momoyama/news/news.html",
+                     T_('Go News'), T_('News and games from the professional scene'));
+  add_link_page_link("http://finance.baylor.edu/rich/go/goguild.html",
+                     T_('Turn-based go guild'), T_('Meet other turn-based go players'));
+  add_link_page_link("http://www.toriyamaworld.com/hikago",
+                     T_('Hikaru no Go'), T_('A manga about go. Recommended!'));
 
 
+  echo "<p>&nbsp;\n";
+  echo "<h3 align=left><a name=\"rules\"></a><font color=$h3_color>" . 
+    T_('Rules') . "</font></h3>\n";
 
-<p><a href="http://sentex.net/~mmcadams/teachgo/index.html">How to
-    Teach Go</a> --- This is all you need to get started.&nbsp; Very basic stuff
+  add_link_page_link("http://playgo.to/interactive/index.html",
+                     T_('An Interactive Introduction'),
+                     T_('This is a very nice site to learn with.'));
+  add_link_page_link("http://www.britgo.org/intro/intro1.html",
+                     T_('Introduction'),
+                     T_('Very well written introduction by the British Go Association.'));
+  add_link_page_link("http://sentex.net/~mmcadams/teachgo/index.html",
+                     T_('How to Teach Go'),
+                     T_('This is all you need to get started. Very basic stuff'));
+  add_link_page_link("http://home.earthlink.net/~scotmc/",
+                     T_('Scot\'s Go Page'),
+                     T_('This is more in-depth.'));
 
-<p><a href="http://home.earthlink.net/~scotmc/">Scot's Go Page</a>
-  --- This is more in-depth.
+  echo "<p>&nbsp;\n";
+  echo "<h3 align=left><a name=\"strategy\"></a><font color=$h3_color>" . 
+    T_('Strategy and terms') . "</font></h3>\n";
 
-<p>&nbsp;
-
-
-
-<h3 align=left><font color=<?php echo $h3_color; ?>><a name="strategy"></a>Strategy &amp; Terms</font>&nbsp;</h3>
-
-<p><a href="http://www.igoweb.org/~pahle/go-stuff/shape.html">An
-    Introduction to Shape</a>
-
-
-<p><a href="http://www.goproblems.com/">Go Problems</a> --- Working
-  through these can help out your game.
-
-<p><a href="http://gtl.jeudego.org/">Go Teaching Ladder</a> --
-  Submit your games for comments to see where you might have played better.
-
-<p><a href="http://nngs.cosmic.org/hmkw/stuff/definitions.html">Common Japanese Go Terms</a>
-  --- You have to know what other players are talking about.
-
-<p><a href="http://www.algonet.se/~palund/glossary/term_000.htm">More Japanese Go Terms</a> --- Translated and explained.
-
-<p>&nbsp;
-
-
-
-<h3 align=left><font color=<?php echo $h3_color; ?>><a name="history"></a>History</font></h3>
-
-<p><a href="http://www.britgo.org/intro/intro1.html#bh">A Brief
-    History</a> --- For you people with short attention spans.
-<p><a href="http://www.cwi.nl/~jansteen/go/history/">The Extended
-    History</a> --- In case you're an aspiring know-it-all.
-
-<p>&nbsp;
+  add_link_page_link("http://www.igoweb.org/~pahle/go-stuff/shape.html",
+                     T_('An Introduction to Shape'));
+  add_link_page_link("http://www.goproblems.com/",
+                     T_('Go Problems'), T_('Working through these can help out your game.'));
+  add_link_page_link("http://gtl.jeudego.org/",
+                     T_('Go Teaching Ladder'),
+                     T_('Submit your games for comments to see where you might have played better.'));
+  add_link_page_link("http://nngs.cosmic.org/hmkw/stuff/definitions.html",
+                     T_('Common Japanese Go Terms'),
+                     T_('You have to know what other players are talking about.'));
+  add_link_page_link("http://www.algonet.se/~palund/glossary/term_000.htm",
+                     T_('More Japanese Go Terms'),
+                     T_('Translated and explained.'));
 
 
+  echo "<p>&nbsp;\n";
+  echo "<h3 align=left><a name=\"history\"></a><font color=$h3_color>" . 
+    T_('History') . "</font></h3>\n";
 
-<h3 align=left><font color=<?php echo $h3_color; ?>><a name="stuff"></a>Go Books, Equipment &amp; Software</font></h3>
-<p><a href="http://www.kiseido.com/">Kiseido</a>
-<p><a href="http://www.yutopian.com/go/">Yutopian</a>
-<p><a href="http://www.samarkand.net/">Samarkand</a>
-<p><a href="http://www.xs4all.nl/~paard//">Het Paard</a> --- European shop
+  add_link_page_link("http://www.britgo.org/intro/intro1.html#bh",
+                     T_('A Brief History'),
+                     T_('For you people with short attention spans.'));
+  add_link_page_link("http://www.cwi.nl/~jansteen/go/history/",
+                     T_('The Extended History'),
+                     T_('In case you\'re an aspiring know-it-all.'));
 
-<p>&nbsp;
+  echo "<p>&nbsp;\n";
+  echo "<h3 align=left><a name=\"stuff\"></a><font color=$h3_color>" . 
+    T_('Go books, equipment and software') . "</font></h3>\n";
 
+  add_link_page_link("http://www.kiseido.com/", T_('Kiseido'));
+  add_link_page_link("http://www.yutopian.com/go/", T_('Yutopian'));
+  add_link_page_link("http://www.samarkand.net/", T_('Samarkand'));
+  add_link_page_link("http://www.xs4all.nl/~paard//", T_('Het Paard'), T_('European shop'));
 
+  echo "<p>&nbsp;\n";
+  echo "<h3 align=left><a name=\"servers\"></a><font color=$h3_color>" . 
+    T_('Other go servers') . "</font></h3>\n";
 
-<h3 align=left><font color=<?php echo $h3_color; ?>><a name="servers"></a>Other go servers</font></h3>
-<p><a href="http://www.itsyourturn.com/">It's your turn</a> --- Also turn based. Has several other games.
-<p><a href="http://kgs.kiseido.com">Kiseido Go Server</a> --- Server with java interface
-<p><a href="http://panda-igs.joyjoy.net/English/contents.html">IGS</a> --- A large server for realtime play
-<p><a href="http://nngs.cosmic.org">NNGS</a> --- An open sourced go server
-<p><a href="http://www.britgo.org/gopcres/play.html">Server list</a> --- A more complete list of servers
+  add_link_page_link("http://www.itsyourturn.com/",
+                     T_('It\'s your turn'),
+                     T_('Also turn based. Has several other games.'));
+  add_link_page_link("http://kgs.kiseido.com",
+                     T_('Kiseido Go Server'),
+                     T_('Server with java interface'));
+  add_link_page_link("http://panda-igs.joyjoy.net/English/contents.html",
+                     T_('IGS'), T_('A large server for realtime play'));
+  add_link_page_link("http://nngs.cosmic.org",
+                     T_('NNGS'), T_('An open sourced go server'));
+  add_link_page_link("http://www.britgo.org/gopcres/play.html",
+                     T_('Server list'), T_('A more complete list of servers'));
 
-</td></tr></table>
+  echo "</td></tr></table>\n";
 
-<?php
-end_page();
+  end_page();
+}
+
 ?>
