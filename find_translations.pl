09a4e6724c8e93580601fd033f63d2f4c5b54417
Erik Ouchterlony <ejlo@users.sf.net>
2002-04-13 19:44:33 +0000

    Perl-script to find translatable sentences

---
+++ find_translations.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+#
+# Perl script to find all translatable sentences.
+#
+
+
+while( <*.php> )
+{
+    $filename = $_;
+
+    select STDOUT;
+
+    open(INFILE, "<$filename") or die "Can't open input file: $!\n";
+
+    $/ = ""; # take all input
+    while (<INFILE>)
+    {
+        if( /T_\(([^)]*)\)/g )
+        {
+            $a = $1;
+            $a =~ s/['"]\s*\.\s*["']//g;
+            $a =~ s/\\n/\n/g;
+            print $filename . ': ' . $a . "\n";
+        }
+    }
+
+    close(INFILE);
+}
+
