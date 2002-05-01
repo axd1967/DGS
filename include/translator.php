e4152c4661d52d825643cfe0ee989a8f72a1561b
Erik Ouchterlony <ejlo@users.sf.net>
2002-05-01 17:35:28 +0000

    Bugfix: include in get_language()

--- include/translator.php
+++ include/translator.php
@@ -63,10 +63,12 @@ class Translator
 
       if( !array_key_exists( $this->current_language, $this->loaded_languages  ) )
         {
-          $add_to_path = '';
-          if( strcmp( basename($HOSTBASE), basename(getcwd()) ) != 0 )
-            $add_to_path = "../";
-          include $add_to_path . "translations/" . $this->current_language . ".php";
+          $file = "translations/" . $this->current_language . ".php";
+
+          if( @is_readable( $file ) )
+             include_once $file;
+          else if( @is_readable( "../$file" ) )
+             include_once "../$file";
 
           $lang_class_name = $this->current_language . "_Language";
           $this->loaded_languages[ $this->current_language ] = new $lang_class_name;
