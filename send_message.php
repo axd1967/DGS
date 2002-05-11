a596ffa06a83abedb475cb730b78d5d7b5b03655
Erik Ouchterlony <ejlo@users.sf.net>
2002-05-11 07:49:57 +0000

    Bugfix: missing include

--- send_message.php
+++ send_message.php
@@ -20,6 +20,7 @@ Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 require( "include/std_functions.php" );
 require( "include/rating.php" );
+require( "include/message_functions.php" );
 
 disable_cache();
 
