061dc093754bda6cdc645afff74fd1487ee40752
Ragnar Ouchterlony <ragnar@users.sf.net>
2002-06-14 21:13:09 +0000

    Updated translation code to accept same language with different charset.

--- translations/all_languages.php
+++ translations/all_languages.php
@@ -19,13 +19,13 @@ along with this program; if not, write to the Free Software Foundation,
 Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-add_to_known_languages( "en", "English", "iso-8859-1" );
-add_to_known_languages( "sv", "Swedish", "iso-8859-1" );
-add_to_known_languages( "no", "Norwegian", "iso-8859-1" );
-add_to_known_languages( "de", "German", "iso-8859-1" );
-add_to_known_languages( "fr", "French", "iso-8859-1" );
-add_to_known_languages( "es", "Spanish", "iso-8859-1" );
-add_to_known_languages( "cs", "Czech", "iso-8859-2" );
-add_to_known_languages( "zh", "Chinese", "big5" );
+$known_languages->add( "en", "English", "iso-8859-1" );
+$known_languages->add( "sv", "Swedish", "iso-8859-1" );
+$known_languages->add( "no", "Norwegian", "iso-8859-1" );
+$known_languages->add( "de", "German", "iso-8859-1" );
+$known_languages->add( "fr", "French", "iso-8859-1" );
+$known_languages->add( "es", "Spanish", "iso-8859-1" );
+$known_languages->add( "cs", "Czech", "iso-8859-2" );
+$known_languages->add( "zh", "Chinese (Traditional)", "big5" );
 
 ?>
