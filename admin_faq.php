946f2df5960f1bdb794a885460fe5e3aa90c32d5
Erik Ouchterlony <ejlo@users.sf.net>
2002-09-07 20:06:21 +0000

    FAQ added

---
+++ admin_faq.php
@@ -0,0 +1,253 @@
+<?php
+/*
+Dragon Go Server
+Copyright (C) 2001 Jim Heiney and Erik Ouchterlony
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+require( "include/std_functions.php" );
+require( "include/form_functions.php" );
+
+{
+  connect2mysql();
+
+  $logged_in = is_logged_in($handle, $sessioncode, $player_row);
+
+  if( !$logged_in )
+    error("not_logged_in");
+
+  if( !($player_row['admin_level'] & ADMIN_FAQ) )
+    error("adminlevel_too_low");
+
+  $id = is_numeric($_GET["id"]) ? $_GET["id"] : 0;
+
+  $show_list = true;
+
+  if( $_GET["edit"] == 'c' or  $_GET["edit"] == 'e')
+  {
+     if( $_GET["edit"] == 'c' )
+        start_page(T_("FAQ Admin").' - '.T_('Edit category'), true, $logged_in, $player_row );
+     else
+        start_page(T_("FAQ Admin").' - '.T_('Edit entry'), true, $logged_in, $player_row );
+
+     $show_list = false;
+
+     echo "<center>\n";
+
+     $result = mysql_query( "SELECT * FROM FAQ WHERE EntryID=$id" );
+
+     if( mysql_num_rows($result) != 1 )
+        error("faq_admin_no_such_entry");
+
+     $row = mysql_fetch_array( $result );
+
+     $faq_edit_form = new Form( 'faqeditform', "admin_faq.php?do_edit=t&id=$id", FORM_POST );
+
+     if( $row["Level"] == 1 )
+     {
+        $faq_edit_form->add_row( array( 'HEADER', T_('Edit category') ) );
+        $faq_edit_form->add_row( array( 'DESCRIPTION', T_('Category'),
+                                        'TEXTINPUT', 'question', 50, 80, $row["Question"] ) );
+     }
+     else
+     {
+        $faq_edit_form->add_row( array( 'HEADER', T_('Edit FAQ entry') ) );
+        $faq_edit_form->add_row( array( 'DESCRIPTION', T_('Question'),
+                                        'TEXTINPUT', 'question', 50, 80, $row["Question"] ) );
+        $faq_edit_form->add_row( array( 'DESCRIPTION', T_('Answer'),
+                                        'TEXTAREA', 'answer', 50, 8, $row["Answer"] ) );
+     }
+
+     $faq_edit_form->add_row( array( 'SUBMITBUTTON', 'submit', T_('Submit') ) );
+     $faq_edit_form->echo_string();
+
+  }
+  else if( $_GET["move"] == 'u' or $_GET["move"] == 'd' )
+  {
+     $result = mysql_query( "SELECT * FROM FAQ WHERE ID=$id" );
+
+     if( mysql_num_rows($result) != 1 )
+        error("faq_admin_no_such_entry");
+
+     $row = mysql_fetch_array( $result );
+
+     $result = mysql_query( "SELECT MAX(SortOrder) as max FROM FAQ " .
+                            "WHERE Parent=" . $row["Parent"] );
+     $row2 = mysql_fetch_array( $result );
+     $max = $row2["max"];
+
+     if( ( $_GET["move"] != 'u' or $row["SortOrder"] > 1 ) and
+         ( $_GET["move"] != 'd' or $row["SortOrder"] < $max ) )
+     {
+        $dir = ($_GET["move"] == 'd' ? 1 : -1 );
+
+        mysql_query( "UPDATE FAQ SET SortOrder=SortOrder-($dir) " .
+                     'WHERE Parent=' . $row["Parent"] . ' ' .
+                     'AND SortOrder=' . ($row["SortOrder"]+$dir) );
+        mysql_query( "UPDATE FAQ SET SortOrder=SortOrder+($dir) " .
+                     "WHERE EntryID=" . $row["EntryID"] );
+     }
+  }
+  else if( $_GET["do_edit"] == 't' )
+  {
+     $result = mysql_query( "SELECT * FROM FAQ WHERE ID=$id" );
+
+     if( mysql_num_rows($result) != 1 )
+        error("faq_admin_no_such_entry");
+
+     $row = mysql_fetch_array( $result );
+
+     if( !isset( $_POST["question"] ) )
+        error("No data");
+
+     $question = trim( $_POST["question"] );
+     $answer = trim( $_POST["answer"] );
+
+     mysql_query("UPDATE FAQ SET Question=\"$question\", Answer=\"$answer\" " .
+                 "WHERE ID=$id");
+  }
+  else if( $_GET["new"] == 'e' or $_GET["new"] == 'c')
+  {
+     if( $_GET["new"] == 'c' )
+        start_page(T_("FAQ Admin").' - '.T_('New category'), true, $logged_in, $player_row );
+     else
+        start_page(T_("FAQ Admin").' - '.T_('New entry'), true, $logged_in, $player_row );
+
+     $show_list = false;
+
+     echo "<center>\n";
+
+     $faq_edit_form = new Form( 'faqeditform', "admin_faq.php?do_new=" .
+                                $_GET["new"] . "&id=$id", FORM_POST );
+
+     if( $_GET["new"] == 'c' )
+     {
+        $faq_edit_form->add_row( array( 'HEADER', T_('New category') ) );
+        $faq_edit_form->add_row( array( 'DESCRIPTION', T_('Category'),
+                                        'TEXTINPUT', 'question', 50, 80, '' ) );
+     }
+     else
+     {
+        $faq_edit_form->add_row( array( 'HEADER', T_('New entry') ) );
+        $faq_edit_form->add_row( array( 'DESCRIPTION', T_('Question'),
+                                        'TEXTINPUT', 'question', 50, 80, '' ) );
+        $faq_edit_form->add_row( array( 'DESCRIPTION', T_('Answer'),
+                                        'TEXTAREA', 'answer', 50, 8, '' ) );
+     }
+
+     $faq_edit_form->add_row( array( 'SUBMITBUTTON', 'submit', T_('Submit') ) );
+     $faq_edit_form->echo_string();
+  }
+  else if( $_GET["do_new"] == 'c' or $_GET["do_new"] == 'e' )
+  {
+     $result = mysql_query( "SELECT * FROM FAQ WHERE ID=$id" );
+
+     if( mysql_num_rows($result) != 1 )
+        error("faq_admin_no_such_entry");
+
+     $row = mysql_fetch_array( $result );
+
+     // First entry
+     if( $row["Level"] == 1 and $_GET["do_new"] == 'e' )
+        $row = array("Parent" => $row["EntryID"], "SortOrder" => 0, "Level" => 2);
+
+     // First category
+     if( $row["Level"] == 0 )
+        $row = array("Parent" => $row["EntryID"], "SortOrder" => 0, "Level" => 1);
+
+     if( !isset( $_POST["question"] ) )
+        error("No data");
+
+     $question = trim( $_POST["question"] );
+     $answer = trim( $_POST["answer"] );
+
+     $result = mysql_query( "SELECT MAX(EntryID) as max FROM FAQ " );
+     $row2 = mysql_fetch_array( $result );
+     $max = $row2["max"];
+
+     mysql_query("UPDATE FAQ SET SortOrder=SortOrder+1 " .
+                 'WHERE Parent=' . $row["Parent"] . ' ' .
+                 'AND SortOrder>' . $row["SortOrder"] ) or die(mysql_error());
+
+     mysql_query("INSERT INTO FAQ SET " .
+                 "SortOrder=" . ($row["SortOrder"]+1) . ', ' .
+                 "Parent=" . $row["Parent"] . ', ' .
+                 "EntryID=" . ($max+1) . ', ' .
+                 "Level=" . $row["Level"] . ', ' .
+                 "Question=\"$question\", " .
+                 "Answer=\"$answer\"") or die(mysql_error());
+  }
+
+
+  if( $show_list )
+  {
+     start_page(T_("FAQ Admin"), true, $logged_in, $player_row );
+
+     echo "<table align=center width=\"85%\" border=0><tr><td>\n";
+
+     echo "<h3 align=left><a name=\"general\"></a><font color=$h3_color>" .
+        T_('FAQ Admin') . "</font></h3>\n";
+
+     $result = mysql_query("SELECT entry.*, " .
+                           "IF(entry.Level=1,entry.SortOrder,parent.SortOrder) AS CatOrder " .
+                           "FROM FAQ AS entry, FAQ AS parent " .
+                           "WHERE entry.Parent = parent.EntryID " .
+                           "ORDER BY CatOrder,Level,SortOrder" );
+
+     echo "<table>\n";
+
+
+     echo "<tr><td><a href=\"admin_faq.php?new=c&id=1" . '"><img border=0 title="' .
+        T_('Add new category') . '" src="images/new.png"></a>';
+
+     while( $row = mysql_fetch_array( $result ) )
+     {
+        if( $row['Level'] == 1 )
+        {
+           echo '<tr><td colspan=2>';
+           $typechar = 'c';
+        }
+        else
+        {
+           echo '<tr><td width=20><td>';
+           $typechar = 'e';
+        }
+
+        echo "<A href=\"admin_faq.php?edit=$typechar&id=" . $row['ID'] .
+           '" title="' . T_("Edit") . '">' . $row['Question'] . "</A>\n";
+
+        echo '<td width=40 align=right><a href="admin_faq.php?move=u&id=' .
+           $row['ID'] . '"><img border=0 title="' . T_("Move up") . '" src="images/up.png"></a>';
+        echo '<td><a href="admin_faq.php?move=d&id=' .
+           $row['ID'] . '"><img border=0 title="' . T_("Move down") . '" src="images/down.png"></a>';
+
+        echo "<td><a href=\"admin_faq.php?new=$typechar&id=" . $row['ID'] .
+           '"><img border=0 title="' .
+           ($typechar == 'e' ? T_('Add new entry') : T_('Add new category')) .
+           '" src="images/new.png"></a>';
+        if( $row["Level"] == 1 )
+           echo "<tr><td witdh=20><td><a href=\"admin_faq.php?new=e&id=" .
+              $row['ID'] . '"><img border=0 title="' . T_('Add new entry') .
+              '" src="images/new.png"></a>';
+     }
+
+
+     echo "</table></table>\n";
+  }
+
+  end_page();
+}
+?>
---
+++ faq.php
@@ -0,0 +1,87 @@
+<?php
+/*
+Dragon Go Server
+Copyright (C) 2001 Jim Heiney and Erik Ouchterlony
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+require( "include/std_functions.php" );
+
+{
+  connect2mysql();
+
+  $logged_in = is_logged_in($handle, $sessioncode, $player_row);
+
+  start_page(T_("FAQ"), true, $logged_in, $player_row );
+
+  echo "<table align=center width=\"85%\" border=0><tr><td>\n";
+  echo "<h3 align=left><a name=\"general\"></a><font color=$h3_color>" .
+    T_('Frequently Asked Questions') . "</font></h3>\n";
+
+
+  if( $_GET["read"] == 't' )
+  {
+     $cat = is_numeric($_GET["cat"]) ? $_GET["cat"] : 0;
+     $result = mysql_query( "SELECT entry.*, parent.SortOrder AS ParentOrder " .
+                            "FROM FAQ AS entry, FAQ AS parent " .
+                            "WHERE entry.Parent = parent.EntryID " .
+                            "AND ( entry.Parent = $cat OR entry.EntryID = $cat ) " .
+                            "ORDER BY ParentOrder,entry.SortOrder" );
+
+     echo "<ul><table width=\"100%\" cellpadding=2 cellspacing=0 border=0><tr><td>\n";
+
+     while( $row = mysql_fetch_array( $result ) )
+     {
+        if( $row['Level'] == 1 )
+        {
+           echo '<h4><A href="faq.php">' . $row['Question'] . "</A></h4><ul>\n";
+        }
+        else
+           echo '<p><A name="Entry' . $row["EntryID"] . '"><li><h4>' . $row['Question'] . '</h4><P>' .$row['Answer'];
+     }
+
+
+     echo "</table></table></ul></ul>\n";
+  }
+  else
+  {
+     $result =  mysql_query( "SELECT entry.*, parent.SortOrder AS ParentOrder " .
+                             "FROM FAQ AS entry, FAQ AS parent " .
+                             "WHERE entry.Parent = parent.EntryID " .
+                             "ORDER BY ParentOrder,SortOrder" );
+
+     echo "<ul><ul><table>\n";
+
+     while( $row = mysql_fetch_array( $result ) )
+     {
+        if( $row['Level'] == 1 )
+        {
+           echo "</table></ul>\n";
+           echo '<A href="faq.php?read=t&cat=' . $row['EntryID'] . '">' . $row['Question'] . "</A>\n";
+           echo "<ul><table width=\"100%\" cellpadding=2 cellspacing=0 border=0><tr><td>\n";
+        }
+        else
+           echo '<li><A href="faq.php?read=t&cat=' . $row['Parent'] .
+              '#Entry' . $row['EntryID'] . '">' . $row['Question'] . "</A>\n";
+     }
+
+
+     echo "</table></table></ul></ul>\n";
+  }
+
+  end_page();
+}
+?>
