c8ad763db827b2eda9607e02910eab98df565837
Erik Ouchterlony <ejlo@users.sf.net>
2002-10-18 17:47:23 +0000

    Change includes to require_once

--- clock_tick.php
+++ clock_tick.php
@@ -18,8 +18,8 @@ along with this program; if not, write to the Free Software Foundation,
 Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-require( "include/std_functions.php" );
-include( "include/rating.php" );
+require_once( "include/std_functions.php" );
+require_once( "include/rating.php" );
 
 if( !$is_down )
 {
--- confirm.php
+++ confirm.php
@@ -18,10 +18,10 @@ along with this program; if not, write to the Free Software Foundation,
 Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-require( "include/std_functions.php" );
-include( "include/board.php" );
-include( "include/move.php" );
-include( "include/rating.php" );
+require_once( "include/std_functions.php" );
+require_once( "include/board.php" );
+require_once( "include/move.php" );
+require_once( "include/rating.php" );
 
 disable_cache();
 
