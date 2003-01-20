420a398ef6c2315ac890a0cf7ef004fe0ca9a2e9
Erik Ouchterlony <ejlo@users.sf.net>
2003-01-20 18:38:40 +0000

    Use rating at game time for finished games

--- sgf.php
+++ sgf.php
@@ -43,18 +43,20 @@ require_once( "include/rating.php" );
    if( !$gid )
       error("unknown_game");
 
-   $result = mysql_query( 'SELECT Games.*, ' .
-                          'Games.Flags+0 AS flags, ' .
-                          'UNIX_TIMESTAMP(Games.Starttime) AS startstamp, ' .
-                          'UNIX_TIMESTAMP(Games.Lastchanged) AS timestamp, ' .
-                          'black.Name AS Blackname, ' .
-                          'black.Handle AS Blackhandle, ' .
-                          'black.Rating2 AS Blackrating, ' .
-                          'white.Name AS Whitename, ' .
-                          'white.Handle AS Whitehandle, ' .
-                          'white.Rating2 AS Whiterating ' .
-                          'FROM Games, Players AS black, Players AS white ' .
-                          "WHERE Games.ID=$gid AND Black_ID=black.ID AND White_ID=white.ID" );
+   $result = mysql_query(
+      'SELECT Games.*, ' .
+      'Games.Flags+0 AS flags, ' .
+      'UNIX_TIMESTAMP(Games.Starttime) AS startstamp, ' .
+      'UNIX_TIMESTAMP(Games.Lastchanged) AS timestamp, ' .
+      'black.Name AS Blackname, ' .
+      'black.Handle AS Blackhandle, ' .
+      "IF(Games.Status='FINISHED',Games.Black_Rating, black.Rating2 ) AS Blackrating, " .
+      'white.Name AS Whitename, ' .
+      'white.Handle AS Whitehandle, ' .
+      "IF(Games.Status='FINISHED',Games.White_Rating, white.Rating2 ) AS Whiterating " .
+      'FROM Games, Players AS black, Players AS white ' .
+      "WHERE Games.ID=$gid AND Black_ID=black.ID AND White_ID=white.ID" )
+      or die(mysql_error());
 
    if( mysql_num_rows($result) != 1 )
       error("unknown_game");
