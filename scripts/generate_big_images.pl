7930570af26a988dd2210f9de2083c6d8b6d0a63
Erik Ouchterlony <ejlo@users.sf.net>
2003-02-22 18:33:35 +0000

    Changed to suit new makefile

--- scripts/generate_big_images.pl
+++ scripts/generate_big_images.pl
@@ -1,52 +1,64 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
+
+# Dragon Go Server
+# Copyright (C) 2003  Erik Ouchterlony
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 use Gimp qw(:auto);
 
 Gimp::init;
 
-foreach $color ('b', 'w')
+$infile = $ARGV[1];
+$outfile = $ARGV[0];
+
+if( $outfile eq 'BigBlackMark.png' || $outfile eq 'BigWhiteMark.png' )
 {
-    if( $color eq 'b' )
+    if( $outfile eq 'BigBlackMark.png' )
     {
         $file='BigBlack.png';
-        $markfile='BigBlackMark.png';
-        $fg_color=[255, 255, 255];
+        $fg_color="#ffffff";
     }
     else
     {
         $file='BigWhite.png';
-        $markfile='BigWhiteMark.png';
-        $fg_color=[0, 0, 0];
+        $fg_color="#000000";
     }
 
     $theImage = gimp_file_load($file, $file);
     $theLayer = gimp_image_active_drawable( $theImage );
     plug_in_autocrop( $theImage, $theLayer );
     gimp_palette_set_foreground( $fg_color );
-    print $file . "\n";
-    file_png_save( $theImage, $theLayer, $file, $file, 0, 9, 0, 0, 0, 0, 0);
-
     gimp_ellipse_select( $theImage, 100, 100, 300, 300, REPLACE, 1, 0, 0);
     gimp_ellipse_select( $theImage, 150, 150, 200, 200, SUB, 1, 0, 0);
     gimp_bucket_fill( $theLayer, FG_BUCKET_FILL, NORMAL_MODE, 80, 15, 1, 125, 125);
     gimp_selection_none( $theImage);
-    print $markfile . "\n";
-    file_png_save( $theImage, $theLayer, $markfile, $markfile, 0, 9, 0, 0, 0, 0, 0);
+    file_png_save( $theImage, $theLayer, $outfile, $outfile, 0, 9, 0, 0, 0, 0, 0);
 }
-
-
-
-@files = ('YinYang.png', 'BigPlayBlack.png', 'BigPlayWhite.png');
-
-foreach $file (@files)
+elsif( $outfile eq 'BigBlack.png' || $outfile eq 'BigWhite.png' ||
+       $outfile eq 'BigPlayBlack.png' || $outfile eq 'BigPlayWhite.png' ||
+       $outfile eq 'YinYang.png' )
 {
-    $theImage = gimp_file_load($file, $file);
+    $theImage = gimp_file_load($infile, $infile);
     $theLayer = gimp_image_active_drawable( $theImage );
     plug_in_autocrop( $theImage, $theLayer );
-    gimp_palette_set_foreground( $fg_color );
     print $file . "\n";
-    file_png_save( $theImage, $theLayer, $file, $file, 0, 9, 0, 0, 0, 0, 0);
+    file_png_save( $theImage, $theLayer, $outfile, $outfile, 0, 9, 0, 0, 0, 0, 0);
 }
+
+print $outfile . "\n";
 
 Gimp::end;
 
