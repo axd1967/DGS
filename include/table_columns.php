8810a09d9f6869dc1aa149d722f4ea41cd9469e8
Rod Ival <rodival@users.sf.net>
2004-03-10 02:01:53 +0000

    a try to remove constant fatal error with devel server

--- include/table_columns.php
+++ include/table_columns.php
@@ -20,7 +20,7 @@ Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 $TranslateGroups[] = "Common";
 
-require_once("form_functions.php");
+require_once("../include/form_functions.php");
 
 /*!
  * \file table_columns.php
