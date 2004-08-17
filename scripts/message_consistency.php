f55a2e604496371b8313fbb6124c56b0e7a0ea9a
Rod Ival <rodival@users.sf.net>
2004-08-17 09:18:53 +0000

    added check

--- scripts/message_consistency.php
+++ scripts/message_consistency.php
@@ -21,38 +21,73 @@ Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 chdir( '../' );
 require_once( "include/std_functions.php" );
 
-define('DEBUG',1);
-
 {
-   connect2mysql();
-
    disable_cache();
+
+   connect2mysql();
 
    //init_standard_folders();
 
-if(DEBUG){
-   function dbg_query($s) { echo "<BR>$s";}
-   echo "<p>(just show queries needed): <br>";
-}else{
-   function dbg_query($s) { 
-     if( !mysql_query( $s) )
-        die("$s;<br>" . mysql_error());
-   }
-   echo "<p>*** Fixes errors: <br>";
-}
 
-   if( !($uid=@$_REQUEST['uid']) )
-      check_myself_message();
+  $logged_in = who_is_logged( $player_row);
+
+  if( !$logged_in )
+    error("not_logged_in");
+
+  $player_level = (int)$player_row['admin_level'];
+  if( !($player_level & ADMIN_DATABASE) )
+    error("adminlevel_too_low");
+
+
+   if( $do_it=@$_REQUEST['do_it'] )
+   {
+      function dbg_query($s) { 
+        if( !mysql_query( $s) )
+           die("<BR>$s;<BR>" . mysql_error() );
+        echo " --- fixed. ";
+      }
+      echo "<p>*** Fixes errors:<br>";
+   }
    else
+   {
+      function dbg_query($s) { echo " --- query:<BR>$s; ";}
+      echo "<p>(just show queries needed):<br>";
+   }
+
+
+   if( ($uid=@$_REQUEST['uid']) > 0 )
       check_myself_message( $uid);
+   else
+      check_myself_message();
+
+
+
+   echo "<p>Lost replied:";
+
+   $query = "SELECT org.*, cor.Replied, cor.Sender, cor.ID as cid, rep.ID as rid"
+     ." FROM Messages as rep, Messages as org, MessageCorrespondents AS cor, MessageCorrespondents AS cre"
+     ." WHERE rep.ReplyTo=org.ID"
+     .  " AND cor.mid=org.ID AND cor.Replied!='Y' AND cor.Sender!='Y'"
+     .  " AND cre.mid=rep.ID AND cre.Sender!='N' AND cor.uid=cre.uid"
+     ." ORDER BY org.ID";
+   $result = mysql_query( $query ) or die(mysql_error());
+
+   while( ($row = mysql_fetch_assoc( $result )) )
+   {
+      echo '<br>mid= ' . $row['ID'] . ' &lt;- ' . $row['rid'];
+      dbg_query("UPDATE MessageCorrespondents SET Replied='Y' " .
+                   "WHERE ID=".$row['cid']." LIMIT 1" );
+   }
+
+   echo "<br>Lost replied done.\n";
+
 }
 
 // Try to find Myself message
 //see also: message_list_query and message_list_table
 function check_myself_message( $user_id=false)
 {
-
-   echo "<p>Messages to myself: ";
+   echo "<p>Messages to myself:";
 
 //Find old way *messages to myself*, i.e. where sender and receiver are the same user.
    $query = "SELECT me.mid as mid, " .
@@ -69,7 +104,7 @@ function check_myself_message( $user_id=false)
 
    while( ($row = mysql_fetch_assoc( $result )) )
    {
-      echo $row['mid']." ";
+      echo '<br>mid ' . $row['mid'];
 
       $folder = @$row['folder'];
       if( !isset($folder) ) $folder = @$row['other_folder'];
@@ -86,6 +121,6 @@ function check_myself_message( $user_id=false)
       dbg_query("DELETE FROM MessageCorrespondents WHERE ID=$mcID LIMIT 1" );
    }
 
-   echo "<p>\n";
+   echo "<br>Messages to myself done.\n";
 }
 ?>
