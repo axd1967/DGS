56d5d63e61ba37ccaa31ba0266276b2dcc94f187
Rod Ival <rodival@users.sf.net>
2004-08-17 09:19:23 +0000

    added option

--- game_consistency.php
+++ game_consistency.php
@@ -18,24 +18,46 @@ along with this program; if not, write to the Free Software Foundation,
 Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
+// Checks and show errors in the Games database.
+
+//chdir( '../' ); //if moved in /scripts
 require_once( "include/std_functions.php" );
 require_once( "include/board.php" );
 require_once( "include/move.php" );
 
 {
+   disable_cache();
+
    connect2mysql();
 
-   if( !($gid=@$_REQUEST['gid']) )
-      $where = "AND DATE_ADD(Lastchanged,INTERVAL 7 day) > FROM_UNIXTIME($NOW)" ;
-   //elseif( $gid='all' ) $where = "" ;
-   else
-      $where = "AND ID=" . $_REQUEST['gid'] ;
+  $logged_in = who_is_logged( $player_row);
 
-   $result = mysql_query("SELECT ID FROM Games WHERE Status!='INVITED' " .
-                         "$where ORDER BY ID");
+  if( !$logged_in )
+    error("not_logged_in");
+
+  $player_level = (int)$player_row['admin_level'];
+  if( !($player_level & ADMIN_DATABASE) )
+    error("adminlevel_too_low");
+
+
+      echo "<p>--- Report only:<br>";
+
+   if( ($gid=@$_REQUEST['gid']) > 0 )
+      $where = " AND ID=$gid";
+   else
+      $where = "" ;
+
+   if( ($since=@$_REQUEST['since']) )
+      $where.= " AND DATE_ADD(Lastchanged,INTERVAL $since) > FROM_UNIXTIME($NOW)";
+
+   $query = "SELECT ID FROM Games WHERE Status!='INVITED'$where ORDER BY ID";
+
+   echo "<p>query: $query;<p>";
+   $result = mysql_query($query);
 
    while( $row = mysql_fetch_array( $result ) )
    {
+      //echo ' ' . $row["ID"];
       check_consistency($row["ID"]);
    }
 
