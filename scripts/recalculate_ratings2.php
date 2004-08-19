2ddfd5753748d3dedf0044472cbd093f04e5ebb4
Rod Ival <rodival@users.sf.net>
2004-08-19 13:33:49 +0000

    comment

--- scripts/recalculate_ratings2.php
+++ scripts/recalculate_ratings2.php
@@ -68,7 +68,7 @@ require_once( "include/rating.php" );
 
    $query = "SELECT Games.ID as gid ".
        "FROM Games, Players as white, Players as black " .
-       "WHERE Status='FINISHED' AND Rated!='N' " . //Rated='Done' " .
+       "WHERE Status='FINISHED' AND Rated!='N' " . //redo Rated='Done' and do missed Rated='Y' 
        "AND white.ID=White_ID AND black.ID=Black_ID ".
        "AND ( NOT ISNULL(white.RatingStatus) ) " .
        "AND ( NOT ISNULL(black.RatingStatus) ) " .
