b845e537c40f6ab4922b22145283846cb2caa4b7
Rod Ival <rodival@users.sf.net>
2006-07-23 22:04:32 +0000

    comments+minor

--- admin_translators.php
+++ admin_translators.php
@@ -37,26 +37,38 @@ require_once( "include/form_functions.php" );
   if( !($player_row['admin_level'] & ADMIN_TRANSLATORS) )
     error("adminlevel_too_low");
 
+
+      $twoletter = get_request_arg('twoletter');
+      $charenc = get_request_arg('charenc');
+      $langname = get_request_arg('langname');
+      $transluser = get_request_arg('transluser');
+      $transladdlang = get_request_arg('transladdlang');
+
+      if( !$charenc )
+         $charenc = 'UTF-8';
+
   start_page(T_('Translator admin'), true, $logged_in, $player_row);
 
   echo "<center>";
 
   $translator_form = new Form( 'translatorform', 'admin_do_translators.php', FORM_POST );
 
-  /* Add language for translation */
-// International languages code (ISO 639):
+// International languages code (ISO 639-1):
 // http://www.loc.gov/standards/iso639-2/englangn.html
+// W3C charsets and encodings index:
+// http://www.w3.org/International/resource-index
 
-      $twoletter = get_request_arg('twoletter');
-      $charenc = get_request_arg('charenc');
-      $langname = get_request_arg('langname');
+// If, a day, the whole site is UTF-8, re-check this site:
+// http://www.w3.org/International/tutorials/tutorial-char-enc/en/all.html#Slide0240
 
+
+  /* Add language for translation */
   $translator_form->add_row( array( 'HEADER', T_('Add language for translation') ) );
-  $translator_form->add_row( array( 'DESCRIPTION', T_('Two-letter language code'),
+  $translator_form->add_row( array( 'DESCRIPTION', T_('Two-letter language code (see ISO 639-1)'),
                                     'TEXTINPUT', 'twoletter', 30, 10, $twoletter ) );
-  $translator_form->add_row( array( 'DESCRIPTION', T_('Language name (i.e. English)'),
+  $translator_form->add_row( array( 'DESCRIPTION', T_('English language name (i.e. French)'),
                                     'TEXTINPUT', 'langname', 30, 50, $langname ) );
-  $translator_form->add_row( array( 'DESCRIPTION', T_("Character encoding (i.e. 'iso-8859-1')"),
+  $translator_form->add_row( array( 'DESCRIPTION', T_("Character encoding (i.e. UTF-8)"),
                                     'TEXTINPUT', 'charenc', 30, 50, $charenc ) );
   $translator_form->add_row( array(
       'SUBMITBUTTON', 'addlanguage', T_('Add language'),
@@ -69,8 +81,8 @@ require_once( "include/form_functions.php" );
   $langs = get_language_descriptions_translated();
   asort($langs);
 
-   $transluser = get_request_arg('transluser');
 
+  /* Show the privileges of the user */
    $translator_form->add_row( array(
       'DESCRIPTION', T_('User to set privileges for (use the userid)'),
       'TEXTINPUT', 'transluser', 30, 80, $transluser,
@@ -114,17 +126,23 @@ require_once( "include/form_functions.php" );
    }
 
 
+  /* Add a single language to the user */
   $translator_form->add_row( array(
       'DESCRIPTION', T_('Select language to make user translator for that language.'),
-      'SELECTBOX', 'transladdlang', 1, $langs, array(), false,
+      'SELECTBOX', 'transladdlang', 1, $langs, array( $transladdlang), false,
       ) );
   $translator_form->add_row( array(
       'SUBMITBUTTON', 'transladd', T_('Add language for translator'),
       ) );
 
 
+
+  /* Define the full set of languages of the user */
   $translator_form->add_row( array(
       'DESCRIPTION', T_('Select the languages the user should be allowed to translate'),
+//Warning: These chars must not be a part of a URI query. From RFC 2396 unwise
+//      unwise = "{" | "}" | "|" | "\" | "^" | "[" | "]" | "`"
+//   Here, transllang[] force a FORM_POST method. (or, maybe, 'transllang%5b%5d')
       'SELECTBOX', 'transllang[]', 7, //transllang[] is a MULTIPLE select box
                   $langs, $transluser_langs, true,
       ) );
