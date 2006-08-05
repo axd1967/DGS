27d3f6995c328934c5cf3148aff9f469e357ccc6
Rod Ival <rodival@users.sf.net>
2006-08-05 22:31:42 +0000

    och to and

--- links.php
+++ links.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Dragon Go Server
-Copyright (C) 2001-2006  Jim Heiney, Erik Ouchterlony och Rod Ival
+Copyright (C) 2001-2006  Jim Heiney, Erik Ouchterlony and Rod Ival
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
