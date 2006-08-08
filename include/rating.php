57126a883c8e1cc6a3bbe8d262a0046a65928f45
Erik Ouchterlony <ejlo@users.sf.net>
2006-08-08 20:49:50 +0000

    Bugfix: ratings on finished games was badly displayed

--- include/rating.php
+++ include/rating.php
@@ -566,7 +566,7 @@ function echo_rating($rating, $show_percent=true, $graph_uid=0, $keep_english=fa
    $T_= ( $keep_english ? 'fnop' : 'T_' );
    //global $dan, $kyu;
 
-   if( !isset($rating) or $rating < -1000 ) return '';
+   if( !isset($rating) or $rating < -2000 ) return '';
 
    $spc = ( $show_percent === true ? '&nbsp;' : ' ' );
 
