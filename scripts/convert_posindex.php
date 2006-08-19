9bf18e870ba03ebbdd81f80222a17cbd799113a7
Rod Ival <rodival@users.sf.net>
2006-08-19 09:03:22 +0000

    Disable this dangerous script

--- scripts/convert_posindex.php
+++ scripts/convert_posindex.php
@@ -18,7 +18,8 @@ along with this program; if not, write to the Free Software Foundation,
 Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-// Checks and show errors in the Games database.
+// Checks and fix the PosIndex bug in the Posts database.
+   exit; //disabled because it's dangerous to execute it twice.
 
 require_once( "../forum/forum_functions.php" );
 
