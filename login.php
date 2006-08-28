aa33fa972dda5eb26443cdde86c091c09f09016d
Erik Ouchterlony <ejlo@users.sf.net>
2006-08-28 15:03:10 +0000

    Minor E_NOTICE fix

--- login.php
+++ login.php
@@ -18,8 +18,8 @@ along with this program; if not, write to the Free Software Foundation,
 Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-if( @$_REQUEST['quick_mode'] )
-   $quick_errors = 1;
+$quick_errors = isset($_REQUEST['quick_mode']);
+
 require_once( "include/std_functions.php" );
 
 {
