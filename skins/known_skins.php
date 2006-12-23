6c7de55b4fd9e8eb2fbda8a7af4294e183eabb34
Rod Ival <rodival@users.sf.net>
2006-12-23 15:11:13 +0000

    Implementation of skins

---
+++ skins/known_skins.php
@@ -0,0 +1,11 @@
+<?php
+
+// The 'dragon' folder (fully filled) is requiered (see default CSS files).
+
+// Lines formed as: 'folder_name' /*32 chars max*/ => 'Convivial name',
+$known_skins = array(
+   'dragon' => 'Dragon Go Server original',
+   'dragon2' => 'DGS try',
+);
+
+?>
