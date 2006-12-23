60d8a905580829b9182d4495dfd4d6a65508d148
Erik Ouchterlony <ejlo@users.sf.net>
2006-12-23 00:58:46 +0000

    Updated to recognize modern browsers

--- scripts/browser_stats.php
+++ scripts/browser_stats.php
@@ -41,10 +41,10 @@ function search_browsers($result, &$Browsers)
             if( $nr == 1 )
                continue 2;
 
-            echo "No Browser Found!!!!!";
+            echo "No Browser Found: {$Browser} <br>\n";
          }
 
-      echo "No Family Found!!!!!";
+      echo "No Family Found:{$Browser}<br>\n";
    }
 }
 
@@ -79,7 +79,9 @@ function echo_browsers(&$Browsers)
    $Browsers = array(array('Opera' => array('opera',0),
                            'Opera 5' => array('opera.5',0),
                            'Opera 6' => array('opera.6',0),
-                           'Opera 7' => array('Opera.7',0)),
+                           'Opera 7' => array('opera.7',0),
+                           'Opera 8' => array('opera.8',0),
+                           'Opera 9' => array('opera.9',0)),
 
                      array('Konqueror' => array('konqueror|safari',0),
                            'Konqueror 2' => array('konqueror.?2',0),
@@ -89,31 +91,47 @@ function echo_browsers(&$Browsers)
                      array('AntFresco' => array('antfresco',0)),
                      array('OmniWeb' => array('omniweb',0)),
                      array('Links' => array('links',0)),
+                     array('Lynx' => array('lynx',0)),
                      array('curl' => array('url',0)),
+                     array('Jakarta' => array('jakarta',0)),
+                     array('w3m' => array('w3m',0)),
+                     array('R1ED Browser' => array('r1ed',0)),
+                     array('libwww-perl' => array('libwww-perl',0)),
+                     array('GameWatch' => array('gamewatch',0)),
+                     array('RTGO Client' => array('rtgo client',0)),
+                     array('Indy Library' => array('indy library',0)),
 
-                     array('MSIE' => array('MSIE',0),
-                           'MSIE <4' => array('MSIE [23]',0),
-                           'MSIE 4' => array('MSIE 4',0),
-                           'MSIE 5' => array('MSIE 5',0),
-                           'MSIE 6' => array('MSIE 6',0)),
+                     array('MSIE' => array('msie',0),
+                           'MSIE <4' => array('msie [23]',0),
+                           'MSIE 4' => array('msie 4',0),
+                           'MSIE 5' => array('msie 5',0),
+                           'MSIE 6' => array('msie 6',0),
+                           'MSIE 7' => array('msie 7',0),
+                           'MSIE Other' => array('msie',0)),
 
                      array('Netscape 4' => array('mozilla.?4',0)),
 
-                     array('Gecko' => array('gecko|mozilla.?[567]',0),
+                     array('Gecko' => array('gecko|firefox|mozilla.?[5678]',0),
+                           'Firefox' => array('firefox',0),
                            'Galeon' => array('galeon',0),
                            'Netscape 6' => array('netscape.?6',0),
                            'Netscape 7' => array('netscape.?7',0),
+                           'Netscape 8' => array('netscape.?8',0),
+                           'Netscape Other' => array('netscape',0),
                            'Phoenix' => array('phoenix',0),
                            'Camino' => array('chimera|camino',0),
-                           'Mozilla' => array('mozilla',0)),
+                           'Gecko Other' => array('gecko|mozilla',0)));
 
-                     array('Other' => array('.',0)));
+
+//                     array('Other' => array('.',0)));
+
 
 
 
 
    $result = mysql_query("SELECT count(*) AS Count, Browser FROM Players " .
-                         "WHERE Browser IS NOT NULL Group by Browser")
+                         "WHERE Browser IS NOT NULL AND Browser!='' AND Activity > 0.1 " .
+                         "GROUP BY Browser")
       or die(mysql_error());
 
    search_browsers($result, $Browsers);
@@ -126,7 +144,8 @@ function echo_browsers(&$Browsers)
    $OSes = array(array('Linux' => array('linux',0),
                        'Linux 2.2' => array('2.2',0),
                        'Linux 2.4' => array('2.4',0),
-                       'Linux 2.5' => array('2.5',0)),
+                       'Linux 2.5' => array('2.5',0),
+                       'Linux 2.6' => array('2.6',0)),
 
                  array('FreeBSD' => array('freebsd',0)),
 
@@ -136,18 +155,26 @@ function echo_browsers(&$Browsers)
                  array('IRIX' => array('irix',0)),
 
                  array('WebTV' => array('webtv',0)),
+                 array('PSP' => array('psp',0)),
+                 array('Palm' => array('palm',0)),
+                 array('Nintendo Wii' => array('wii',0)),
 
                  array('Unix' => array('unix',0)),
+
+                 array('Mobile phone' => array('nokia|ericsson',0),
+                       'Nokia' => array('nokia',0),
+                       'SonyEricsson' => array('ericsson',0)),
 
                  array('Mac' => array('mac',0),
                        'OS X' => array('os x',0)),
 
                  array('Windows' => array('win',0),
+                       'XP' => array('(win|windows).?(xp|nt 5.1)',0),
                        'NT' => array('(win|windows).?nt',0),
                        '95' => array('(win|windows).?95',0),
                        '98' => array('(win|windows).?98',0),
-                       '2000' => array('(win|windows).?2000',0),
-                       'XP' => array('(win|windows).?xp',0)),
+                       '2000' => array('(win|windows).?2000',0)),
+
 
                  array('Other' => array('.',0)));
 
