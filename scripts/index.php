89e8a625d77308c3b8292337fe459d9527b0fdd4
Rod Ival <rodival@users.sf.net>
2006-12-30 11:06:05 +0000

    fake index enhancement

--- scripts/index.php
+++ scripts/index.php
@@ -1,6 +1,7 @@
 <?php
-chdir('../');
-//require_once( 'include/std_functions.php' );
+do {
+  if(!chdir('../')) exit;
+} while(!file_exists('include/connect2mysql.php'));
 require_once( 'include/connect2mysql.php' );
   jump_to('index.php');
 ?>
