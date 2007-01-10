ba64739b38d4e75dfd6bd0fe44fb6b88c335e51f
Rod Ival <rodival@users.sf.net>
2007-01-10 18:35:58 +0000

    CSS adjust using the Table_info class + ordering of the bio infos (need to add the SortOrder column in the Bio db)

--- change_bio.php
+++ change_bio.php
@@ -21,7 +21,8 @@ Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 $TranslateGroups[] = "Users";
 
 require_once( "include/std_functions.php" );
-require_once( "include/rating.php" );
+//require_once( "include/rating.php" );
+
 
 {
    disable_cache();
@@ -33,64 +34,140 @@ require_once( "include/rating.php" );
    if( !$logged_in )
       error("not_logged_in");
 
-
    if( $player_row["Handle"] == "guest" )
       error("not_allowed_for_guest");
 
+   $change_it = isset($_REQUEST['action']);
 
 
-   $result = mysql_query("SELECT * FROM Bio where uid=" . $player_row["ID"] . " order by ID")
+   // use default sort-order: SortOrder & ID (fallback)
+   $result = mysql_query("SELECT * FROM Bio WHERE uid=" . $player_row["ID"]
+               . " order by SortOrder, ID")
       or error('mysql_query_failed','change_bio.find_bio');
 
-
-
-
-   while( $row = mysql_fetch_array( $result ) )
+   $bios = array();
+   $idx = 0;
+   while( $row = mysql_fetch_assoc( $result ) )
    {
-      extract($row);
+      $ID= $row['ID'];
 
-      $EnteredText = trim(get_request_arg("text$ID"));
-      $EnteredCategory = trim(get_request_arg("category$ID"));
+      // delete entry
+      if ( $change_it )
+      {
+         $EnteredText = trim(get_request_arg("text$ID"));
+         if( $EnteredText == "" )
+         {
+            $query = "DELETE FROM Bio WHERE ID=$ID";
+            mysql_query( $query )
+               or error('mysql_query_failed', "change_bio.delete_bio[$ID]");
+            continue;
+         }
+      }
 
-      if( $EnteredCategory == '' )
-         $EnteredCategory = trim(get_request_arg("other$ID"));
+      $idx++;
+      $row['newpos'] = $idx;
+      $bios[$idx] = $row;
+   }
+   $max_pos = $idx;
 
-      if( $EnteredText == $Text and $EnteredCategory == $Category )
+   // compute the new SortOrder
+   if ( !$change_it )
+   foreach( $bios as $idx => $row )
+   {
+      $ID= $row['ID'];
+
+      // check for bios movements
+      $move = (int)@$_REQUEST['move'.$ID];
+      if ( !$move )
          continue;
 
-      if( $EnteredText == "" )
-         $query = "DELETE FROM Bio WHERE ID=$ID";
-      else //$EnteredCategory could be ''
+      $pos = $idx + $move;
+      while( $pos < 1 )
+         $pos+= $max_pos;
+      while( $pos > $max_pos )
+         $pos-= $max_pos;
+
+      // swap in internal struct
+      $move = $bios[$idx]['newpos'];
+      $bios[$idx]['newpos'] = $bios[$pos]['newpos'];
+      $bios[$pos]['newpos'] = $move;
+      // note: the result is slightly different if multiple adjacent ups or
+      // multiple adjacent downs but it is called with only one move at a time.
+   }
+
+   // update existing DB-entries
+   foreach( $bios as $idx => $row ) {
+      $ID= $row['ID'];
+
+      if ( $change_it )
+      {
+         $EnteredText = trim(get_request_arg("text$ID"));
+         $EnteredCategory = trim(get_request_arg("category$ID"));
+         if( $EnteredCategory == '' )
+            $EnteredCategory = trim(get_request_arg("other$ID"));
+
+         if( $EnteredText == $row['Text']
+            && $EnteredCategory == $row['Category']
+            && $row['SortOrder'] == $row['newpos']
+            )
+            continue;
+
          $query = "UPDATE Bio set uid=" . $player_row["ID"] .
             ', Text="'.addslashes($EnteredText).'"' .
             ', Category="'.addslashes($EnteredCategory).'"' .
+            ', SortOrder="'.$row['newpos'].'"' .
             " WHERE ID=$ID";
+      }
+      else
+      {
+         if( $row['SortOrder'] == $row['newpos'] )
+            continue;
+
+         $query = "UPDATE Bio set uid=" . $player_row["ID"] .
+            //', Text="'.addslashes($row['Text']).'"' .
+            //', Category="'.addslashes($row['Category']).'"' .
+            ', SortOrder="'.$row['newpos'].'"' .
+            " WHERE ID=$ID";
+      }
 
       mysql_query( $query )
-         or error('mysql_query_failed','change_bio.alter_bio');
+         or error('mysql_query_failed', "change_bio.alter_bio[$ID]");
    }
 
-   for($i=1; $i<=3; $i++)
+   // add new entries
+   $idx = get_request_arg("newcnt");
+   if ( $change_it )
+   for( $ID=1; $ID <= $idx; $ID++ )
    {
-      $EnteredText = trim(get_request_arg("newtext$i"));
-      $EnteredCategory = trim(get_request_arg("newcategory$i"));
+      $EnteredText = trim(get_request_arg("newtext$ID"));
+      $EnteredCategory = trim(get_request_arg("newcategory$ID"));
 
       if( $EnteredCategory == '' )
-         $EnteredCategory = trim(get_request_arg("newother$i"));
+         $EnteredCategory = trim(get_request_arg("newother$ID"));
 
       if( $EnteredText == "" )
          continue;
 
       $query = "INSERT INTO Bio set uid=" . $player_row["ID"] .
             ', Text="'.addslashes($EnteredText).'"' .
-            ', Category="'.addslashes($EnteredCategory).'"' ;
+            ', Category="'.addslashes($EnteredCategory).'"' .
+            ', SortOrder="'.(++$max_pos).'"';
 
       mysql_query( $query )
          or error('mysql_query_failed','change_bio.insert_bio');
    }
 
+
+   if ( !$change_it )
+   {
+      // was up/down-move
+      jump_to("edit_bio.php?editorder=1");
+   }
+
    $msg = urlencode(T_('Bio updated!'));
 
-   jump_to("userinfo.php?uid=" . $player_row["ID"] . URI_AMP."sysmsg=$msg");
+   jump_to("edit_bio.php?sysmsg=$msg");
+   //jump_to("userinfo.php?uid=" . $player_row["ID"] . URI_AMP."sysmsg=$msg");
+
 }
 ?>
--- edit_bio.php
+++ edit_bio.php
@@ -21,9 +21,10 @@ Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 $TranslateGroups[] = "Users";
 
 require_once( "include/std_functions.php" );
-require_once( "include/timezones.php" );
-require_once( "include/rating.php" );
 require_once( "include/form_functions.php" );
+require_once( 'include/table_infos.php' );
+
+define('USER_BIO_ADDENTRIES', 3);
 
 
 function find_category_box_text($cat)
@@ -44,7 +45,10 @@ function find_category_box_text($cat)
    if( !$logged_in )
       error("not_logged_in");
 
-   $result = mysql_query("SELECT * FROM Bio where uid=" . $player_row["ID"] . " order by ID")
+   $editorder = isset($_REQUEST['editorder']);
+
+   $result = mysql_query("SELECT * FROM Bio where uid=" . $player_row["ID"]
+               . " order by SortOrder, ID")
       or error('mysql_query_failed', 'edit_bio.find_bios');
 
 
@@ -65,52 +69,117 @@ function find_category_box_text($cat)
 
 
 
+   $page = "edit_bio.php";
    $title = T_("Edit biographical info");
-   start_page( $title, true, $logged_in, $player_row );
-   echo "<center><h3><font color=$h3_color>$title</font></h3></center>\n";
-
-
-   echo "<CENTER>\n";
-   $bio_form = new Form( 'bioform', 'change_bio.php', FORM_POST );
-
-   while( $row = mysql_fetch_array( $result ) )
-   {
-      $cat = find_category_box_text($row["Category"]);
-
-      $bio_form->add_row( array( 'SELECTBOX', "category".$row["ID"], 1,
-                                 $categories, $cat, false,
-
-                                 'BR',
-
-                                 'TEXTINPUT', "other".$row["ID"], 15, 40,
-                                 ($cat == '' ? $row["Category"] : '' ),
-
-                                 'TD',
-
-                                 'TEXTAREA', "text".$row["ID"],40,4,$row["Text"] ) );
-    }
-
-// And now three empty ones:
-
-   for($i=1; $i<=3; $i++)
-   {
-      $bio_form->add_row( array( 'SELECTBOX', "newcategory" . $i, 1,
-                            $categories, '', false,
-
-                            'BR',
-
-                            'TEXTINPUT', "newother" . $i, 15, 40, "",
-
-                            'TD',
-
-                            'TEXTAREA', "newtext" . $i, 40, 4, "" ) );
+   $othertitle = T_("Edit biographical order");
+   if ( $editorder ) {
+      $str = $title;
+      $title = $othertitle;
+      $othertitle = $str;
    }
 
-   $bio_form->add_row( array( 'SUBMITBUTTON', 'action', T_('Change bio') ) );
-   $bio_form->echo_string(1);
+   start_page( $title, true, $logged_in, $player_row );
+   echo "<h3 class=header>$title</h3>\n";
+
+   echo "<CENTER>\n";
+
+   {
+      $cat_max = 40;
+      $cat_width = 20;
+      $text_width = 60;
+      $text_height = 4;
+
+      $moveurl = 'change_bio.php';
+      if ( !$editorder )
+      {
+         $bio_form = new Form( 'bioform', $moveurl, FORM_POST );
+      }
+      else
+      {
+         $bio_table= new Table_info('biomove');
+      }
+      $moveurl.= '?';
+
+      while( $row = mysql_fetch_array( $result ) )
+      {
+         $cat = find_category_box_text($row["Category"]);
+
+         if ( !$editorder )
+         {
+            $bio_row = array(
+               'CELL', 0, 'class=header',
+               'SELECTBOX', "category".$row["ID"], 1, $categories, $cat, false,
+               'BR',
+               'TEXTINPUT', "other".$row["ID"], $cat_width, $cat_max
+                          , ($cat == '' ? $row["Category"] : '' ),
+               'CELL', 0, 'class=info',
+               'TEXTAREA', "text".$row["ID"], $text_width, $text_height, $row["Text"]
+            );
+
+            $bio_form->add_row( $bio_row );
+         }
+         else
+         {
+            $bio_table->add_row( array(
+               'header' => '<div class=bold>'
+                     . make_html_safe($cat == ''
+                                 ? $row["Category"] : $categories[$cat] )
+                     . "\n</div><div class=center>\n"
+                     . anchor($moveurl.'move'.$row['ID'].'=1'
+                           , image( 'images/down.png', 'down')
+                           , T_("Move down"))
+                     . "\n"
+                     . anchor($moveurl.'move'.$row['ID'].'=-1'
+                           , image( 'images/up.png', 'up')
+                           , T_("Move up"))
+                     . "\n</div>",
+               'info' => make_html_safe($row["Text"],true),
+            ) );
+         }
+      } //while($row)
+
+      if ( !$editorder )
+      {
+         // And now some empty ones:
+         for($i=1; $i <= USER_BIO_ADDENTRIES; $i++)
+         {
+            $bio_form->add_row( array(
+                  'CELL', 0, 'class=newheader',
+                  'SELECTBOX', "newcategory" . $i, 1, $categories, '', false,
+
+                  'BR',
+                  'TEXTINPUT', "newother" . $i, $cat_width, $cat_max, "",
+
+                  'CELL', 0, 'class=newinfo',
+                  'TEXTAREA', "newtext" . $i, $text_width, $text_height, "",
+               ) );
+         }
+
+         $bio_form->add_row( array(
+                  'HIDDEN', 'newcnt', USER_BIO_ADDENTRIES,
+                  'SUBMITBUTTON', 'action', T_('Change bio')
+               ) );
+         $bio_form->echo_string(1);
+      }
+      else
+      {
+         $bio_table->echo_table();
+      }
+   }
 
    echo "</CENTER><BR>\n";
 
-   end_page();
+
+   if ( $editorder ) {
+      $page = $page;
+   } else {
+      $page = make_url($page, array( 'editorder' => '1') );
+   }
+   $menu_array = array(
+         $othertitle  => $page,
+         T_('User info') => 'userinfo.php',
+      );
+
+   end_page(@$menu_array);
 }
 ?>
