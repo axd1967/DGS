d7aabbc995a29c187fc31f001bf406299127a32f
Rod Ival <rodival@users.sf.net>
2007-01-12 12:55:38 +0000

    included connect2mysql

--- include/quick_common.php
+++ include/quick_common.php
@@ -18,6 +18,7 @@ along with this program; if not, write to the Free Software Foundation,
 Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
+require_once( "include/connect2mysql.php" );
 require_once( "include/error_functions.php" );
 
 $is_down = false;
