862dcaa02c9c5b04a561ee9cd8a534c6a3a8b6a6
Jens-Uwe Gaspar <juga@users.sf.net>
2007-06-30 18:12:43 +0000

    added example how to use filters with or without Forms

---
+++ code_examples/filter_example.php
@@ -0,0 +1,243 @@
+<?php
+
+/*
+ * Code example demonstrating use of Filter-Framework (Table + Form):
+ *   1. with Form + Filters
+ *   2. with External-Form + Table + Filters
+ *   3. Table + Filters
+ *
+ * Usage: open its URL in browser
+ */
+
+chdir("../");
+require_once("include/std_functions.php");
+require_once("include/std_classes.php");
+require_once("include/form_functions.php");
+require_once("include/table_columns.php");
+require_once("include/filter.php");
+chdir("code_examples/");
+
+/* init vars */
+
+$page = 'filter_example.php';
+
+$DEBUG_SQL = true;
+
+echo "<html><body>\n";
+
+// Filter-Demos with types and config
+$arr_filter_demos = array(
+   1 => 'Form with Filters + Show-Rows',
+   2 => 'Table + External-Form with Filters',
+   3 => 'Table with Filters',
+);
+
+$fdemo = get_request_arg('fdemo');
+if ( !isset($arr_filter_demos[$fdemo]) )
+   $fdemo = 1;
+
+/* control-form to select filter-demo */
+
+# select demo
+$actform = new Form( "myname", $page, FORM_GET );
+$actform->add_row( array(
+      'DESCRIPTION',  'Filter-DemoType',
+      'SELECTBOX',    'fdemo', 1, $arr_filter_demos, $fdemo, false,
+      'SUBMITBUTTON', 'action', 'Choose Filter-DemoType',
+      ));
+$actform->echo_string();
+
+# prep hidden for adding common fdemo into filter-forms
+$rq = new RequestParameters();
+$rq->add_entry( 'fdemo', $fdemo );
+
+
+/* init vars for filters */
+
+$ARR_ANIMALS = array( 1 => 'monkey', 2 => 'lion', 3 => 'dog' );
+
+$qsql = new QuerySQL(
+   SQLP_FIELDS, '*',
+   SQLP_FROM, 'TableName' );
+
+
+/* -------------------- (1) Form + Filters + Show-Rows ---------------------------  */
+// other example: see forum/search.php
+
+if ( $fdemo == 1 )
+{
+   echo "<h3>Example #1 - Form used with Filter (+ Show-Rows-selection)</h3>\n";
+
+   // static filters
+   $filter = new SearchFilter('t1');
+   $f1 =& $filter->add_filter( 1, 'Text', 'textColA', true,
+         array( FC_START_WILD => 1 ));
+   $f2 =& $filter->add_filter( 2, 'Text', 'textColB', true,
+         array( FC_NO_RANGE => 1, FC_START_WILD => STARTWILD_OPTMINCHARS ));
+   $filter->init();
+
+   // form for static filters
+   $form = new Form( 'table1', $page, FORM_GET );
+   $form->attach_table( $rq ); // add fdemo-var
+
+   $form->add_row( array(
+         'DESCRIPTION', 'Text-Column A',
+         'FILTER',      $filter, 1,
+         'TEXT',        $f1->get_syntax_description(),
+         'BR',
+         'FILTERERROR', $filter, 1, $FERR1, $FERR2."<BR>", true,
+         ));
+   $form->add_row( array(
+         'DESCRIPTION', 'Text-Column B',
+         'FILTER',      $filter, 2,
+         'TEXT',        $f2->get_syntax_description(),
+         'BR',
+         'FILTERERROR', $filter, 2, $FERR1, $FERR2."<BR>", true,
+         ));
+   $form->add_row( array(
+         'TAB',
+         'CELL',        1, 'align=left',
+         'OWNHTML',     implode( '', $filter->get_submit_elements() ) ));
+
+   $maxrows = get_request_arg( 'maxrows' );
+   $form->add_row( array(
+         'DESCRIPTION', T_('Show Rows'),
+         'SELECTBOX',   'maxrows', 1, build_maxrows_array($maxrows), $maxrows, false ));
+
+   $q1 = $qsql->clone();
+   $q1->merge( $filter->get_query() );
+   $form->echo_string();
+   if ( $DEBUG_SQL ) echo "<br>QUERY: " . make_html_safe($q1->get_select()) . "<br>\n";
+   if ( $DEBUG_SQL ) echo "<br>Max Rows: " . $maxrows . "<br>\n";
+}
+
+
+
+/* -------------------- (2) External-Form + Table + Filters ----------------------  */
+// other example: see search_messages.php, user_stats.php
+
+if ( $fdemo == 2 )
+{
+   echo "<hr size=1>\n";
+   echo "<h3>Example #2 - External-Form and Table with Filter</h3>\n";
+
+   // static filters
+   $filter = new SearchFilter('t2s');
+   $filter->add_filter(1, 'Boolean', "boolColC='a_value'", true);
+   $filter->add_filter(2, 'Boolean', array( true => 'condition1', false => 'condition2'), true,
+         array( FC_LABEL => 'label #2' ));
+   $filter->init();
+
+   // table filters
+   $tfilter = new SearchFilter('t2');
+   $tfilter->add_filter( 1, 'Numeric', 'ID', true);
+   $tfilter->add_filter( 2, 'Text',    'Animal', true, array( FC_NO_WILD => 1 ));
+   $tfilter->init();
+
+   $table = new Table( 'table2', $page, '' );
+   $table->register_filter( $tfilter );
+   $table->add_or_del_column();
+
+   // External-Form
+   $form = new Form( 'table2', $page, FORM_GET, false );
+   $form->set_config( FEC_EXTERNAL_FORM, true );
+   $table->set_externalform( $form ); // also attach offset, sort, manage-filter as hidden (table) to ext-form
+   $form->attach_table( $filter ); // attach manage-filter as hiddens (static) to ext-form
+   $form->attach_table( $rq ); // add fdemo-var
+
+   // add_tablehead($nr, $descr, $sort=NULL, $desc_def=false, $undeletable=false, $attbs=NULL)
+   $table->add_tablehead( 1, 'ID', 'ID', false, true);
+   $table->add_tablehead( 2, 'Animal', 'Animal');
+
+   // form for static filters
+   $form->add_row( array(
+         'DESCRIPTION', 'Bool-Column C',
+         'FILTER',      $filter, 1,
+         'BR',
+         'FILTERERROR', $filter, 1, $FERR1, $FERR2."<BR>", true,
+         ));
+   $form->add_row( array(
+         'DESCRIPTION', 'Bool-Column D',
+         'FILTER',      $filter, 2,
+         'BR',
+         'FILTERERROR', $filter, 2, $FERR1, $FERR2."<BR>", true,
+         ));
+   $form->add_row( array(
+         'TAB',
+         'CELL',        1, 'align=left',
+         'OWNHTML',     implode( '', $filter->get_submit_elements() ) ));
+
+   // build SQL-query (for user-table)
+   $q2 = $qsql->clone();
+   $q2->merge( $filter->get_query(GETFILTER_ALL) );
+   $q2->merge( $table->get_query() );
+
+   // build table
+   foreach( $ARR_ANIMALS as $id => $animal )
+   {
+      $arr_row = array();
+      if( $table->Is_Column_Displayed[1] )
+         $arr_row[1] = "<td>$id</td>";
+      if( $table->Is_Column_Displayed[2] )
+         $arr_row[2] = "<td>$animal</td>";
+      $table->add_row( $arr_row );
+   }
+
+   // print static-filter, table
+   echo $form->print_start_default()
+      . $form->get_form_string() // static form
+      . $table->make_table()
+      . $form->print_end();
+   if ( $DEBUG_SQL ) echo "<br>QUERY: " . make_html_safe($q2->get_select()) . "<br>\n";
+}
+
+
+
+/* -------------------- (3) Table + Filters --------------------------------------  */
+// other example: see waiting_room.php, users.php, show_games.php
+
+if ( $fdemo == 3 )
+{
+   echo "<hr size=1>\n";
+   echo "<h3>Example #3 - Table with Filter</h3>\n";
+
+   // table filters
+   $tfilter = new SearchFilter('t3');
+   $tfilter->add_filter( 1, 'RatedSelect', 'Rated', true,
+         array( FC_STATIC => 1, FC_ADD_HAVING => 1 ));
+   $tfilter->add_filter( 2, 'Boolean', 'ActiveLevel>10', true,
+         array( FC_LABEL => 'Activity', FC_STATIC => 1 ));
+   $tfilter->init();
+
+   $table = new Table( 'table3', $page, '' );
+   $table->use_show_rows( false );
+   $table->register_filter( $tfilter );
+   $table->add_or_del_column();
+   $table->add_external_parameters( $rq, true ); // add fdemo-var
+
+   // add_tablehead($nr, $descr, $sort=NULL, $desc_def=false, $undeletable=false, $attbs=NULL)
+   $table->add_tablehead( 1, 'ID', 'ID', false, true);
+   $table->add_tablehead( 2, 'Animal', 'Animal');
+
+   // build SQL-query (for user-table)
+   $q3 = $qsql->clone();
+   $q3->merge( $table->get_query() );
+
+   // build table
+   foreach( $ARR_ANIMALS as $id => $animal )
+   {
+      $arr_row = array();
+      if( $table->Is_Column_Displayed[1] )
+         $arr_row[1] = "<td>$id</td>";
+      if( $table->Is_Column_Displayed[2] )
+         $arr_row[2] = "<td>$animal</td>";
+      $table->add_row( $arr_row );
+   }
+
+   $table->echo_table();
+   if ( $DEBUG_SQL ) echo "<br>QUERY: " . make_html_safe($q3->get_select()) . "<br>\n";
+}
+
+echo "</body></html>\n";
+
+?>
