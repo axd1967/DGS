3a0f6640bfda6d15c5281cf61fb5181db26b9684
Rod Ival <rodival@users.sf.net>
2007-07-08 20:58:25 +0000

    limitation of the cells size in an other way that cut_string()

--- edit_bio.php
+++ edit_bio.php
@@ -120,7 +120,11 @@ function find_category_box_text($cat)
          }
          else
          {
-            $text = cut_string( $row['Text'], 200 );
+            //$text = make_html_safe($row["Text"], false);
+            $text = textarea_safe($row["Text"]);
+            $text = "<TEXTAREA disabled name=\"dtext".$row["ID"] //readonly disabled
+               . "\" cols=\"$text_width\" rows=\"$text_height\">$text</TEXTAREA>";
+
             $bio_table->add_sinfo(
                '<div>'
                      . make_html_safe($cat != '' ? $categories[$cat]
@@ -136,7 +140,7 @@ function find_category_box_text($cat)
                            , T_("Move up"))
                      . "\n</div>"
                //don't use add_info() to avoid the INFO_HTML here:
-               ,make_html_safe($text, true)
+               ,$text
             );
          }
       } //while($row)
@@ -188,14 +192,4 @@ function find_category_box_text($cat)
 
    end_page(@$menu_array);
 }
-
-// try to cut after potential tag, but the xml could be broken (cutting off end-tags)
-function cut_string( $str, $len )
-{
-   $r = preg_replace( "/^(.*?)<[^<>]*?$/", "\\1", substr( $str, 0, $len) );
-   if ( strlen($str) > $len )
-      $r .= ' [...]';
-   return $r;
-}
-
 ?>
