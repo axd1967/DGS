cc56030f83eaca09c44d8ef62313abb5bf967f4d
Jens-Uwe Gaspar <juga@users.sf.net>
2007-07-18 19:07:45 +0000

    removed todo to save filter (for now)

--- include/filter.php
+++ include/filter.php
@@ -361,8 +361,6 @@ class SearchFilter
    /*! \brief Parses vars from _REQUEST to update activity-state of filters (used for Table). */
    function add_or_del_filter()
    {
-      //! \todo global $player_row;
-
       $act = (string) $this->get_arg(FFORM_TOGGLE_ACTION, false); // set if show-/hide-action needed
       $fid = (string) $this->get_arg(FFORM_TOGGLE_FID, false); // if act set: >0=Nr for thead[Nr], 0=hide-all, -1=show-all
       if ( $act and (string)$fid != '' )
@@ -381,8 +379,6 @@ class SearchFilter
          else // fname=0 (hide all)
             $this->setall_active(false);
       }
-
-      //! \todo enter code here to update filter-showing in DB (using player_row)
    }
 
    /*! \brief Returns reference of specified filter or null if no filter defined for id. */
