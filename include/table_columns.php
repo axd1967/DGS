46c2f3f09c77a716f394e16ee6cbf353aa87093a
Rod Ival <rodival@users.sf.net>
2007-07-29 21:44:09 +0000

    CSS adjustment

--- include/table_columns.php
+++ include/table_columns.php
@@ -493,7 +493,7 @@ class Table
       //dot.gif is a 1x1 transparent image.
       if( !is_numeric($width) )
          $width = $button_width;
-      return "<img class=MinWidth src='{$base_path}images/dot.gif' width=$width height=1 alt=''><br>";
+      return "<img class=MinWidth src='{$base_path}images/dot.gif' width=$width height=1 alt=''>";
    }
 
    /*!
