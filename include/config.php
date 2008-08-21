0ebf8cb86f9f101f6b7f03dbe13657ef16b29203
Jens-Uwe Gaspar <juga@users.sf.net>
2008-08-21 00:40:46 +0000

    - replaced some unchanged vars with constant defines (can remove global-ref):   DB_NAME, EMAIL_FROM, FRIENDLY_LONG_NAME/SHORT_NAME, HOSTBASE/NAME,   MYSQLHOST/USER/PASSWORD, SUB_PATH

--- include/config.php
+++ include/config.php
@@ -3,23 +3,23 @@
 // Friendly names.
 
 //Long name: Keep it AscII
-$FRIENDLY_LONG_NAME = "Dragon Go Server";
+define('FRIENDLY_LONG_NAME', 'Dragon Go Server');
 //Short name: Begining with a letter then only letters or numbers
-$FRIENDLY_SHORT_NAME = "DGS";
+define('FRIENDLY_SHORT_NAME', 'DGS');
 
 
 // This is the main url. If the main page is, e.g.,
 // http://www.some_domain.com/~my_dir/index.php
 // set $HOSTBASE = "http://www.some_domain.com/~my_dir/";
 
-$HOSTBASE = "http://localhost/";
+define('HOSTBASE', 'http://localhost/');
 
 // This is the server name. If the main page is, e.g.,
 // http://www.some_domain.com/~my_dir/index.php
 // http://www.some_domain.com/index.php
 // set $HOSTNAME = "www.some_domain.com";
 
-$HOSTNAME = "localhost";
+define('HOSTNAME', 'localhost');
 
 // This is used for cookies. If the main page is, e.g.,
 // http://www.some_domain.com/~my_dir/index.php
@@ -27,22 +27,22 @@ $HOSTNAME = "localhost";
 // http://127.0.0.1/~my_dir/index.php
 // set $SUB_PATH = "/~my_dir/";
 
-$SUB_PATH = "/";
+define('SUB_PATH', '/');
 
 // Then you should have: $HOSTBASE = "http://" . $HOSTNAME . $SUB_PATH;
 
 
 // From address for notification emails
-$EMAIL_FROM = "noreply@localhost";
+define('EMAIL_FROM', 'noreply@localhost');
 
 
 // These four are used to connect to mysql, with a command corresponding to
 // mysql -u$MYSQLUSER -p$MYSQLPASSWORD -h$MYSQLHOST -D$DB_NAME
 
-$MYSQLHOST = "mysql";
-$MYSQLUSER = "dragongoserver";
-$MYSQLPASSWORD = "";
-$DB_NAME = "dragongoserver";
+define('MYSQLHOST', 'mysql');
+define('MYSQLUSER', 'dragongoserver');
+define('MYSQLPASSWORD', '');
+define('DB_NAME', 'dragongoserver');
 
 
 // Dependent of your mysql version:
