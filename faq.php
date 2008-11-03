30c4c05d63c51663d52ffbc8082ce91a015588a3
Jens-Uwe Gaspar <juga@users.sf.net>
2008-11-03 03:37:35 +0000

    added search on FAQ:   matching words-regex on texts exported in translations-dir for user-specific   language or FAQ-original-english

--- faq.php
+++ faq.php
@@ -21,6 +21,7 @@ $TranslateGroups[] = "FAQ";
 
 require_once( "include/std_functions.php" );
 require_once( "include/faq_functions.php" );
+require_once( "include/form_functions.php" );
 
 {
    connect2mysql();
@@ -30,25 +31,134 @@ require_once( "include/faq_functions.php" );
    start_page(T_("FAQ"), true, $logged_in, $player_row );
    $menu_array = array();
 
-   //$faqhide = "AND entry.Hidden='N' AND (entry.Level=1 OR parent.Hidden='N') ";
-   $faqhide = " AND entry.Hidden='N' AND parent.Hidden='N'"; //need a viewable root
-
    echo "<table class=FAQ><tr><td>\n";
    echo "<h3 class=Header align=left><a name=\"general\">" .
          T_('Frequently Asked Questions') . "</a></h3>\n";
 
+   // init vars
+   $faq_url = 'faq.php?';
+   //$faqhide = "AND entry.Hidden='N' AND (entry.Level=1 OR parent.Hidden='N') ";
+   $faqhide = " AND entry.Hidden='N' AND parent.Hidden='N'"; //need a viewable root
+
+   $arr_languages = get_language_descriptions_translated();
+   $lang = get_request_arg('lang', 0);
+   if ( !$lang ) $lang = recover_language( $player_row );
+
+   // parse search terms
+   $is_search = @$_REQUEST['search'];
+   $qterm = get_request_arg('qterm', '');
+   $orig = ( get_request_arg('orig', '') ) ? 1 : 0;
+   $err_search = '';
+   $rx_term = '';
+   if( $is_search )
+   {
+      $err_search = check_faq_search_terms( $qterm );
+      $rx_term = build_regex_term( $qterm );
+      if( (string)$err_search != '' || (string)$rx_term == '' )
+         $is_search = false;
+   }
+
+   // search form
+   //TODO align form-content left-aligned, input-elems-titles would be NTH
+   $faq_form = new Form( 'faq', $faq_url, FORM_GET );
+   $faq_form->add_row( array(
+      'DESCRIPTION',    T_('Search Terms#FAQ'),
+      'TEXTINPUT',      'qterm', 30, -1, $qterm,
+      'SUBMITBUTTONX',  'search', T_('Search'),
+                        array( 'accesskey' => ACCKEY_ACT_FILT_SEARCH ),
+      'TEXT',           ' ' . sprintf( T_('using language (%s)#FAQ'), $arr_languages[$lang] ),
+      ));
+   if( $err_search )
+      $faq_form->add_row( array( 'TAB', 'TEXT', "<span class=ErrMsg>($err_search)</span>" ));
+   $faq_form->add_row( array(
+      'TAB',
+      'CHECKBOX', 'orig', 1, T_('search only in original (english) FAQ'), $orig ));
+   echo "</td></tr><tr><td>\n";
+   $faq_form->echo_string(1);
 
    $cat = @$_GET['cat'];
    if( $cat !== 'all' && !is_numeric($cat) ) $cat = 0;
-   if( @$_GET["read"] == 't' )
-   { //expand answers
+
+   if( $is_search )
+   { // show matching faq-entries
+      // read whole FAQ from DB, then match on file-read translations
       $result = mysql_query(
          "SELECT entry.*, parent.SortOrder AS ParentOrder, " .
          "Question.Text AS Q, Answer.Text AS A, " .
          "IF(entry.Level=1,entry.SortOrder,parent.SortOrder) AS CatOrder " .
          "FROM (FAQ AS entry, FAQ AS parent, TranslationTexts AS Question) " .
          "LEFT JOIN TranslationTexts AS Answer ON Answer.ID=entry.Answer " .
-         "WHERE parent.ID=entry.Parent$faqhide AND Question.ID=entry.Question " .
+         "WHERE parent.ID=entry.Parent $faqhide AND Question.ID=entry.Question " .
+         "ORDER BY CatOrder,ParentOrder,entry.SortOrder")
+      or error('mysql_query_failed', 'faq.search_entries');
+
+      $found_entries = 0;
+      if( mysql_num_rows($result) > 0 )
+      {
+         echo "</td></tr><tr><td class=FAQsearch>\n";
+
+         echo faq_item_html( 0);
+         $outbuf = '';
+         $cntmatch = 0; // count of matches for level1-section
+         while( $row = mysql_fetch_assoc( $result ) )
+         { //expand answers
+            $level = $row['Level'];
+            if( $orig )
+            { // no language-translation (use orig-texts from DB)
+               $question = $row['Q'];
+               $answer   = $row['A'];
+            }
+            else
+            { // use user-language-specific translation
+               $question = T_( $row['Q'] );
+               $answer   = T_( $row['A'] );
+            }
+
+            $match = search_faq_match_terms( $question, $answer, $rx_term );
+            if( $level == 1 || $match )
+            {
+               $a_attb = ( $level == 1 )
+                  ? "href=\"faq.php#Title{$row['ID']}\""
+                  : "name=\"Entry{$row['ID']}\"";
+               $faqtext = faq_item_html( $level, $question, $answer, $a_attb, $rx_term );
+            }
+            else
+               $faqtext = '';
+
+            if( $level == 1 )
+            {
+               if( $cntmatch > 0 )
+                  echo $outbuf;
+               $cntmatch = 0;
+               $outbuf = $faqtext;
+            }
+            else if( (string)$faqtext != '' )
+            {
+               $outbuf .= $faqtext;
+               $cntmatch++;
+               $found_entries++;
+            }
+         }
+         if( $cntmatch > 0 )
+            echo $outbuf;
+         echo faq_item_html(-1);
+      }
+      mysql_free_result($result);
+
+      if( $found_entries == 0 )
+         echo '<strong>' . T_('No matching entries found in FAQ.') . "</strong>\n";
+
+      $menu_array[T_('Go back to the FAQ index')]= "faq.php";
+   }
+   elseif( @$_GET["read"] == 't' )
+   { // show one or all faq-section(s) with all entries expanded
+      $result = mysql_query(
+         "SELECT entry.*, parent.SortOrder AS ParentOrder, " .
+         "Question.Text AS Q, Answer.Text AS A, " .
+         "IF(entry.Level=1,entry.SortOrder,parent.SortOrder) AS CatOrder " .
+         "FROM (FAQ AS entry, FAQ AS parent, TranslationTexts AS Question) " .
+         "LEFT JOIN TranslationTexts AS Answer ON Answer.ID=entry.Answer " .
+         "WHERE parent.ID=entry.Parent $faqhide AND Question.ID=entry.Question " .
          ( $cat === 'all' ? '' : "AND (entry.ID=$cat OR entry.Parent=$cat) " ) .
          "ORDER BY CatOrder,ParentOrder,entry.SortOrder")
       or error('mysql_query_failed', 'faq.find_entries');
@@ -76,7 +186,7 @@ require_once( "include/faq_functions.php" );
       $menu_array[T_('Go back to the FAQ index')]= "faq.php";
    }
    else
-   { //titles only
+   { // show only faq-titles
       $result = mysql_query(
          "SELECT entry.*, Question.Text AS Q, " .
          "IF(entry.Level=1,entry.SortOrder,parent.SortOrder) AS CatOrder " .
