9f8d62d52b3229f3978447a2b246ba24a9232eb0
Jens-Uwe Gaspar <juga@users.sf.net>
2009-01-03 02:01:58 +0000

    CVS-MERGE 1.0.14: added snapshot for DGS-release 1.0.14, changed texts of previous snapshots

--- snapshot.php
+++ snapshot.php
@@ -34,7 +34,11 @@ $ThePage = new Page('Docs');
 
    add_link_page_link('snapshot/DragonGoServer-cvs.tar.gz', 'DragonGoServer-cvs.tar.gz', T_('The latest version of the source code, directly from the cvs'));
 
-   add_link_page_link('snapshot/DragonGoServer-stable-200712.tar.gz', 'DragonGoServer-stable-200712.tar.gz', T_('The code this server is running'));
+   add_link_page_link('snapshot/DragonGoServer-stable-200812.tar.gz', 'DragonGoServer-stable-200812.tar.gz', T_('The code this server is running (version 1.0.14)'));
+
+   add_link_page_link('snapshot/DragonGoServer-stable-200712.tar.gz', 'DragonGoServer-stable-200712.tar.gz', T_('The previous version 1.0.13'));
+
+   add_link_page_link('snapshot/DragonGoServer-stable-200608.tar.gz', 'DragonGoServer-stable-200608.tar.gz', T_('The version 1.0.12'));
 
    add_link_page_link('snapshot/DragonGoServer-stable-200608.tar.gz', 'DragonGoServer-stable-200608.tar.gz', T_('The previous version'));
 
