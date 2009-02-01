61be8730967d377c70885ff7c2bf532af75e1bae
Jens-Uwe Gaspar <juga@users.sf.net>
2009-02-01 16:39:49 +0000

    - moved DGS-unrelated functions to separate include-file utilities.php + added more doc:   mod, modf, swap, array_combine, array_value_to_key_and_value, map_to_string, array_bsearch, limit - changed colors for activity-levels: arranged activity-stars like traffic-light, keeping green as highest activity-level - deprecated nsq_addslashes()

--- include/std_functions.php
+++ include/std_functions.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Dragon Go Server
-Copyright (C) 2001-2007  Erik Ouchterlony, Rod Ival
+Copyright (C) 2001-2009  Erik Ouchterlony, Rod Ival, Jens-Uwe Gaspar
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 $TranslateGroups[] = "Common";
 
 require_once( "include/quick_common.php" );
+require_once( 'include/utilities.php' );
 
 require_once( "include/time_functions.php" );
 if( !isset($page_microtime) )
@@ -267,115 +268,6 @@ define('FORUMFLAG_THREAD_SHOWAUTHOR', 0x02);
 define('FORUMFLAG_POSTVIEW_AUTOREAD', 0x04); // viewing marks thread as read //TODO (not implemented yet)
 define('FORUMFLAG_POSTVIEW_OVERVIEW', 0x08); // show overview
 //-----
-
-
-/****
- * get rid of the negative results of ($k % $m)
- * ( remember: ((-$k) % $m) == -($k % $m) )
- * return a int within [0..(int)abs($m)[
- *
- * if used with float, the truncation is toward zero,
- *  (( i.e. in the same way that the PHP (int)$x does:
- *     (int)$x := ( $x < 0 ? ceil($x) : floor($x) )
- *     ceil($x) := -floor(-$x)
- *  ))
- * with $m integer, an other way is to use (int)modf($k,$m):
- *  $k   abs($m)  mod($k,$m) (int)modf($k,$m)  modf($k,$m)
- *  3.0     3         0            0              0.0
- *  2.5     3         2            2              2.5
- *  2.0     3         2            2              2.0
- *  1.5     3         1            1              1.5
- *  1.0     3         1            1              1.0
- *  0.5     3         0            0              0.5
- *  0.0     3         0            0              0.0
- * -0.5     3         0     <>     2              2.5
- * -1.0     3         2            2              2.0
- * -1.5     3         2     <>     1              1.5
- * -2.0     3         1            1              1.0
- * -2.5     3         1     <>     0              0.5
- * -3.0     3         0            0              0.0
- ****/
-//equ: function mod( $k, $m) { return ($k % $m + $m) % $m; }
-function mod( $k, $m)
-{
-   $m= (int)( $m < 0 ? -$m : $m);
-   $k= $k % $m; return ( $k < 0 ? $k+$m : $k);
-}
-
-
-//get rid of the negative results of fmod($k % $m)
-//return a float within [0..abs($m)[
-function modf( $k, $m)
-{
-   if( $m < 0 ) $m = -$m;
-   return $k - $m * floor($k/$m);
-}
-
-function swap(&$a, &$b)
-{
-   $tmp = $a;
-   $a = $b;
-   $b = $tmp;
-}
-
-if( !function_exists('array_combine') ) //exists in PHP5
-{
-   function array_combine($keys, $values)
-   {
-      $res = array();
-      while( (list(, $k)=each( $keys))
-          && (list(, $v)=each( $values))
-         )
-      {
-         $res[$k]= $v;
-      }
-      return $res;
-   }
-}
-
-function array_value_to_key_and_value( $array )
-{
-  $new_array = array();
-  foreach( $array as $value )
-    $new_array[$value] = $value;
-
-  return $new_array;
-}
-
-// returns string-representation of flat map: "key=[val], ..."
-function map_to_string( $map, $sep = ', ' )
-{
-   if( !is_array($map) )
-      return '';
-
-   $arr = array();
-   foreach( $map as $key => $val )
-      $arr[]= "$key=[$val]";
-
-   return implode( $sep, $arr );
-}
-
-/**
- * Quick search in a sorted array. $haystack must be sorted.
- * will return:
- *  the index where $needle was found (the lower doublon if duplicates).
- *  the index of the next higher if not found (where it must be inserted).
- *  count($haystack) if no higher ($needle must be appended).
- **/
-function array_bsearch($needle, &$haystack)
-{
-   $h= count($haystack);
-   $l= 0;
-   while( $h > $l )
-   {
-      $p= ($h+$l)>>1;
-      if( $needle > $haystack[$p] )
-         $l= $p+1;
-      else
-         $h= $p;
-   }
-   return $h;
-} //array_bsearch
 
 
 function start_html( $title, $no_cache, $skinname=NULL, $style_string=NULL, $last_modified_stamp=NULL )
@@ -2533,11 +2425,12 @@ function add_link_page_link( $link=false, $linkdesc='', $extra='', $active=true)
 
 function activity_string( $act_lvl)
 {
+   // arrange as a traffic-light
    switch( (int)$act_lvl )
    {
-      case 1: $img= 'star2.gif'; break;
-      case 2: $img= 'star.gif'; break;
-      case 3: $img= 'star3.gif'; break;
+      case 1: $img= 'star3.gif'; break; // red
+      case 2: $img= 'star2.gif'; break; // orange
+      case 3: $img= 'star.gif'; break;  // green
       default: return '&nbsp;';
    }
    global $base_path;
@@ -2546,6 +2439,7 @@ function activity_string( $act_lvl)
 }
 
 
+// @deprecated: not used -> TODO remove
 function nsq_addslashes( $str )
 {
   return str_replace( array( "\\", "\"", "\$" ), array( "\\\\", "\\\"", "\\\$" ), $str );
@@ -2826,7 +2720,7 @@ function not_in_clause( $arr_enum )
 
 function RGBA($r, $g, $b, $a=NULL)
 {
-   if( $a === NULL )
+   if( is_null($a) )
       return sprintf("%02x%02x%02x", $r, $g, $b);
    else
       return sprintf("%02x%02x%02x%02x", $r, $g, $b, $a);
@@ -2868,33 +2762,6 @@ function blend_warning_cell_attb( $title='', $bgcolor='f7f5e3', $col='ff000033')
    if( $title )
       $str.= ' title="' . $title . '"';
    return $str;
-}
-
-function limit($val, $minimum, $maximum, $default)
-{
-   if( is_string( $val) )
-   {
-      $val = trim( $val);
-      if( strlen( $val) > 1 )
-      {
-         if( substr($val,-1) == '%'
-               && is_numeric($minimum)
-               && is_numeric($maximum)
-               )
-            $val = ($maximum-$minimum)*(substr($val,0,-1)/100.) + $minimum;
-         elseif( is_numeric(strpos('hHxX#$',$val[0])) )
-            $val = base_convert( substr($val,1), 16, 10);
-      }
-   }
-
-   if( !is_numeric($val) )
-      return (isset($default) ? $default : $val );
-   elseif( is_numeric($minimum) && $val < $minimum )
-      return $minimum;
-   elseif( is_numeric($maximum) && $val > $maximum )
-      return $maximum;
-
-   return $val;
 }
 
 function attb_quote( $str)
