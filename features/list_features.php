434da73ea460b683b829015c114a84922726de71
Jens-Uwe Gaspar <juga@users.sf.net>
2009-02-13 01:17:37 +0000

    - re-organized & sync'ed table-column-IDs for feature-/vote-list - layout: show own user-vote as centered number

--- features/list_features.php
+++ features/list_features.php
@@ -38,9 +38,6 @@ require_once( "features/lib_votes.php" );
       error('not_logged_in');
 
    $my_id = (int)@$player_row['ID'];
-   //TODO: check that guests can view features & votes
-   //if( $my_id <= GUESTS_ID_MAX )
-      //error('not_allowed_for_guest');
 
    $is_super_admin = Feature::is_super_admin();
    $user_vote_reason = Feature::allow_vote_check(); //TODO
@@ -58,15 +55,15 @@ require_once( "features/lib_votes.php" );
 
    // table filters
    $ffilter->add_filter( 1, 'Numeric', 'FL.ID', true, array( FC_SIZE => 8 ));
-   $ffilter->add_filter( 2, 'Selection',     # filter on status
+   $ffilter->add_filter( 3, 'Selection',     # filter on status
          Feature::build_filter_selection_status('FL.Status'),
          true, array( FC_DEFAULT => 1 )); // def=0..
    $filter_subject =&
-      $ffilter->add_filter( 3, 'Text', 'FL.Subject', true,
+      $ffilter->add_filter( 4, 'Text', 'FL.Subject', true,
          array( FC_SIZE => 30, FC_SUBSTRING => 1, FC_START_WILD => STARTWILD_OPTMINCHARS ) );
    $ffilter->add_filter( 6, 'RelativeDate', 'FL.Lastchanged', true,
          array( FC_TIME_UNITS => FRDTU_ALL|FRDTU_ABS ));
-   $ffilter->add_filter( 7, 'Selection',     # filter on user-voted-state
+   $ffilter->add_filter( 8, 'Selection',     # filter on user-voted-state
          array( T_('All#filterfeat')      => '',
                 T_('Unvoted#filterfeat')  => "ISNULL(FV.fid)", // FC_DEFAULT
                 T_('Voted#filterfeat')    => "FV.fid>0",
@@ -80,15 +77,15 @@ require_once( "features/lib_votes.php" );
    $ftable->add_or_del_column();
 
    // add_tablehead($nr, $descr, $attbs=null, $mode=TABLE_NO_HIDE|TABLE_NO_SORT, $sortx='')
-   //TODO: re-org col-IDs
+   // NOTE: col-IDs in sync with list_votes.php
    $ftable->add_tablehead( 1, T_('Vote ID#header'),     'Button', TABLE_NO_HIDE, 'FL.ID+'); // static
-   $ftable->add_tablehead(33, T_(''),                   'Image', TABLE_NO_HIDE, '');
-   $ftable->add_tablehead( 2, T_('Status#header'),      'Enum', 0, 'FL.Status+');
-   $ftable->add_tablehead( 3, T_('Subject#header'),     '', 0, 'FL.Subject+');
-   $ftable->add_tablehead( 7, T_('My Vote#header'),     'Number', 0, 'FV.Points-');
-   $ftable->add_tablehead( 8, T_('Lastvoted#header'),   'Date', 0, 'FV.Lastchanged+');
+   $ftable->add_tablehead( 2, '',                       'Image', TABLE_NO_HIDE, ''); // edit (static)
+   $ftable->add_tablehead( 3, T_('Status#header'),      'Enum', 0, 'FL.Status+');
+   $ftable->add_tablehead( 4, T_('Subject#header'),     '', 0, 'FL.Subject+');
+   $ftable->add_tablehead( 8, T_('My Vote#header'),     'NumberC', 0, 'FV.Points-');
+   $ftable->add_tablehead( 9, T_('Lastvoted#header'),   'Date', 0, 'FV.Lastchanged+');
    if( $is_super_admin )
-      $ftable->add_tablehead( 4, T_('Editor#header'),   'User', 0, 'FL.Editor_ID+');
+      $ftable->add_tablehead( 7, T_('Editor#header'),   'User', 0, 'FL.Editor_ID+');
    $ftable->add_tablehead( 5, T_('Created#header'),     'Date', 0, 'FL.Created+');
    $ftable->add_tablehead( 6, T_('Lastchanged#header'), 'Date', 0, 'FL.Lastchanged+');
 
@@ -126,28 +123,28 @@ require_once( "features/lib_votes.php" );
          $frow_strings[1] = button_TD_anchor( $url, $ID,
                ( $allow_vote ? T_('Vote') : T_('View vote') ));
       }
-      if( $feature->allow_edit() && $ftable->Is_Column_Displayed[33] ) // Edit-Action
+      if( $feature->allow_edit() && $ftable->Is_Column_Displayed[2] ) // Edit-Action
       {
-         $frow_strings[33] = anchor( "edit_feature.php?fid=$ID",
+         $frow_strings[2] = anchor( "edit_feature.php?fid=$ID",
                image( "{$base_path}images/edit.gif", 'E'),
                T_('Edit feature'), 'class=ButIcon');
       }
-      if( $ftable->Is_Column_Displayed[2] )
-         $frow_strings[2] = $feature->status;
       if( $ftable->Is_Column_Displayed[3] )
-         $frow_strings[3] = make_html_safe( $feature->subject, false, $rx_term);
-      if( $is_super_admin && $ftable->Is_Column_Displayed[4] )
-         $frow_strings[4] = user_reference( REF_LINK, 1, '', $feature->editor );
+         $frow_strings[3] = $feature->status;
+      if( $ftable->Is_Column_Displayed[4] )
+         $frow_strings[4] = make_html_safe( $feature->subject, SUBJECT_HTML, $rx_term);
+      if( $is_super_admin && $ftable->Is_Column_Displayed[7] )
+         $frow_strings[7] = user_reference( REF_LINK, 1, '', $feature->editor );
       if( $ftable->Is_Column_Displayed[5] )
          $frow_strings[5] = ($feature->created > 0 ? date(DATEFMT_FEATLIST, $feature->created) : '' );
       if( $ftable->Is_Column_Displayed[6] )
          $frow_strings[6] = ($feature->lastchanged > 0 ? date(DATE_FMT2, $feature->lastchanged) : '' );
       if( !is_null($fvote) )
       {
-         if( $ftable->Is_Column_Displayed[7] )
-            $frow_strings[7] = $fvote->points;
          if( $ftable->Is_Column_Displayed[8] )
-            $frow_strings[8] = ($fvote->lastchanged > 0 ? date(DATE_FMT2, $fvote->lastchanged) : '' );
+            $frow_strings[8] = $fvote->points;
+         if( $ftable->Is_Column_Displayed[9] )
+            $frow_strings[9] = ($fvote->lastchanged > 0 ? date(DATE_FMT2, $fvote->lastchanged) : '' );
       }
 
       $ftable->add_row( $frow_strings );
