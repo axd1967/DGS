3338f6e81ecd8f94ab3632659ae3c7f9507a9433
Jens-Uwe Gaspar <juga@users.sf.net>
2009-03-01 18:21:49 +0000

    - added Tournament-list page - added table Tournament and classes to manage tournaments

---
+++ tournaments/include/tournament.php
@@ -0,0 +1,288 @@
+<?php
+/*
+Dragon Go Server
+Copyright (C) 2001-2009  Erik Ouchterlony, Jens-Uwe Gaspar
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+ /* Author: Jens-Uwe Gaspar */
+
+$TranslateGroups[] = 'Tournament';
+
+require_once( 'include/std_classes.php' );
+
+/*!
+ * \file tournament.php
+ *
+ * \brief Functions for handling tournaments: tables Tournament
+ */
+
+
+
+ /*!
+  * \class Tournament
+  *
+  * \brief Class to manage Tournament-table
+  *
+  * Examples:
+  *    $cfg->insert_default_config_board( $user_id );
+  *    $cfg = ConfigBoard::load_config_board( $user_id );
+  */
+
+define('TOURNEY_SCOPE_DRAGON',  'DRAGON');
+define('TOURNEY_SCOPE_PUBLIC',  'PUBLIC');
+define('TOURNEY_SCOPE_PRIVATE', 'PRIVATE');
+define('CHECK_TOURNEY_SCOPE', 'DRAGON|PUBLIC|PRIVATE');
+
+define('TOURNEY_TYPE_ROUND_ROBIN', 'ROUNDROBIN');
+define('CHECK_TOURNEY_TYPE', 'ROUNDROBIN');
+
+define('TOURNEY_STATUS_ADMIN',    'ADM');
+define('TOURNEY_STATUS_NEW',      'NEW');
+define('TOURNEY_STATUS_REGISTER', 'REG');
+define('TOURNEY_STATUS_PAIR',     'PAIR');
+define('TOURNEY_STATUS_PLAY',     'PLAY');
+define('TOURNEY_STATUS_CLOSED',   'CLOSED');
+define('CHECK_TOURNEY_STATUS', 'ADM|NEW|REG|PAIR|PLAY|CLOSED');
+
+// lazy-init in Tournament::get..Text()-funcs
+$ARR_GLOBALS_TOURNAMENT = array();
+
+class Tournament
+{
+   var $ID;
+   var $Scope;
+   var $Type;
+   var $Title;
+   var $Description;
+   var $Owner_ID;
+   var $Owner_Handle;
+   var $Status;
+   var $Created;
+   var $Lastchanged;
+   var $StartTime;
+   var $EndTime;
+
+   /*! \brief Constructs ConfigBoard-object with specified arguments. */
+   function Tournament( $id, $scope=TOURNEY_SCOPE_PUBLIC, $type=TOURNEY_TYPE_ROUND_ROBIN,
+                        $title='', $description='', $owner_id=0, $owner_handle='',
+                        $status=TOURNEY_STATUS_NEW, $created=0, $lastchanged=0,
+                        $starttime=0, $endtime=0 )
+   {
+      $this->ID = (int)$id;
+      $this->setScope( $scope );
+      $this->setType( $type );
+      $this->Title = $title;
+      $this->Description = $description;
+      $this->Owner_ID = (int)$owner_id;
+      $this->Owner_Handle = $owner_handle;
+      $this->setStatus( $status );
+      $this->Created = (int)$created;
+      $this->Lastchanged = (int)$lastchanged;
+      $this->StartTime = (int)$starttime;
+      $this->EndTime = (int)$endtime;
+   }
+
+   function setScope( $scope )
+   {
+      if( !preg_match( "/^(".CHECK_TOURNEY_SCOPE.")$/", $scope ) )
+         error('invalid_args', "Tournament.setScope($scope)");
+      $this->Scope = $scope;
+   }
+
+   function setType( $type )
+   {
+      if( !preg_match( "/^(".CHECK_TOURNEY_TYPE.")$/", $type ) )
+         error('invalid_args', "Tournament.setType($type)");
+      $this->Type = $type;
+   }
+
+   function setStatus( $status )
+   {
+      if( !preg_match( "/^(".CHECK_TOURNEY_STATUS.")$/", $status ) )
+         error('invalid_args', "Tournament.setStatus($status)");
+      $this->Status = $status;
+   }
+
+   /*!
+    * \brief Inserts Tournament-entry.
+    * \note sets Created=NOW, Lastchanged=NOW, StartTime=NOW
+    */
+   function insert()
+   {
+      global $NOW;
+      $this->Created = $NOW;
+      $this->Lastchanged = $NOW;
+      $this->StartTime = $NOW;
+
+      db_query( "Tournament::insert({$this->ID})",
+            "INSERT INTO Tournament SET "
+            . " Scope='{$this->Scope}'"
+            . ",Type='{$this->Type}'"
+            . ",Title='{$this->Title}'"
+            . ",Description='{$this->Description}'"
+            . ",Owner_ID='{$this->Owner_ID}'"
+            . ",Status='{$this->Status}'"
+            . ",Created=FROM_UNIXTIME({$this->Created})"
+            . ",Lastchanged=FROM_UNIXTIME({$this->Lastchanged})"
+            . ",StartTime=FROM_UNIXTIME({$this->StartTime})"
+            . ",EndTime=FROM_UNIXTIME({$this->EndTime})"
+         );
+   }
+
+   /*!
+    * \brief Updates Tournament-entry.
+    * \note sets Lastchanged=NOW
+    */
+   function update()
+   {
+      global $NOW;
+      $this->Lastchanged = $NOW;
+
+      db_query( "Tournament::update({$this->ID})",
+            "UPDATE Tournament SET "
+            . " Scope='{$this->Scope}'"
+            . ",Type='{$this->Type}'"
+            . ",Title='{$this->Title}'"
+            . ",Description='{$this->Description}'"
+            . ",Owner_ID='{$this->Owner_ID}'"
+            . ",Status='{$this->Status}'"
+            //. ",Created=FROM_UNIXTIME({$this->Created})"
+            . ",Lastchanged=FROM_UNIXTIME({$this->Lastchanged})"
+            . ",StartTime=FROM_UNIXTIME({$this->StartTime})"
+            . ",EndTime=FROM_UNIXTIME({$this->EndTime})"
+            . " WHERE ID='{$this->ID}' LIMIT 1"
+         );
+   }
+
+   // ------------ static functions ----------------------------
+
+   /*! \brief Returns Tournament-object created from specified (db-)row. */
+   function new_from_row( $row )
+   {
+      $tournament = new Tournament(
+            @$row['ID'],
+            @$row['Scope'],
+            @$row['Type'],
+            @$row['Title'],
+            @$row['Description'],
+            @$row['Owner_ID'],
+            @$row['X_OwnerHandle'],
+            @$row['Status'],
+            @$row['X_Created'],
+            @$row['X_Lastchanged'],
+            @$row['X_StartTime'],
+            @$row['X_EndTime']
+         );
+      return $tournament;
+   }
+
+   /*! \brief Returns enhanced (passed) ListIterator with Tournament-objects. */
+   function load_tournaments( $iterator )
+   {
+      $qsql = new QuerySQL();
+      $qsql->add_part( SQLP_FIELDS,
+         'T.*',
+         'UNIX_TIMESTAMP(T.Created) AS X_Created',
+         'UNIX_TIMESTAMP(T.Lastchanged) AS X_Lastchanged',
+         'UNIX_TIMESTAMP(T.StartTime) AS X_StartTime',
+         'UNIX_TIMESTAMP(T.EndTime) AS X_EndTime',
+         'Owner.Handle AS X_OwnerHandle' );
+      $qsql->add_part( SQLP_FROM,
+         'Tournament AS T',
+         'INNER JOIN Players AS Owner ON Owner.ID=T.Owner_ID' );
+
+      $iterator->setQuerySQL( $qsql );
+      $query = $iterator->buildQuery();
+      $result = db_query( "Tournament.load_tournaments", $query );
+      $iterator->setResultRows( mysql_num_rows($result) );
+
+      $iterator->clearItems();
+      while( $row = mysql_fetch_array( $result ) )
+      {
+         $tourney = Tournament::new_from_row( $row );
+         $iterator->addItem( $tourney );
+      }
+      mysql_free_result($result);
+
+      return $iterator;
+   }
+
+   /*! \brief Returns scope-text or all scope-texts (if arg=null). */
+   function getScopeText( $scope=null )
+   {
+      // lazy-init of texts
+      if( !isset($ARR_GLOBALS_TOURNAMENT['SCOPE']) )
+      {
+         $arr = array();
+         $arr[TOURNEY_SCOPE_DRAGON]  = T_('Dragon#T_scope');
+         $arr[TOURNEY_SCOPE_PUBLIC]  = T_('Public#T_scope');
+         $arr[TOURNEY_SCOPE_PRIVATE] = T_('Private#T_scope');
+         $ARR_GLOBALS_TOURNAMENT['SCOPE'] = $arr;
+      }
+
+      if( is_null($scope) )
+         return $ARR_GLOBALS_TOURNAMENT['SCOPE'];
+      if( !isset($ARR_GLOBALS_TOURNAMENT['SCOPE'][$scope]) )
+         error('invalid_args', "Tournament.getScopeText($scope)");
+      return $ARR_GLOBALS_TOURNAMENT['SCOPE'][$scope];
+   }
+
+   /*! \brief Returns type-text or all type-texts (if arg=null). */
+   function getTypeText( $type=null )
+   {
+      // lazy-init of texts
+      if( !isset($ARR_GLOBALS_TOURNAMENT['TYPE']) )
+      {
+         $arr = array();
+         $arr[TOURNEY_TYPE_ROUND_ROBIN] = T_('Round Robin#T_type');
+         $ARR_GLOBALS_TOURNAMENT['TYPE'] = $arr;
+      }
+
+      if( is_null($type) )
+         return $ARR_GLOBALS_TOURNAMENT['TYPE'];
+      if( !isset($ARR_GLOBALS_TOURNAMENT['TYPE'][$type]) )
+         error('invalid_args', "Tournament.getTypeText($type)");
+      return $ARR_GLOBALS_TOURNAMENT['TYPE'][$type];
+   }
+
+   /*! \brief Returns status-text or all status-texts (if arg=null). */
+   function getStatusText( $status=null )
+   {
+      // lazy-init of texts
+      if( !isset($ARR_GLOBALS_TOURNAMENT['STATUS']) )
+      {
+         global $player_row;
+         $arr = array();
+         if( @$player_row['admin_level'] & ADMIN_DEVELOPER )
+            $arr[TOURNEY_STATUS_ADMIN] = T_('Admin#T_status');
+         $arr[TOURNEY_STATUS_NEW]      = T_('New#T_status');
+         $arr[TOURNEY_STATUS_REGISTER] = T_('Register#T_status');
+         $arr[TOURNEY_STATUS_PAIR]     = T_('Pair#T_status');
+         $arr[TOURNEY_STATUS_PLAY]     = T_('Play#T_status');
+         $arr[TOURNEY_STATUS_CLOSED]   = T_('Closed#T_status');
+         $ARR_GLOBALS_TOURNAMENT['STATUS'] = $arr;
+      }
+
+      if( is_null($status) )
+         return $ARR_GLOBALS_TOURNAMENT['STATUS'];
+      if( !isset($ARR_GLOBALS_TOURNAMENT['STATUS'][$status]) )
+         error('invalid_args', "Tournament.getStatusText($status)");
+      return $ARR_GLOBALS_TOURNAMENT['STATUS'][$status];
+   }
+
+} // end of 'Tournament'
+
+?>
---
+++ tournaments/list.php
@@ -0,0 +1,142 @@
+<?php
+/*
+Dragon Go Server
+Copyright (C) 2001-2009  Erik Ouchterlony, Jens-Uwe Gaspar
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+$TranslateGroups[] = 'Tournament';
+
+chdir('..');
+require_once( 'include/std_functions.php' );
+require_once( 'include/std_classes.php' );
+require_once( 'include/table_columns.php' );
+require_once( 'include/filter.php' );
+require_once( 'include/classlib_profile.php' );
+require_once( 'include/classlib_userconfig.php' );
+require_once( 'tournaments/include/tournament.php' );
+
+{
+   #$DEBUG_SQL = true;
+   connect2mysql();
+
+   $logged_in = who_is_logged( $player_row);
+   if( !$logged_in )
+      error('not_logged_in');
+   $my_id = $player_row['ID'];
+   $cfg_tblcols = ConfigTableColumns::load_config( $my_id, CFGCOLS_TOURNAMENT_LIST );
+
+   $page = "list.php?";
+
+   // config for filters
+   $scope_filter_array = array( T_('All') => '' );
+   foreach( Tournament::getScopeText() as $scope => $text )
+      $scope_filter_array[$text] = "T.Scope='$scope'";
+
+   $type_filter_array = array( T_('All') => '' );
+   foreach( Tournament::getTypeText() as $type => $text )
+      $type_filter_array[$text] = "T.Type='$type'";
+
+   $status_filter_array = array( T_('All') => '' );
+   foreach( Tournament::getStatusText() as $status => $text )
+      $status_filter_array[$text] = "T.Status='$status'";
+
+   // init search profile
+   $search_profile = new SearchProfile( $my_id, PROFTYPE_FILTER_TOURNAMENT_LIST );
+   $tfilter = new SearchFilter( '', $search_profile );
+   $ttable = new Table( 'tournament', $page, $cfg_tblcols );
+   $ttable->set_profile_handler( $search_profile );
+   $search_profile->handle_action();
+
+   // table filters
+   $tfilter->add_filter( 1, 'Numeric', 'T.ID', true);
+   $tfilter->add_filter( 2, 'Selection', $scope_filter_array, true );
+   $tfilter->add_filter( 3, 'Selection', $type_filter_array, true );
+   $tfilter->add_filter( 4, 'Selection', $status_filter_array, true );
+   $tfilter->add_filter( 8, 'RelativeDate', 'T.StartTime', true,
+         array( FC_TIME_UNITS => FRDTU_YMWD|FRDTU_ABS ));
+   $tfilter->init();
+
+   // init table
+   $ttable->register_filter( $tfilter );
+   $ttable->add_or_del_column();
+
+   // add_tablehead($nr, $descr, $attbs=null, $mode=TABLE_NO_HIDE|TABLE_NO_SORT, $sortx='')
+   $ttable->add_tablehead( 1, T_('ID#headert'), 'Button', TABLE_NO_HIDE, 'ID-');
+   $ttable->add_tablehead( 2, T_('Scope#headert'), 'Enum', 0, 'Scope+');
+   $ttable->add_tablehead( 3, T_('Type#headert'), 'Enum', 0, 'Type+');
+   $ttable->add_tablehead( 4, T_('Status#headert'), 'Enum', 0, 'Status+');
+   $ttable->add_tablehead( 5, T_('Title#headert'), '', TABLE_NO_HIDE, 'Title+');
+   $ttable->add_tablehead( 6, T_('Owner#headert'), 'User', 0, 'X_OwnerHandle+');
+   $ttable->add_tablehead( 7, T_('Last changed#headert'), 'Date', 0, 'Lastchanged-');
+   $ttable->add_tablehead( 8, T_('Start time#headert'), 'Date', 0, 'StartTime+');
+   $ttable->add_tablehead( 9, T_('End time#headert'), 'Date', 0, 'EndTime+');
+
+   $ttable->set_default_sort( 1 ); //on ID
+
+   $iterator = new ListIterator( 'Tournaments',
+         $ttable->get_query(),
+         $ttable->current_order_string('ID-'),
+         $ttable->current_limit_string() );
+   $iterator = Tournament::load_tournaments( $iterator );
+
+
+   $title = T_('Tournament list');
+   start_page($title, true, $logged_in, $player_row,
+               button_style($player_row['Button']) );
+
+   if( $DEBUG_SQL ) echo "QUERY: " . make_html_safe( $iterator->Query );
+   echo "<h3 class=Header>". $title . "</h3>\n";
+
+
+   $show_rows = $ttable->compute_show_rows( $iterator->ResultRows );
+   while( ($show_rows-- > 0) && list(,$tourney) = $iterator->getListIterator() )
+   {
+      $ID = $tourney->ID;
+      $row_str = array();
+
+      if( $ttable->Is_Column_Displayed[ 1] )
+         $row_str[ 1] = button_TD_anchor( "view_tournament.php?tid=$ID", $ID );
+      if( $ttable->Is_Column_Displayed[ 2] )
+         $row_str[ 2] = Tournament::getScopeText( $tourney->Scope );
+      if( $ttable->Is_Column_Displayed[ 3] )
+         $row_str[ 3] = Tournament::getTypeText( $tourney->Type );
+      if( $ttable->Is_Column_Displayed[ 4] )
+         $row_str[ 4] = Tournament::getStatusText( $tourney->Status );
+      if( $ttable->Is_Column_Displayed[ 5] )
+         $row_str[ 5] = make_html_safe( $tourney->Title );
+      if( $ttable->Is_Column_Displayed[ 6] )
+         $row_str[ 6] = user_reference( REF_LINK, 1, '', $tourney->Owner_ID, $tourney->Owner_Handle, '' );
+      if( $ttable->Is_Column_Displayed[ 7] )
+         $row_str[ 7] = ($tourney->Lastchanged > 0) ? date(DATE_FMT2, $tourney->Lastchanged) : '';
+      if( $ttable->Is_Column_Displayed[ 8] )
+         $row_str[ 8] = ($tourney->StartTime > 0) ? date(DATE_FMT2, $tourney->StartTime) : '';
+      if( $ttable->Is_Column_Displayed[ 9] )
+         $row_str[ 9] = ($tourney->EndTime > 0) ? date(DATE_FMT2, $tourney->EndTime) : '';
+
+      $ttable->add_row( $row_str );
+   }
+
+   // print table
+   echo $ttable->make_table();
+
+
+   $menu_array = array();
+   $menu_array[T_('Create tournament')] = 'tournaments/edit_tournament.php';
+
+   end_page(@$menu_array);
+}
+
+?>
