3971de5f15bf23dda2d2041ee2720aaa7200e819
Jens-Uwe Gaspar <juga@users.sf.net>
2009-03-21 20:43:49 +0000

    - added tournament-admin ADMIN_TOURNAMENT - removed ADMIN_ADD_ADMIN, one admin-role SUPERADMIN is enough to manage admins (add-new and edit) - SUPERADMIN can't remove SUPERADMIN-flag for himself - cleanup (use newer db-query-funcs, added comments, simplified URL-args)

--- admin_admins.php
+++ admin_admins.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Dragon Go Server
-Copyright (C) 2001-2007  Erik Ouchterlony, Rod Ival
+Copyright (C) 2001-2009  Erik Ouchterlony, Rod Ival, Jens-Uwe Gaspar
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
@@ -23,119 +23,121 @@ require_once( "include/std_functions.php" );
 require_once( "include/form_functions.php" );
 require_once( "include/table_columns.php" );
 
+//TODO layouting
+
 {
   connect2mysql();
 
   $logged_in = who_is_logged( $player_row);
-
   if( !$logged_in )
     error('not_logged_in');
+  $my_id = $player_row['ID'];
 
+  // only SUPERADMIN can manage admins
   $player_level = (int)@$player_row['admin_level']; //local modifications
   if( !($player_level & ADMIN_SUPERADMIN) )
     error('adminlevel_too_low');
 
-  $admin_tasks = array(
-                        'AddAdm' => array( ADMIN_ADD_ADMIN, /*T_*/('New admin')),
+  $admin_tasks = array( // admin-level-id => arr( admin-bitmask, admin-text )
                         'ADMIN'  => array( ADMIN_SUPERADMIN, /*T_*/('Admins')),
                         'Passwd' => array( ADMIN_PASSWORD, /*T_*/('Password')),
                         'TRANS'  => array( ADMIN_TRANSLATORS, /*T_*/('Translators')),
                         'Forum'  => array( ADMIN_FORUM, /*T_*/('Forum')),
                         'FAQ'    => array( ADMIN_FAQ, /*T_*/('FAQ')),
                         'Skin'   => array( ADMIN_SKINNER, /*T_*/('Skin')),
+                        'TRNEY'  => array( ADMIN_TOURNAMENT, /*T_*/('Tournament')),
                         'Devel'  => array( ADMIN_DEVELOPER, /*T_*/('Developer')),
                         'Dbase'  => array( ADMIN_DATABASE, /*T_*/('Database')),
                       );
 
 // Make sure all previous admins gets into the Admin array
-  $result = mysql_query("SELECT ID, Adminlevel+0 AS admin_level FROM Players " .
-                        "WHERE Adminlevel != 0")
-     or error('mysql_query_failed','admin_admins.find_admins');
-
+  $result = db_query( 'admin_admins.find_admins',
+      "SELECT ID, Adminlevel+0 AS admin_level FROM Players WHERE Adminlevel > 0" );
    while( $row = mysql_fetch_array($result) )
    {
-      $Admin[$row['ID']] = ~$player_level & (int)$row['admin_level'] ;
-      $AdminOldLevel[$row['ID']] = (int)$row['admin_level'];
+      $uid = $row['ID'];
+      $AdminOldLevel[$uid] = (int)$row['admin_level'];
+      $Admin[$uid] = 0;
    }
    mysql_free_result($result);
 
+   // SUPERADMIN can't remove SUPERADMIN-flag for himself
+   $Admin[$my_id] |= ADMIN_SUPERADMIN;
 
-  if( @$_GET['update'] == 't' && @$_POST['action'] )
+
+/* Actual POST/REQUEST calls used:
+     (no args) | refresh=                     : show admin-users and admin-levels
+     update=&<aid>_<uid>=''|Y                 : update admins with changed admin-levels
+     update=&...&newadmin=handle&<aid>_new=.. : add new admin with uid='new'
+*/
+
+  if( @$_REQUEST['update'] )
   {
+     // update admin-levels? -> Admin[uid] = new admin-levels
      $Admin['new'] = 0;
      foreach( $_POST as $item => $value )
      {
         if( $value != 'Y' )
            continue;
 
-        list($type, $id) = explode('_', $item, 2);
+        // uid = Players.ID | 'new'
+        list($type, $uid) = explode('_', $item, 2);
+        $amask = (int)@$admin_tasks[$type][0];
+        if( $amask == 0 || ($uid !== 'new' && $uid <= GUESTS_ID_MAX) )
+           error('bad_data', "admin_admins.update($uid,$type,$amask)");
 
-        $val = $player_level & (int)$admin_tasks[$type][0];
-
-        if( !($id > 0 || $id==='new') || !$val)
-           error('bad_data');
-
-        $Admin[$id] |= $val;
+        $Admin[$uid] |= $amask;
      }
 
-     if( !($Admin[$player_row['ID']] & ADMIN_SUPERADMIN) )
-        error('admin_no_longer_admin_admin');
-
+     // add new admin?
      $newadmin= get_request_arg('newadmin');
      if( $Admin['new'] != 0 && !empty($newadmin))
      {
-        $result = mysql_query("SELECT ID,Adminlevel+0 AS admin_level FROM Players " .
-                  "WHERE Handle='".mysql_addslashes($newadmin)."' LIMIT 1")
-           or error('mysql_query_failed','admin_admins.find_new_admin');
-
-        if( @mysql_num_rows($result) != 1 )
-           error('unknown_user');
-
-        $row = mysql_fetch_array($result);
-        mysql_free_result($result);
+        $row = mysql_fetch_row( "admin_admins.find_new_admin($newadmin)",
+            "SELECT ID,Adminlevel+0 AS admin_level FROM Players " .
+                  "WHERE Handle='".mysql_addslashes($newadmin)."' LIMIT 1" );
+        if( !$row )
+           error('unknown_user', "admin_admin.check.user($newadmin)");
         if( $row["admin_level"] != 0 )
            error('new_admin_already_admin');
 
-        $Admin[$row['ID']] = $Admin['new'];
-        $AdminOldLevel[$row['ID']] = 0;
+        $uid = $row['ID'];
+        $AdminOldLevel[$uid] = 0;
+        $Admin[$uid] = $Admin['new'];
      }
      unset($Admin['new']);
 
-     foreach( $Admin as $id => $adm_level )
+     // update admin-levels
+     foreach( $Admin as $uid => $adm_level )
      {
         $adm_level = (int)$adm_level;
-        //remove the capacity to add an admin if not already an ADMIN_SUPERADMIN
-        if( !($adm_level & ADMIN_SUPERADMIN) )
-           $adm_level &= ~ADMIN_ADD_ADMIN;
-        $Admin[$id] = $adm_level;
-
-        if( $adm_level != $AdminOldLevel[$id] )
+        if( $adm_level != $AdminOldLevel[$uid] )
         {
-           admin_log( @$player_row['ID'], @$player_row['Handle'], 
-                sprintf("grants %s from 0x%x to 0x%x.", (string)$id, $AdminOldLevel[$id], $adm_level) );
+           admin_log( $my_id, @$player_row['Handle'],
+                sprintf("grants %s from 0x%x to 0x%x.", (string)$uid, $AdminOldLevel[$uid], $adm_level) );
 
-           mysql_query("UPDATE Players SET Adminlevel=$adm_level WHERE ID=$id LIMIT 1")
-              or error('mysql_query_failed','admin_admins.update_admin');
+           db_query( "admin_admins.update_admin($my_id,$uid,$adm_level)",
+               "UPDATE Players SET Adminlevel=$adm_level WHERE ID=$uid LIMIT 1" );
         }
      }
 
-     $player_level = (int)$Admin[$player_row['ID']];
-  }
+     $player_level = (int)$Admin[$my_id];
+  } //update
 
-  $result = mysql_query("SELECT ID, Handle, Name, Adminlevel+0 AS admin_level FROM Players " .
-                        "WHERE Adminlevel != 0 ORDER BY ID")
-     or error('mysql_query_failed','admin_admins.find_admins2');
 
+   // reload admin-users with Handle,Name and changed admin-levels
+   $result = db_query( 'admin_admins.find_admins2',
+      "SELECT ID, Handle, Name, Adminlevel+0 AS admin_level FROM Players " .
+      "WHERE Adminlevel > 0 ORDER BY ID" );
 
    $atable = new Table( 'admin', '', '', '', TABLE_NO_SIZE );
 
 
    start_page(/*T_*/("Admin").' - './*T_*/('Edit admin staff'), true, $logged_in, $player_row );
-
    echo "<h3 class=Header>" . /*T_*/('Admins') . "</h3>\n";
 
 
-   $marked_form = new Form('admin','admin_admins.php?update=t', FORM_POST, true, 'FormTable');
+   $marked_form = new Form('admin','admin_admins.php', FORM_POST, true, 'FormTable');
    $marked_form->attach_table( $atable);
    $marked_form->set_tabindex(1);
 
@@ -152,8 +154,7 @@ require_once( "include/table_columns.php" );
    }
    $atable->set_default_sort( 1); //on ID
 
-   //can't add an admin if had not the privlege
-   $new_admin = ($player_level & ADMIN_ADD_ADMIN);
+   $new_admin = true;
    while( ($row=mysql_fetch_assoc( $result )) || $new_admin )
    {
       $arow_strings = array();
@@ -161,17 +162,17 @@ require_once( "include/table_columns.php" );
       $col = 3;
       if( is_array($row) )
       {
-         $id = $row['ID'];
+         $uid = $row['ID'];
          $level = $row['admin_level'];
-         $arow_strings[1]= "<A href=\"userinfo.php?uid=$id\">$id</A>";
-         $arow_strings[2]= "<A href=\"userinfo.php?uid=$id\">" . $row["Handle"] . "</A>";
-         $arow_strings[3]= "<A href=\"userinfo.php?uid=$id\">" .
+         $arow_strings[1]= "<A href=\"userinfo.php?uid=$uid\">$uid</A>";
+         $arow_strings[2]= "<A href=\"userinfo.php?uid=$uid\">" . $row["Handle"] . "</A>";
+         $arow_strings[3]= "<A href=\"userinfo.php?uid=$uid\">" .
             make_html_safe($row["Name"]) . "</A>";
       }
       else
       {
-         $new_admin = false;
-         $id = 'new';
+         $new_admin = false; // last-line built
+         $uid = 'new';
          $arow_strings[1]= array(
             'attbs' => array( 'colspan' => $col, 'class'=>'nowrap'),
             'text' => /*T_*/('New admin') . ": "
@@ -185,22 +186,16 @@ require_once( "include/table_columns.php" );
       {
          list( $amask, $aname) = $tmp;
 
-         if( $amask & $player_level )
-            $tmp = '';
-         else
-            $tmp = ' disabled';
-
-         //only ADMIN_SUPERADMIN can grant ADMIN_ADD_ADMIN
-         if( $amask == ADMIN_ADD_ADMIN
-            && !($player_level & ADMIN_SUPERADMIN) )
-            $tmp = ' disabled';
-
+         $attr = '';
+         // SUPERADMIN can't change SUPERADMIN-flag for himself
+         if( ($amask & $level & ADMIN_SUPERADMIN) && ($uid == $my_id) )
+            $attr .= ' disabled';
          if( $amask & $level )
-            $tmp.= ' checked';
+            $attr .= ' checked';
 
-         $tmp = "\n  <input type=\"checkbox\" name=\"${aid}_$id\" value=\"Y\"$tmp>";
-
-         $arow_strings[++$col]= $tmp;
+         $chkname = $aid . '_' . $uid;
+         $arow_strings[++$col] =
+            "\n  <input type=\"checkbox\" name=\"{$chkname}\" value=\"Y\"$attr>";
       }
 
       $atable->add_row( $arow_strings );
@@ -209,10 +204,10 @@ require_once( "include/table_columns.php" );
 
    $atable->echo_table();
 
-   echo $marked_form->print_insert_submit_buttonx( 'action',
-            /*T_*/('Update changes'), array( 'accesskey' => ACCKEY_ACT_EXECUTE ));
-
-   echo $marked_form->print_end();
+   echo $marked_form->print_insert_submit_buttonx( 'update',
+            /*T_*/('Update changes'), array( 'accesskey' => ACCKEY_ACT_EXECUTE )),
+        $marked_form->print_insert_submit_button( 'refresh', /*T_*/('Refresh')),
+        $marked_form->print_end();
 
    end_page();
 }
