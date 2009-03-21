c8cc05be43c2f13bceaf21ac6b47bacc6d9135fd
Jens-Uwe Gaspar <juga@users.sf.net>
2009-03-21 20:55:39 +0000

    - added check, if tournament-editing or adding tournament directors is allowed - added basic registration for tournament participants   - show number of completed registrations   - for admins & tournament directors show number of registrations on other status   - show a users registration status - added general information section common to all tournaments with contents and links to most of the user-related tournament pages - added own set with "bottom"-links for each page-section - cleaned up section with tournament-title and description

--- tournaments/view_tournament.php
+++ tournaments/view_tournament.php
@@ -24,6 +24,7 @@ require_once( 'include/std_functions.php' );
 require_once( 'include/gui_functions.php' );
 require_once( 'include/form_functions.php' );
 require_once( 'tournaments/include/tournament.php' );
+require_once( 'tournaments/include/tournament_participant.php' );
 
 $ThePage = new Page('Tournament');
 
@@ -42,54 +43,119 @@ $ThePage = new Page('Tournament');
    if( is_null($tourney) )
       error('unknown_tournament', "view_tournament.find_tournament($tid)");
 
+   $allow_edit_tourney = $tourney->allow_edit_tournaments( $my_id );
+
+   $page_tdirs   = "tournaments/list_directors.php?tid=$tid";
+   $page_tourney = "tournaments/view_tournament.php?tid=$tid";
+
 
    $page_title = sprintf( T_('Tournament #%s'), $tid );
    start_page( $page_title, true, $logged_in, $player_row );
 
-   $title =
-      sprintf( "[%s Tournament] - %s",
-         Tournament::getScopeText($tourney->Scope),
-         make_html_safe($tourney->Title, true) );
-   //echo "<h3 class=Header>" . $title . "</h3>\n";
-   section( 'info', $title ); //T_('Information#T_view') );//TODO
+   // --------------- Information -----------------------------------
 
+   $title = sprintf( T_('%1$s %2$s Tournament - General Information'),
+         Tournament::getScopeText($tourney->Scope),
+         Tournament::getTypeText($tourney->Type) );
+   section( 'info', $title );
    echo
-      make_html_safe($tourney->Description, true),
-      "<br>\n",
-      T_('Last changed date'), date(DATEFMT_TOURNAMENT, $tourney->Lastchanged),
-      "<br>\n",
-      T_('Start time'), date(DATEFMT_TOURNAMENT, $tourney->StartTime),
-      "<br>\n",
-      T_('End time'), date(DATEFMT_TOURNAMENT, $tourney->EndTime),
-      "<br>\n",
-      T_('Type'), Tournament::getTypeText($tourney->Type),
-      "<br>\n",
-      T_('Status'), Tournament::getStatusText($tourney->Status),
+      make_html_safe(
+         sprintf( T_('This page contains all necessary information and links to participate in the tournament. '
+                  . 'There are different sections with a <home %1$s>description of the tournament</home>, '
+                  . 'the used <home %2$s>rulesets</home>, <home %3$s>registration information</home> '
+                  . 'and the <home %4$s>tournament results</home>.' ),
+            $page_tourney.'#title',
+            $page_tourney.'#rules',
+            $page_tourney.'#registration',
+            $page_tourney.'#result' ), true ),
+      "<br><br>\n",
+      make_html_safe(
+         sprintf( T_('When you have a question about the tournament, please send '
+                  . 'a message to one of the <home %s>tournament directors</home> '
+                  . 'or ask in the <home forum/index.php>Tournaments forum</home>.'),
+            $page_tdirs ), true ),
       "\n";
 
+   // ------------- Section Menu
+
+   $sectmenu = array();
+   $sectmenu[T_('Tournament directors')] = $page_tdirs;
+   if( $tourney->allow_edit_tournaments($my_id) )
+      $sectmenu[T_('Manage this tournament')] =
+         array( 'url' => "tournaments/edit_tournament.php?tid=$tid", 'class' => 'TAdmin' );
+   make_menu( $sectmenu, false);
+
+   // --------------- Title ---------------------
+
+   echo
+      "<hr>\n", '<a name="title">', "\n",
+      "<h2 class=Header>" . make_html_safe($tourney->Title, true) . "</h2>\n",
+      make_html_safe($tourney->Description, true),
+      "\n";
+
+   // --------------- Rules -----------------------------------------
+
+   echo "<hr>\n", '<a name="rules">', "\n";
    section( 'tournament', T_('Rules#T_view') );
    echo
       "[TODO] Show Ruleset", //TODO
       "\n";
 
+   // --------------- Registration ----------------------------------
+
+   echo "<hr>\n", '<a name="registration">', "\n";
    section( 'tournament', T_('Registration#T_view') );
-   echo
-      "[TODO] Registration (TPs, register, invite, ask-TD)", //TODO
+   $cnt_status = ($allow_edit_tourney) ? NULL : TP_STATUS_REGISTER;
+   $tp_counts = TournamentParticipant::count_tournament_participants( $tid, $cnt_status );
+   $td_view = "<br>\n";
+   if( $allow_edit_tourney && @$tp_counts[TP_STATUS_REGISTER] != $tp_counts['*'])
+   {
+      $td_view .= "<ul>";
+      foreach( $tp_counts as $t_status => $cnt )
+         if( $t_status != '*' )
+            $td_view .= "  <li>" . sprintf( T_('Status [%s] : %d users'), $t_status, $cnt ) . "\n";
+      $td_view .= "</ul>\n";
+   }
+   $cnt_participants = (int)@$tp_counts[TP_STATUS_REGISTER];
+
+   $reg_user_status = TournamentParticipant::isTournamentParticipant( $tid, $my_id );
+   $reg_user_info = ( count($tourney->allow_register($my_id, true)) )
+      ? '' : TournamentParticipant::getStatusText( $reg_user_status, false, true );
+
+   echo "\n",
+      sprintf( T_('Complete registrations for this tournament: %s user(s)'), $cnt_participants ),
+      $td_view,
+      "<br>\n",
+      ( ($reg_user_info)
+            ? sprintf( '%s%s<span class="TUserStatus">%s</span>',
+                       T_('Your Registration status:'), SMALL_SPACING, $reg_user_info )
+            : '' ),
       "\n";
 
+   // ------------- Section Menu
+
+   $sectmenu = array();
+   $sectmenu[T_('Tournament participants')] = "tournaments/list_participants.php?tid=$tid";
+
+   $reg_user_str = TournamentParticipant::getLinkTextRegistration( $tourney, $my_id, $reg_user_status );
+   if( $reg_user_str )
+      $sectmenu[$reg_user_str] = "tournaments/register.php?tid=$tid"; # for user
+
+   if( $allow_edit_tourney )
+      $sectmenu[T_('Invite user')] =
+         array( 'url' => "tournaments/invite.php?tid=$tid", 'class' => 'TAdmin' ); # for TD
+
+   make_menu( $sectmenu, false);
+
+   // --------------- Results ---------------------------------------
+
+   echo "<hr>\n", '<a name="result">', "\n";
    section( 'tournament', T_('Results#T_view') );
    echo
       "[TODO] Results (Show Winners, Show intermediate results (link))", //TODO
       "\n";
 
 
-   $menu_array = array();
-   $menu_array[T_('Tournaments')] = "tournaments/list_tournaments.php";
-   $menu_array[T_('Tournament directors')] = "tournaments/list_directors.php?tid=$tid";
-   if( $tourney->allow_edit_tournaments($my_id) )
-      $menu_array[T_('Edit this tournament')] = "tournaments/edit_tournament.php?tid=$tid";
-
-   end_page(@$menu_array);
+   end_page();
 }
-
 ?>
