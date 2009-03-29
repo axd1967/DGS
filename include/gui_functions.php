59ffa95c1d53fdeb23a426afc3d97a51e670ec60
Jens-Uwe Gaspar <juga@users.sf.net>
2009-03-29 18:43:44 +0000

    echo_notes: title optional, pre-spacing

--- include/gui_functions.php
+++ include/gui_functions.php
@@ -102,21 +102,21 @@ function TD_button( $title, $href, $isrc, $ialt)
 }
 
 /*! \brief Prints notes in formatted table if there are notes. */
-function echo_notes( $table_id, $title, $notes )
+function echo_notes( $table_id, $title, $notes, $pre_sep=true )
 {
    if( !is_array($notes) || count($notes) == 0 )
       return;
 
-   echo "<br><br>\n",
+   echo ( $pre_sep ? "<br><br>\n" : '' ),
       "<table id=\"{$table_id}\">\n",
-      "<tr><th>" . make_html_safe($title, 'line') . "</th></tr>\n",
+      ( $title != '' ? "<tr><th>" . make_html_safe($title, 'line') . "</th></tr>\n" : '' ),
       "<tr><td><ul>";
    foreach( $notes as $note )
    {
       if( is_null($note) || (string)$note === '' )
          echo "<p></p>\n";
       else
-         echo "  <li>" . make_htmL_safe($note, 'line') . "\n";
+         echo '  <li>' . make_html_safe($note, 'line') . "\n";
    }
    echo "</ul>\n",
       "</td></tr></table>\n";
