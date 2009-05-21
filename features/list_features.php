5b5b18e91f424b572b00a06a72b3bdd0e4e82c3e
Jens-Uwe Gaspar <juga@users.sf.net>
2009-05-21 20:01:47 +0000

    renamed title: Feature list -> Features to vote on

--- features/list_features.php
+++ features/list_features.php
@@ -102,7 +102,7 @@ require_once( "features/lib_votes.php" );
 
    $show_rows = $ftable->compute_show_rows(mysql_num_rows($result));
 
-   $title = T_('Feature list');
+   $title = T_('Features to vote on');
    start_page( $title, true, $logged_in, $player_row,
                button_style($player_row['Button']) );
    if( $DEBUG_SQL ) echo "QUERY: " . make_html_safe($query);
