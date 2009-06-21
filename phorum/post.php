39525e181d2ef7dede743a068062b8a50f1bafaa
Jens-Uwe Gaspar <juga@users.sf.net>
2009-06-21 20:16:34 +0000

    - added page to avoid redirection because of missing page and writing of an Errorlog-row: [htaccess 404: /phorum/post.php]   Requesting 'post.php' looks like a "spam"-attack, but it consumes server-resources, so now "catched" it with existing page marking it as "old page".

---
+++ phorum/post.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<HTML>
+<HEAD>
+ <TITLE>old page</TITLE>
+</HEAD>
+<BODY>
+
+<b>This is an old page no longer available on the Dragon Go Server.</b>
+
+</BODY>
+</HTML>
