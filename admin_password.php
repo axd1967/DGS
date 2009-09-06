51e00d253a01e35b527b02cbf69d89bc241219ad
Jens-Uwe Gaspar <juga@users.sf.net>
2009-09-06 14:49:04 +0000

    removed translation-group till there are any translation-texts

--- admin_password.php
+++ admin_password.php
@@ -17,7 +17,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-$TranslateGroups[] = "Admin";
+// translations removed for this page: $TranslateGroups[] = "Admin";
 
 require_once( "include/std_functions.php" );
 require_once( "include/form_functions.php" );
