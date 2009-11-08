42efe0906009c2e93ccae41a4ae002831f3437cd
Jens-Uwe Gaspar <juga@users.sf.net>
2009-11-08 01:10:29 +0000

    errorlabel-fix

--- scripts/updates/fix_message_thread-1_0_15.php
+++ scripts/updates/fix_message_thread-1_0_15.php
@@ -30,7 +30,7 @@ require_once( "include/std_functions.php" );
       error('not_logged_in');
 
    if( !(@$player_row['admin_level'] & ADMIN_DATABASE) )
-      error('adminlevel_too_low', 'fix_game_comments-1_0_15');
+      error('adminlevel_too_low', 'fix_message_thread-1_0_15');
 
    $page = $_SERVER['PHP_SELF'];
    $page_args = array();
