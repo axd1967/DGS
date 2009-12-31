9c4cb4dcbb0d97ed4846fe29de215d081bceb13f
Jens-Uwe Gaspar <juga@users.sf.net>
2009-12-31 19:10:17 +0000

    more hints who wants to change what for whom in error-message

--- send_new_password.php
+++ send_new_password.php
@@ -27,12 +27,12 @@ require_once( "include/std_functions.php" );
    connect2mysql();
 
    $logged_in = who_is_logged( $player_row);
+   $my_id = @$player_row['ID'];
 
    if( isset($_POST['goback']) )
       jump_to("index.php");
 
    $pswduser = get_request_arg('pswduser');
-
    if( strtolower($pswduser) == 'guest' )
       error('not_allowed_for_guest');
 
@@ -41,10 +41,9 @@ require_once( "include/std_functions.php" );
          "FROM Players WHERE Handle='".mysql_addslashes($pswduser)."' LIMIT 1" );
 
    if( @mysql_num_rows($result) != 1 )
-      error('unknown_user');
+      error('unknown_user', "send_new_password.find_user($pswduser)");
 
    $row = mysql_fetch_assoc($result);
-
    if( $row['ID'] <= GUESTS_ID_MAX )
       error('not_allowed_for_guest');
 
@@ -54,30 +53,30 @@ require_once( "include/std_functions.php" );
       {
          // Could force email only if admin
          if( !$logged_in )
-            error('not_logged_in');
+            error('not_logged_in', "send_new_password.check_admin.password($my_id,$pswduser)");
          if( !(@$player_row['admin_level'] & ADMIN_PASSWORD) )
-            error('adminlevel_too_low', 'send_new_password.reset_password');
+            error('adminlevel_too_low', "send_new_password.reset_password($my_id,$pswduser)");
 
          $row['Newpassword'] = '';
       }
    }
 
    if( !empty($row['Newpassword']) )
-      error('newpassword_already_sent');
+      error('newpassword_already_sent', "send_new_password.already_sent($pswduser)");
 
    if( !empty($_POST['email']) )
    {
       // Could force email only if admin
       if( !$logged_in )
-         error('not_logged_in');
+         error('not_logged_in', "send_new_password.check_admin.email($my_id,$pswduser)");
       if( !(@$player_row['admin_level'] & ADMIN_PASSWORD) )
-         error('adminlevel_too_low', 'send_new_password.set_email');
+         error('adminlevel_too_low', "send_new_password.set_email($my_id,$pswduser)");
 
       $row['Email'] = trim($_POST['email']);
    }
 
    if( empty($row['Email']) )
-      error('no_email');
+      error('no_email', "send_new_password.miss_email($my_id,$pswduser)");
 
 
 // Now generate new password:
