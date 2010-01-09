5c5505d6fe6b42895fed640d92aa728a8434c50a
Jens-Uwe Gaspar <juga@users.sf.net>
2010-01-09 21:36:46 +0000

    - emphasized Komi-/Handicap-adjustment for tournament-rules - code/GUI cleanups-refactorings, adjusted texts

--- tournaments/view_tournament.php
+++ tournaments/view_tournament.php
@@ -34,6 +34,7 @@ require_once( 'tournaments/include/tournament_rules.php' );
 
 $GLOBALS['ThePage'] = new Page('Tournament');
 
+
 {
    connect2mysql();
 
@@ -49,6 +50,8 @@ $GLOBALS['ThePage'] = new Page('Tournament');
    if( is_null($tourney) )
       error('unknown_tournament', "view_tournament.find_tournament($tid)");
    $allow_edit_tourney = $tourney->allow_edit_tournaments( $my_id );
+
+   // init
 
    // TP-count
    $tp_counts = TournamentParticipant::count_tournament_participants( $tid );
@@ -68,26 +71,23 @@ $GLOBALS['ThePage'] = new Page('Tournament');
 
    // --------------- Information -----------------------------------
 
-   $title = sprintf( T_('%1$s %2$s Tournament - General Information'),
-         Tournament::getScopeText($tourney->Scope),
-         Tournament::getTypeText($tourney->Type) );
+   $title = sprintf( '%s %s %s',
+                     Tournament::getScopeText($tourney->Scope),
+                     Tournament::getTypeText($tourney->Type),
+                     T_('Tournament - General Information') );
    section( 'info', $title );
    echo
+      T_('This page contains all necessary information and links to participate in the tournament.'),
+      T_('There are different sections:#tourney'), "\n<ul>",
+         "\n<li>", anchor( "$base_path$page_tourney#title", T_('Tournament description') ),
+         "\n<li>", anchor( "$base_path$page_tourney#rules", T_('Tournament ruleset') ),
+         "\n<li>", anchor( "$base_path$page_tourney#registration", T_('Tournament registration information') ),
+         "\n<li>", anchor( "$base_path$page_tourney#result", T_('Tournament results') ),
+      "</ul>\n",
       make_html_safe(
-         sprintf( T_('This page contains all necessary information and links to participate in the tournament. '
-                  . 'There are different sections with a <home %1$s>description of the tournament</home>, '
-                  . 'the used <home %2$s>rulesets</home>, <home %3$s>registration information</home> '
-                  . 'and the <home %4$s>tournament results</home>.'),
-            $page_tourney.'#title',
-            $page_tourney.'#rules',
-            $page_tourney.'#registration',
-            $page_tourney.'#result' ), true ),
-      "<br><br>\n",
-      make_html_safe(
-         sprintf( T_('When you have a question about the tournament, please send '
-                  . 'a message to one of the <home %s>tournament directors</home> '
-                  . 'or ask in the <home forum/index.php>Tournaments forum</home>.'),
-            $page_tdirs ), true ),
+         T_('When you have a question about the tournament, please send a message '
+            . 'to one of the tournament directors or ask in the <home forum/index.php>Tournaments forum</home>.'),
+         true ),
       "\n";
 
    // ------------- Section Menu
@@ -132,61 +132,7 @@ $GLOBALS['ThePage'] = new Page('Tournament');
    section( 'tournament', T_('Rules#T_view') );
 
    if( !is_null($trule) )
-   {
-      $adj_komi = array();
-      if( $trule->AdjKomi )
-         $adj_komi[] = sprintf( T_('adjusted by %s points#trules_komi'),
-               ( $trule->AdjKomi > 0 ? '+' : '') . sprintf('%.1f', $trule->AdjKomi) );
-      if( $trule->JigoMode == JIGOMODE_ALLOW_JIGO )
-         $adj_komi[] = T_('Jigo allowed#trules_komi');
-      elseif( $trule->JigoMode == JIGOMODE_NO_JIGO )
-         $adj_komi[] = T_('No Jigo allowed#trules_komi');
-
-      $adj_handi = array();
-      if( $trule->AdjHandicap )
-         $adj_handi[] = sprintf( T_('adjusted by %s stones#trules_handi'),
-               ( $trule->AdjHandicap > 0 ? '+' : '') . $trule->AdjHandicap );
-      if( $trule->MinHandicap > 0 && $trule->MaxHandicap < MAX_HANDICAP )
-         $adj_handi[] = sprintf( T_('limited by min. %s and max. %s stones#trules_handi'),
-               $trule->MinHandicap, min( MAX_HANDICAP, $trule->MaxHandicap) );
-      elseif( $trule->MinHandicap > 0 )
-         $adj_handi[] = sprintf( T_('limited by min. %s stones#trules_handi'), $trule->MinHandicap );
-      elseif( $trule->MaxHandicap < MAX_HANDICAP )
-         $adj_handi[] = sprintf( T_('limited by max. %s stones#trules_handi'), $trule->MaxHandicap );
-
-      $itable = new Table_info('gamerules');
-      $itable->add_sinfo( T_('Board Size:#trules'), $trule->Size .' x '. $trule->Size );
-      $itable->add_sinfo( T_('Handicap Type:#trules'),
-            TournamentRules::getHandicaptypeText($trule->Handicaptype) );
-      $itable->add_sinfo( T_('Handicap:#trules'),
-         ( $trule->needsCalculatedHandicap()
-               ? T_('calculated stones#trules_handi')
-               : sprintf( T_('%s stones#trules_handi'), $trule->Handicap) )
-         . ', ' .
-         ( $trule->needsCalculatedKomi()
-               ? T_('calculated komi#trules_handi')
-               : sprintf( T_('%s komi#trules_handi'), $trule->Komi) ));
-      if( count($adj_handi) )
-         $itable->add_sinfo( T_('Handicap adjustment:#trules_handi'), implode(', ', $adj_handi) );
-      if( count($adj_komi) )
-         $itable->add_sinfo( T_('Komi adjustment:#trules_komi'), implode(', ', $adj_komi) );
-      if( ENA_STDHANDICAP )
-         $itable->add_sinfo( T_('Standard placement:#trules_handi'), yesno($trule->StdHandicap) );
-      $itable->add_sinfo( T_('Main time:#trules'), TimeFormat::echo_time($trule->Maintime)
-            . ( ($trule->Byotime == 0) ? SMALL_SPACING.T_('(absolute time)#trules') : '' ));
-      if( $trule->Byotime > 0 )
-         $itable->add_sinfo(
-            TimeFormat::echo_byotype($trule->Byotype) . ':',
-            TimeFormat::echo_time_limit( -1, $trule->Byotype, $trule->Byotime, $trule->Byoperiods, 0) );
-      $itable->add_sinfo( T_('Clock runs on weekends:#trules'), yesno($trule->WeekendClock) );
-      $itable->add_sinfo( T_('Rated:#trules'), yesno($trule->Rated) );
-
-      if( $trule->Notes != '')
-         echo make_html_safe( $trule->Notes, true ), "<br><br>\n";
-      echo T_('The following ruleset is used for all rounds in this tournament:'),
-         $itable->make_table(),
-         "<br>\n";
-   }
+      echo_tournament_rules( $trule );
 
 
    // --------------- Registration ----------------------------------
@@ -195,57 +141,18 @@ $GLOBALS['ThePage'] = new Page('Tournament');
    section( 'tournament', T_('Registration#T_view') );
 
    if( !is_null($tprops) )
-   {
-      $arr_tprops = array();
-
-      // limit register end-time
-      if( $tprops->RegisterEndTime )
-         $arr_tprops[] = sprintf( T_('Registration phase ends on [%s]'),
-               TournamentUtils::formatDate($tprops->RegisterEndTime) );
-
-      // limit participants
-      if( $tprops->MinParticipants > 0 && $tprops->MaxParticipants > 0 )
-         $arr_tprops[] = sprintf( T_('Tournament needs: min. %s and max. %s participants'),
-               $tprops->MinParticipants, $tprops->MaxParticipants );
-      elseif( $tprops->MinParticipants > 0 )
-         $arr_tprops[] = sprintf( T_('Tournament needs: min. %s participants'), $tprops->MinParticipants );
-      elseif( $tprops->MaxParticipants > 0 )
-         $arr_tprops[] = sprintf( T_('Tournament needs: max. %s participants'), $tprops->MaxParticipants );
-
-      // use-rating-mode, limit user-rating
-      $arr_tprops[] = TournamentProperties::getRatingUseModeText( $tprops->RatingUseMode, false );
-      if( $tprops->UserRated )
-         $arr_tprops[] = sprintf( T_('User rating must be between [%s - %s].'),
-               echo_rating( $tprops->UserMinRating, false ),
-               echo_rating( $tprops->UserMaxRating, false ));
-
-      // limit games-number
-      if( $tprops->UserMinGamesFinished > 0 )
-         $arr_tprops[] = sprintf( T_('User must have at least %s finished games.'),
-               $tprops->UserMinGamesFinished );
-      if( $tprops->UserMinGamesRated > 0 )
-         $arr_tprops[] = sprintf( T_('User must have at least %s rated finished games.'),
-               $tprops->UserMinGamesRated );
-
-      if( count($arr_tprops) )
-         echo T_('To register for this tournament the following criteria must match:'),
-              '<ul><li>', implode("\n<li>", $arr_tprops), "</ul>\n";
-      echo ( $tprops->Notes != '' ? make_html_safe($tprops->Notes, true) . "<br><br>\n" : '');
-   }
+      echo_tournament_registration( $tprops );
 
    // number of registered users
-   $tpcnt_view = "<br>\n<ul>";
+   echo sprintf( T_('Registrations for this tournament: %s user(s)'), $tp_count_all ),
+      "<br>\n<ul>";
    foreach( $tp_counts as $t_status => $cnt )
    {
-      $tpcnt_view .= "  <li>" .
-         sprintf( T_('%3d users on status [%s]'),
-                  $cnt, TournamentParticipant::getStatusText($t_status) )
-         . "\n";
+      echo "  <li>",
+         sprintf( T_('%3d users on status [%s]'), $cnt, TournamentParticipant::getStatusText($t_status) ),
+         "\n";
    }
-   $tpcnt_view .= "</ul>\n";
-
-   echo sprintf( T_('Registrations for this tournament: %s user(s)'), $tp_count_all ),
-      $tpcnt_view;
+   echo "</ul>\n";
 
    // ------------- Section Menu
 
@@ -273,4 +180,105 @@ $GLOBALS['ThePage'] = new Page('Tournament');
 
    end_page();
 }
+
+
+function echo_tournament_rules( $trule )
+{
+   $adj_komi = array();
+   if( $trule->AdjKomi )
+      $adj_komi[] = sprintf( T_('adjusted by %s points#trules_komi'),
+            spacing( ($trule->AdjKomi > 0 ? '+' : '') . sprintf('%.1f', $trule->AdjKomi), 1, 'b') );
+   if( $trule->JigoMode == JIGOMODE_ALLOW_JIGO )
+      $adj_komi[] = T_('Jigo allowed#trules_komi');
+   elseif( $trule->JigoMode == JIGOMODE_NO_JIGO )
+      $adj_komi[] = T_('No Jigo allowed#trules_komi');
+
+   $adj_handi = array();
+   if( $trule->AdjHandicap )
+      $adj_handi[] = sprintf( T_('adjusted by %s stones#trules_handi'),
+            spacing( ($trule->AdjHandicap > 0 ? '+' : '') . $trule->AdjHandicap, 1, 'b') );
+   if( $trule->MinHandicap > 0 && $trule->MaxHandicap < MAX_HANDICAP )
+      $adj_handi[] = sprintf( T_('limited by min. %s and max. %s stones#trules_handi'),
+            $trule->MinHandicap, min( MAX_HANDICAP, $trule->MaxHandicap) );
+   elseif( $trule->MinHandicap > 0 )
+      $adj_handi[] = sprintf( T_('limited by min. %s stones#trules_handi'), $trule->MinHandicap );
+   elseif( $trule->MaxHandicap < MAX_HANDICAP )
+      $adj_handi[] = sprintf( T_('limited by max. %s stones#trules_handi'), $trule->MaxHandicap );
+
+   $itable = new Table_info('gamerules');
+   $itable->add_sinfo( T_('Board Size:#trules'), $trule->Size .' x '. $trule->Size );
+   $itable->add_sinfo( T_('Handicap Type:#trules'),
+         TournamentRules::getHandicaptypeText($trule->Handicaptype) );
+   $itable->add_sinfo( T_('Handicap:#trules'),
+      ( $trule->needsCalculatedHandicap()
+            ? T_('calculated stones#trules_handi')
+            : sprintf( T_('%s stones#trules_handi'), $trule->Handicap) )
+      . ', ' .
+      ( $trule->needsCalculatedKomi()
+            ? T_('calculated komi#trules_handi')
+            : sprintf( T_('%s komi#trules_handi'), $trule->Komi) ));
+   if( count($adj_handi) )
+      $itable->add_sinfo( T_('Handicap adjustment:#trules_handi'), implode(', ', $adj_handi) );
+   if( count($adj_komi) )
+      $itable->add_sinfo( T_('Komi adjustment:#trules_komi'), implode(', ', $adj_komi) );
+   if( ENA_STDHANDICAP )
+      $itable->add_sinfo( T_('Standard placement:#trules_handi'), yesno($trule->StdHandicap) );
+   $itable->add_sinfo( T_('Main time:#trules'), TimeFormat::echo_time($trule->Maintime)
+         . ( ($trule->Byotime == 0) ? SMALL_SPACING.T_('(absolute time)#trules') : '' ));
+   if( $trule->Byotime > 0 )
+      $itable->add_sinfo(
+         TimeFormat::echo_byotype($trule->Byotype) . ':',
+         TimeFormat::echo_time_limit( -1, $trule->Byotype, $trule->Byotime, $trule->Byoperiods, 0) );
+   $itable->add_sinfo( T_('Clock runs on weekends:#trules'), yesno($trule->WeekendClock) );
+   $itable->add_sinfo( T_('Rated:#trules'), yesno($trule->Rated) );
+
+   if( $trule->Notes != '')
+      echo make_html_safe( $trule->Notes, true ), "<br><br>\n";
+
+   echo T_('The following ruleset is used for this tournament'), ':',
+      $itable->make_table(),
+      "<br>\n";
+}//echo_tournament_rules
+
+
+function echo_tournament_registration( $tprops )
+{
+   $arr_tprops = array();
+
+   // limit register end-time
+   if( $tprops->RegisterEndTime )
+      $arr_tprops[] = sprintf( T_('Registration phase ends on [%s]'),
+            TournamentUtils::formatDate($tprops->RegisterEndTime) );
+
+   // limit participants
+   if( $tprops->MinParticipants > 0 && $tprops->MaxParticipants > 0 )
+      $arr_tprops[] = sprintf( T_('Tournament needs: min. %s and max. %s participants'),
+            $tprops->MinParticipants, $tprops->MaxParticipants );
+   elseif( $tprops->MinParticipants > 0 )
+      $arr_tprops[] = sprintf( T_('Tournament needs: min. %s participants'), $tprops->MinParticipants );
+   elseif( $tprops->MaxParticipants > 0 )
+      $arr_tprops[] = sprintf( T_('Tournament needs: max. %s participants'), $tprops->MaxParticipants );
+
+   // use-rating-mode, limit user-rating
+   $arr_tprops[] = TournamentProperties::getRatingUseModeText( $tprops->RatingUseMode, false );
+   if( $tprops->UserRated )
+      $arr_tprops[] = sprintf( T_('User rating must be between [%s - %s].'),
+            echo_rating( $tprops->UserMinRating, false ),
+            echo_rating( $tprops->UserMaxRating, false ));
+
+   // limit games-number
+   if( $tprops->UserMinGamesFinished > 0 )
+      $arr_tprops[] = sprintf( T_('User must have at least %s finished games.'),
+            $tprops->UserMinGamesFinished );
+   if( $tprops->UserMinGamesRated > 0 )
+      $arr_tprops[] = sprintf( T_('User must have at least %s rated finished games.'),
+            $tprops->UserMinGamesRated );
+
+   if( count($arr_tprops) )
+      echo T_('To register for this tournament the following criteria must match'), ':',
+           '<ul><li>', implode("\n<li>", $arr_tprops), "</ul>\n";
+   if( $tprops->Notes != '' )
+      echo make_html_safe($tprops->Notes, true), "<br><br>\n";
+}//echo_tournament_registration
+
 ?>
