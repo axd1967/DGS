49e91fca7aa495dabeb7dba595da57df4bad9d71
Jens-Uwe Gaspar <juga@users.sf.net>
2010-01-18 22:12:27 +0000

    index-page redirecting to tournament-list page

---
+++ tournaments/ladder/index.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Dragon Go Server
+Copyright (C) 2001-2010  Erik Ouchterlony, Jens-Uwe Gaspar
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// redirect to [tournamens/list.php]
+
+do {
+  if( !chdir('../') ) exit;
+} while( !file_exists('include/connect2mysql.php') );
+require_once( 'include/connect2mysql.php' );
+
+jump_to('tournaments/list_tournaments.php');
+
+?>
