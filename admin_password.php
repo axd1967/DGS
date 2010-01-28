2c7db0d76961c22ee2d141b2ef65af7b7af5a709
Jens-Uwe Gaspar <juga@users.sf.net>
2010-01-28 22:51:51 +0000

    add 2nd form to show email (if existing) on admin-new-password page

--- admin_password.php
+++ admin_password.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Dragon Go Server
-Copyright (C) 2001-2010  Erik Ouchterlony, Rod Ival
+Copyright (C) 2001-2010  Erik Ouchterlony, Rod Ival, Jens-Uwe Gaspar
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
@@ -33,32 +33,70 @@ require_once( "include/form_functions.php" );
    if( !(@$player_row['admin_level'] & ADMIN_PASSWORD) )
       error('adminlevel_too_low');
 
-   start_page(/*T_*/('Admin').' - './*T_*/('Send password'), true, $logged_in, $player_row );
+   $user = get_request_arg('pswduser');
+   $user_email = null;
+   if( @$_REQUEST['show_email'] && (string)$user != '' )
+   {
+      $query = "SELECT Email FROM Players WHERE Handle='".mysql_addslashes($user)."' LIMIT 1";
+      $row = mysql_single_fetch( "admin_password.find_user($user)", $query );
+      $row_email = ( $row ) ? trim($row['Email']) : '';
+      if( (string)$row_email != '' )
+         $user_email = $row_email;
+   }
 
-   echo "<center>\n";
+
+   start_page(/*T_*/('Admin').' - './*T_*/('Send password'), true, $logged_in, $player_row );
 
    $passwd_form = new Form( 'adminnewpasswdform', "send_new_password.php", FORM_POST );
 
    $passwd_form->add_row( array( 'HEADER', /*T_*/('New password') ) );
 
    $passwd_form->add_row( array( 'DESCRIPTION', /*T_*/('Userid'),
-                                 'TEXTINPUT', 'pswduser', 16, 16, '',
-                                 'TD',
-                                 'SUBMITBUTTON', 'action', /*T_*/('Send password'),
-                               ) );
+                                 'TEXTINPUT', 'pswduser', 16, 16, $user, ));
    $passwd_form->add_row( array( 'DESCRIPTION', /*T_*/('Email'),
                                  'TEXTINPUT', 'email', 32, 80, '',
-                                 'TD',
-                                 'TEXT', /*T_*/('to replace the user\'s one'),
-                               ) );
-   $passwd_form->add_row( array( 'TAB',
-                                 'CELL', 2, 'align=left',
+                                 'TEXT', /*T_*/('to replace the user\'s one'), ));
+   $passwd_form->add_row( array( 'TAB', 'CELL', 1, 'align=left',
                                  'CHECKBOX', 'overnew', 1,
-                                 /*T_*/('overwrite the current new password process'), 0,
+                                     /*T_*/('overwrite the current new password process'), 0, ));
+   $passwd_form->add_row( array( 'TAB', 'CELL', 1, 'align=left',
+                                 'SUBMITBUTTON', 'action', /*T_*/('Send password'),
                                ) );
    $passwd_form->echo_string(1);
 
-   echo "</center>\n";
-   end_page();
+   echo "<p></p>\n";
+
+
+   $email_form = new Form( 'adminemailform', "admin_password.php", FORM_GET );
+
+   $email_form->add_row( array( 'HEADER', /*T_*/('User information') ) );
+
+   $email_form->add_row( array(
+         'CELL', 2, '',
+         'TEXT', /*T_*/('Please keep in mind, that the email is protected by DGS\' Privacy Policy!'), ));
+   $email_form->add_row( array(
+         'CELL', 2, '',
+         'TEXT', /*T_*/('See also') . ':' . MED_SPACING
+               . anchor( $base_path."policy.php", /*T_*/('DGS Privacy Policy')), ));
+   $email_form->add_empty_row();
+
+   $email_form->add_row( array(
+         'DESCRIPTION', /*T_*/('Userid'),
+         'TEXTINPUT', 'pswduser', 16, 16, $user,
+         'TEXT', SMALL_SPACING,
+         'SUBMITBUTTON', 'show_email', /*T_*/('Show email'), ));
+   if( @$_REQUEST['show_email'] || !is_null($user_email) )
+   {
+      $email_form->add_row( array(
+            'DESCRIPTION', /*T_*/('Email'),
+            'TEXT', ( is_null($user_email) ? NO_VALUE : $user_email ), ));
+   }
+   $email_form->echo_string();
+
+
+   $menu_array = array();
+   $menu_array[T_('New password')] = "admin_password.php";
+
+   end_page(@$menu_array);
 }
 ?>
