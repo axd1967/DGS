ae58c03d7e6dbe5468ff4b485db71a4d1b6ea74e
Jens-Uwe Gaspar <juga@users.sf.net>
2010-03-06 21:31:01 +0000

    - added handling of long-user-absence   - check once a day if there are users not being online for TournamentLadderProps.UserAbsenceDays without vacation and process TournamentLadderProps.UserAbsenceAction for them either removing user from ladder or moving absent user to last position in ladder   - added daily clock for daily check in tournaments-cron processing long user-absences   - added Players.UseVacation with (starting) days of new vacation period to check on user-absence for tournaments - moved tournament-locking check and cron-lock-setting/releasing into tournament-cache class - added const DBG_TEST for testing code, can be set in local-config for local test-systems

--- tournaments/cron_tournaments.php
+++ tournaments/cron_tournaments.php
@@ -43,75 +43,103 @@ if( ALLOW_TOURNAMENTS && !$is_down )
 
    $row = mysql_single_fetch( 'cron_tournament.check_frequency',
       "SELECT ($NOW-UNIX_TIMESTAMP(Lastchanged)) AS timediff"
-      ." FROM Clock WHERE ID=205 LIMIT 1" );
+      ." FROM Clock WHERE ID=".CLOCK_CRON_TOURNEY." LIMIT 1" );
    if( !$row )
       $TheErrors->dump_exit('cron_tournament');
-//TODO commented only for testing
-//   if( $row['timediff'] < $chk_time_diff )
-//      $TheErrors->dump_exit('cron_tournament');
+   if( !DBG_TEST && $row['timediff'] < $chk_time_diff )
+      $TheErrors->dump_exit('cron_tournament');
 
    db_query( 'cron_tournament.set_lastchanged',
-         "UPDATE Clock SET Ticks=1, Lastchanged=FROM_UNIXTIME($NOW) WHERE ID=205 LIMIT 1" )
+         "UPDATE Clock SET Ticks=1, Lastchanged=FROM_UNIXTIME($NOW) WHERE ID=".CLOCK_CRON_TOURNEY." LIMIT 1" )
       or $TheErrors->dump_exit('cron_tournament');
 
 
    // ---------- BEGIN ------------------------------
 
-   $player_row['Handle'] = '#CRON'; // for tourney-tables ChangedBy-fields
+   $player_row['Handle'] = '#CRONT'; // for tourney-tables ChangedBy-fields
    $tg_order = "ORDER BY TG.tid ASC, TG.Lastchanged ASC, TG.ID ASC";
    $thelper = new TournamentHelper();
 
+
+   // ---------- jobs running only once a day
+
+   // NOTE: added special daily-jobs here to avoid the complexity of cron-locking
+   //       of daily_cron/tournament-cron-scripts
+
+   $row = mysql_single_fetch( 'cron_tournament.daily.check_frequency',
+      "SELECT ($NOW-UNIX_TIMESTAMP(Lastchanged)) AS timediff"
+      ." FROM Clock WHERE ID=".CLOCK_CRON_TOURNEY_DAILY." LIMIT 1" );
+   if( $row )
+   {
+      if( DBG_TEST || $row['timediff'] >= 3600*24 ) // one day-check
+      {
+         db_query( 'cron_tournament.daily.next_run_date',
+            "UPDATE Clock SET Lastchanged=FROM_UNIXTIME($NOW) WHERE ID=".CLOCK_CRON_TOURNEY_DAILY." LIMIT 1" );
+
+         run_once_daily();
+      }
+   }
+
+
    // ---------- handle tournament-game ending by score/resignation/jigo/timeout
 
-   $tg_iterator = new ListIterator( 'Tournament.cron.load_tgames.score', null, $tg_order );
+   $tg_iterator = new ListIterator( 'cron_tournament.load_tgames.score', null, $tg_order );
    $tg_iterator = TournamentGames::load_tournament_games( $tg_iterator, 0, TG_STATUS_SCORE );
 
-   $lock_tourney = null;
    while( list(,$arr_item) = $tg_iterator->getListIterator() )
    {
       list( $tgame, $orow ) = $arr_item;
       $tid = $tgame->tid;
 
-      $tourney = $thelper->tcache->load_tournament( 'Tournament.cron.game_end', $tid );
+      $tourney = $thelper->tcache->load_tournament( 'cron_tournament.game_end', $tid );
       if( !is_null($tourney) && $thelper->process_tournament_game_end( $tourney, $tgame, /*check*/true ) )
       {
-         // release (previous) lock when handling new tourney-ID
-         if( !is_null($lock_tourney) && ($tourney->ID != $lock_tourney->ID) )
-         {
-            $lock_tourney->update_flags( TOURNEY_FLAG_LOCK_CRON, 0 );
-            $lock_tourney = null;
-         }
-
-         // check locks
-         if( $tourney->isFlagSet(TOURNEY_FLAG_LOCK_ADMIN | TOURNEY_FLAG_LOCK_TDWORK) )
-            continue;
-
-         // lock for tourney-changes
-         if( is_null($lock_tourney) )
-         {
-            $tourney->update_flags( TOURNEY_FLAG_LOCK_CRON, 1 );
-            $lock_tourney = $tourney;
-         }
-
-         $thelper->process_tournament_game_end( $tourney, $tgame, /*check*/false );
+         $thelper->tcache->release_tournament_cron_lock( $tid );
+         if( $thelper->tcache->set_tournament_cron_lock( $tid ) )
+            $thelper->process_tournament_game_end( $tourney, $tgame, /*check*/false );
       }
    }
-   if( !is_null($lock_tourney) )
-      $lock_tourney->update_flags( TOURNEY_FLAG_LOCK_CRON, 0 );
+   $thelper->tcache->release_tournament_cron_lock();
 
 
    // ---------- finish waiting, due tournament-games
 
-   $wait_ticks = (int)$thelper->tcache->load_clock_ticks( 'Tournament.cron.game_wait', CLOCK_TOURNEY_GAME_WAIT );
-   TournamentGames::update_tournament_game_wait( 'Tournament.cron', $player_row['Handle'], $wait_ticks );
+   $wait_ticks = (int)$thelper->tcache->load_clock_ticks( 'cron_tournament.game_wait', CLOCK_TOURNEY_GAME_WAIT );
+   TournamentGames::update_tournament_game_wait( 'cron_tournament', $player_row['Handle'], $wait_ticks );
 
 
    // ---------- END --------------------------------
 
    db_query( 'cron_tournament.reset_tick',
-         "UPDATE Clock SET Ticks=0 WHERE ID=205 LIMIT 1" );
+      "UPDATE Clock SET Ticks=0 WHERE ID=".CLOCK_CRON_TOURNEY." LIMIT 1" );
 
    if( !$chained )
       $TheErrors->dump_exit('cron_tournament');
 }//$is_down
+
+
+function run_once_daily()
+{
+   global $thelper;
+
+   // ---------- Handle long-user-absence for tournaments
+
+   $tl_iterator = TournamentHelper::load_ladder_absent_users(
+      new ListIterator( 'cron_tournament.load_ladder_user_absence' ));
+
+   ta_begin();
+   {//HOT-section to process long-user-absence
+      while( list(,$arr_item) = $tl_iterator->getListIterator() )
+      {
+         list( $tladder, $orow ) = $arr_item;
+         $tid = $tladder->tid;
+
+         $thelper->tcache->release_tournament_cron_lock( $tid );
+         if( $thelper->tcache->set_tournament_cron_lock( $tid ) )
+            TournamentLadder::process_user_absence( $tid, $tladder->uid, $orow['TLP_UserAbsenceAction'] );
+      }
+      $thelper->tcache->release_tournament_cron_lock();
+   }
+   ta_end();
+}//run_once_daily
 ?>
