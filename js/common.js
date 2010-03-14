7c638ce595e6279e4dcb9b12c59d32b68e0f608c
Jens-Uwe Gaspar <juga@users.sf.net>
2010-03-14 02:10:49 +0000

    - added utility-function sprintf with support of '%s'-format - added functions to show popup info-box with with rank-change-info for ladder-tournament - removed unused version-var

--- js/common.js
+++ js/common.js
@@ -1,6 +1,7 @@
+// <!--
 /*
 Dragon Go Server
-Copyright (C) 2001-2010  Erik Ouchterlony
+Copyright (C) 2001-2010  Erik Ouchterlony, Jens-Uwe Gaspar
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
@@ -16,9 +17,70 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-var version = 0;
+function sprintf() {
+   if( sprintf.arguments.length < 2 )
+      return;
 
-function toggle_class(obj,cl1,cl2)
-{
-   obj.className=((obj.className==cl1)?cl2:cl1);
+   var data = sprintf.arguments[0];
+   for( var i=1; i < sprintf.arguments.length; ++i )
+      data = data.replace( /%s/, sprintf.arguments[i] );
+
+   return data;
 }
+if( !String.sprintf )
+   String.sprintf = sprintf;
+
+function toggle_class( obj, cl1, cl2 )
+{
+   obj.className = ( obj.className == cl1 ) ? cl2 : cl1;
+}
+
+function showInfo( e, title, content )
+{
+   var infoBoxElem = document.getElementById('InfoBox');
+   infoBoxElem.innerHTML = "<table><tr><th>" + title + "</th></tr>" + "<tr><td>" + content + "</td></tr></table>";
+
+   var x, y;
+   if( document.all )
+   {
+      x = event.clientX + document.body.scrollLeft;
+      y = event.clientY + document.body.scrollTop;
+   }
+   else
+   {
+      x = e.pageX;
+      y = e.pageY;
+   }
+
+   var boxStyle = infoBoxElem.style;
+   boxStyle.top  = (y + 10) + 'px';
+   boxStyle.left = (x + 10) + 'px';
+   boxStyle.visibility = 'visible';
+}
+
+function hideInfo()
+{
+   document.getElementById('InfoBox').style.visibility = 'hidden';
+}
+
+// NOTE: requires global vars with translated texts: T_rankInfoTitle/Format
+function showTLRankInfo( e, rank, best_rank, period_rank, history_rank )
+{
+   var diff1 = buildRankDiff( rank, period_rank );
+   var diff2 = buildRankDiff( rank, history_rank );
+   showInfo( e, T_rankInfoTitle, String.sprintf( T_rankInfoFormat, rank, best_rank, diff1, diff2 ) );
+}
+
+// see TournamentLadder::build_rank_diff()
+function buildRankDiff( rank, prev_rank )
+{
+   if( rank == prev_rank )
+      rank_diff = '=';
+   else if( rank < prev_rank )
+      rank_diff = '+' + (prev_rank - rank);
+   else //rank > prev_rank
+      rank_diff = '-' + (rank - prev_rank);
+   return String.sprintf( "%s. (%s)", prev_rank, rank_diff );
+}
+
+// -->
