e531bdeb24149bd32e78e3bd0d2d09cfa03116a6
Jens-Uwe Gaspar <juga@users.sf.net>
2010-03-14 02:10:05 +0000

    removed unused version-var

--- js/goban_editor.js
+++ js/goban_editor.js
@@ -16,7 +16,5 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-var version = 1;
-
 // Here comes the new JavaScript editor code
 
