9dbd177c945d9634e524b5fb37f43a7e32e792fc
Jens-Uwe Gaspar <juga@users.sf.net>
2010-03-22 22:05:59 +0000

    revision 1.8 copied from history of tournaments/edit_round.php date: 2010/01/24 01:31:53;  author: juga;  state: Exp;  lines: +2 -2 - adjusted bottom-menu link-texts:   - "View this tournament" -> "Tournament info"   - "Manage this tournament" -> "Manage tournament"

--- tournaments/roundrobin/edit_round_props.php
+++ tournaments/roundrobin/edit_round_props.php
@@ -164,8 +164,8 @@ $GLOBALS['ThePage'] = new Page('TournamentRoundEdit');
 
 
    $menu_array = array();
-   $menu_array[T_('View this tournament')] = "tournaments/view_tournament.php?tid=$tid";
-   $menu_array[T_('Manage this tournament')] =
+   $menu_array[T_('Tournament info')] = "tournaments/view_tournament.php?tid=$tid";
+   $menu_array[T_('Manage tournament')] =
          array( 'url' => "tournaments/manage_tournament.php?tid=$tid", 'class' => 'TAdmin' );
 
    end_page(@$menu_array);
