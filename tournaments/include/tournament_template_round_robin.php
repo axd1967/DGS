d19d730990790bca0a2454b6b8de227f355f1c99
Jens-Uwe Gaspar <juga@users.sf.net>
2010-03-27 16:28:59 +0000

    added TODO

--- tournaments/include/tournament_template_round_robin.php
+++ tournaments/include/tournament_template_round_robin.php
@@ -114,6 +114,8 @@ class TournamentTemplateRoundRobin extends TournamentTemplate
       if( $t_status == TOURNEY_STATUS_REGISTER || $t_status == TOURNEY_STATUS_PAIR )
          $errors = array_merge( $errors, $tround->check_properties() );
 
+      //TODO check, that there are enough TPs taking care users with TPs.StartRound > 1
+
       return $errors;
    }
 
