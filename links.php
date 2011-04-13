d53a1d052ea7f03beb0ff7fbbc2b9005b9c20914
Jens-Uwe Gaspar <juga@users.sf.net>
2011-04-13 08:07:03 +0000

    - bugfix: use translations when loading Links from DB - bugfix: hide categories and entries if marked as hidden in DB - adjusted page to seed database with former Links-entries if no Links-table exists, show static links instead then - use one method for both: create Links in DB, and statically show them

--- links.php
+++ links.php
@@ -19,8 +19,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 $TranslateGroups[] = "Docs";
 
-require_once( "include/std_functions.php" );
+require_once 'include/std_functions.php';
+require_once 'include/admin_faq_functions.php';
+
 $GLOBALS['ThePage'] = new Page('Links');
+
 
 {
    connect2mysql();
@@ -30,126 +33,156 @@ $GLOBALS['ThePage'] = new Page('Links');
    start_page(T_('Links'), true, $logged_in, $player_row, "a.blue:visited{color:purple;}" );
    echo "<h3 class=Header>", T_('Link collection'), "</h3>\n";
 
+   // init
+   $TW_ = 'T_'; // for non-const translation-texts
    $link_class = 'DocLinkNarrow';
 
    // show Links from database, or static links if no entries found in db
    if( !load_links() )
+   {
+      ta_begin();
+      {//HOT-section to seed database with link-texts
+         make_static_links( true );
+      }
+      ta_end();
+
       make_static_links();
+   }
 
    end_page();
 }//main
 
 
-function make_static_links()
+function make_static_links( $create=false )
 {
-   section( 'General', T_('General Info'));
+   if( $create )
+   {
+      $sect = 'save_link_section';
+      $link = 'save_link_entry';
+      $linkend = 'nop';
+   }
+   else
+   {
+      $sect = '_section';
+      $link = '_add_link_page_link';
+      $linkend = 'add_link_page_link';
+   }
 
-   add_link_page_link("http://senseis.xmp.net",
-                     T_('Sensei\'s Library'),
-                     T_('A collaboration web site. Read and contribute!'));
-   add_link_page_link("http://gobase.org",
-                     T_('Jan van der Steens Pages'), T_('Lots of info on go'));
-   add_link_page_link("http://www.gogod.co.uk/",
-                     T_('GoGoD database and encyclopaedia'), T_('A large database of professional games'));
-   add_link_page_link("http://senseis.xmp.net/?RGGFAQ",
-                     T_('Go FAQ'),
-                     T_('Frequently asked questions about go for the rec.games.go newsgroup'));
-   add_link_page_link('http://www.intergofed.org/members.htm',
-                     'IGF',
-                     T_('International Go Federation. Find federations and associations in the world.'));
-   add_link_page_link("http://igo-kisen.hp.infoseek.co.jp/topics.html",
-                     T_('Go News'), T_('News and games from the professional scene'));
-   add_link_page_link('http://gosensations.com/',
-                     T_('Go Sensations'), T_('Go News and Sensations from other Go servers'));
-   add_link_page_link("http://senseis.xmp.net/?HikaruNoGo",
-                     T_('Hikaru no Go'), T_('A manga about go. Recommended!'));
-   add_link_page_link();
+   $sect( 'General', 'General Info');
 
-
-   section( 'Rules', T_('Rules'));
-
-   add_link_page_link("http://playgo.to/interactive/index.html",
-                     T_('An Interactive Introduction'),
-                     T_('This is a very nice site to learn with.'));
-   add_link_page_link("http://www.britgo.org/intro/intro1.html",
-                     T_('Introduction'),
-                     T_('Very well written introduction by the British Go Association.'));
-   add_link_page_link("http://www.pandanet.co.jp/English/introduction_of_go/",
-                     T_('Introduction to Go'),
-                     T_('You can master Go in 10 days'));
-   add_link_page_link('http://web.archive.org/web/20070101004300/http://www.sentex.net/~mmcadams/teachgo/',
-                     T_('How to Teach Go'),
-                     T_('This is all you need to get started. Very basic stuff'));
-   add_link_page_link();
+   $link("http://senseis.xmp.net", 'Sensei\'s Library', 'A collaboration web site. Read and contribute!');
+   $link("http://gobase.org", 'Jan van der Steens Pages', 'Lots of info on go');
+   $link("http://www.gogod.co.uk/", 'GoGoD database and encyclopaedia', 'A large database of professional games');
+   $link("http://senseis.xmp.net/?RGGFAQ", 'Go FAQ',
+         'Frequently asked questions about go for the rec.games.go newsgroup');
+   $link('http://www.intergofed.org/members.htm', 'IGF',
+         'International Go Federation. Find federations and associations in the world.');
+   $link("http://igo-kisen.hp.infoseek.co.jp/topics.html", 'Go News', 'News and games from the professional scene');
+   $link('http://gosensations.com/', 'Go Sensations', 'Go News and Sensations from other Go servers');
+   $link("http://senseis.xmp.net/?HikaruNoGo", 'Hikaru no Go', 'A manga about go. Recommended!');
+   $linkend();
 
 
-   section( 'Strategy', T_('Strategy and terms'));
+   $sect( 'Rules', 'Rules');
 
-   add_link_page_link("http://www.goproblems.com/",
-                     T_('Go Problems'), T_('Working through these can help out your game.'));
-   add_link_page_link("http://gtl.jeudego.org/",
-                     T_('Go Teaching Ladder'),
-                     T_('Submit your games for comments to see where you might have played better.'));
-   add_link_page_link("http://www.yomiuri.co.jp/dy/columns/0001/",
-                     T_('The Magic of Go'), T_('A weekly go column by Rob Van Zeijst.'));
-   add_link_page_link("http://senseis.xmp.net/?EssentialGoTerms",
-                     T_('Common Japanese Go Terms'),
-                     T_('You have to know what other players are talking about.'));
-   add_link_page_link();
+   $link("http://playgo.to/interactive/index.html", 'An Interactive Introduction',
+         'This is a very nice site to learn with.');
+   $link("http://www.britgo.org/intro/intro1.html", 'Introduction',
+         'Very well written introduction by the British Go Association.');
+   $link("http://www.pandanet.co.jp/English/introduction_of_go/", 'Introduction to Go', 'You can master Go in 10 days');
+   $link('http://web.archive.org/web/20070101004300/http://www.sentex.net/~mmcadams/teachgo/', 'How to Teach Go',
+         'This is all you need to get started. Very basic stuff');
+   $linkend();
 
 
-   section( 'History', T_('History'));
+   $sect( 'Strategy', 'Strategy and terms');
 
-   add_link_page_link("http://www.britgo.org/intro/intro1.html#bh",
-                     T_('A Brief History'),
-                     T_('For you people with short attention spans.'));
-   add_link_page_link("http://gobase.org/information/history/",
-                     T_('The Extended History'),
-                     T_('In case you\'re an aspiring know-it-all.'));
-   add_link_page_link();
-
-
-   section( 'Equipment', T_('Go books, equipment and software'));
-
-   add_link_page_link("http://www.gobooks.info/",
-                     T_('Annotated Go Bibliographies'),
-                     T_('A large collection of go book reviews'));
-   add_link_page_link("http://www.slateandshell.com/", 'Slate & Shell');
-   add_link_page_link("http://www.kiseido.com/", 'Kiseido');
-   add_link_page_link("http://www.yutopian.com/go/", 'Yutopian');
-   add_link_page_link("http://www.samarkand.net/", 'Samarkand');
-   add_link_page_link("http://www.schaakengo.nl/", 'Het Paard', T_('European shop'));
-   add_link_page_link();
+   $link("http://www.goproblems.com/", 'Go Problems', 'Working through these can help out your game.');
+   $link("http://gtl.jeudego.org/", 'Go Teaching Ladder',
+         'Submit your games for comments to see where you might have played better.');
+   $link("http://www.yomiuri.co.jp/dy/columns/0001/", 'The Magic of Go', 'A weekly go column by Rob Van Zeijst.');
+   $link("http://senseis.xmp.net/?EssentialGoTerms", 'Common Japanese Go Terms',
+         'You have to know what other players are talking about.');
+   $linkend();
 
 
-   section( 'Servers', T_('Other go servers'));
+   $sect( 'History', 'History');
 
-   add_link_page_link("http://www.itsyourturn.com/",
-                     'IYT - It\'s your turn',
-                     T_('Also turn based. Has several other games.'));
-   add_link_page_link("http://www.online-go.com/",
-                     'OGS',
-                     T_('Another turn based. Focus on organised tournament play.'));
-   add_link_page_link("http://www.gokgs.com/",
-                     'KGS',
-                     T_('Kiseido Go Server with java interface'));
-   add_link_page_link("http://www.pandanet.co.jp/English/",
-                     'IGS', T_('A large server for realtime play'));
-   add_link_page_link("http://senseis.xmp.net/?GoServers",
-                     T_('Server list'), T_('A more complete list of servers'));
-   add_link_page_link();
+   $link("http://www.britgo.org/intro/intro1.html#bh", 'A Brief History', 'For you people with short attention spans.');
+   $link("http://gobase.org/information/history/", 'The Extended History', 'In case you\'re an aspiring know-it-all.');
+   $linkend();
+
+
+   $sect( 'Equipment', 'Go books, equipment and software');
+
+   $link("http://www.gobooks.info/", 'Annotated Go Bibliographies', 'A large collection of go book reviews');
+   $link("http://www.slateandshell.com/", 'Slate & Shell');
+   $link("http://www.kiseido.com/", 'Kiseido');
+   $link("http://www.yutopian.com/go/", 'Yutopian');
+   $link("http://www.samarkand.net/", 'Samarkand');
+   $link("http://www.schaakengo.nl/", 'Het Paard', 'European shop');
+   $linkend();
+
+
+   $sect( 'Servers', 'Other go servers');
+
+   $link("http://www.itsyourturn.com/", 'IYT - It\'s your turn', 'Also turn based. Has several other games.');
+   $link("http://www.online-go.com/", 'OGS', 'Another turn based. Focus on organised tournament play.');
+   $link("http://www.gokgs.com/", 'KGS', 'Kiseido Go Server with java interface');
+   $link("http://www.pandanet.co.jp/English/", 'IGS', 'A large server for realtime play');
+   $link("http://senseis.xmp.net/?GoServers", 'Server list', 'A more complete list of servers');
+   $linkend();
 }//make_static_links
+
+function _section( $id, $header )
+{
+   global $TW_;
+   section( $id, $TW_($header) );
+}//_section
+
+function _add_link_page_link( $link=false, $linkdesc='', $extra='' )
+{
+   global $TW_;
+   add_link_page_link( $link, $TW_($linkdesc), $TW_($extra) );
+}//_add_link_page_link
+
+
+function nop()
+{
+}
+
+function save_link_section( $arg, $title )
+{
+   global $f;
+
+   //save_new_faq_entry(dbgmsg,dbtable,tr_group,fid,is_cat,Q,A,Ref, append,translatable,log,chk_mode)
+   $f = AdminFAQ::save_new_faq_entry( 'links.save_link_section', 'Links', 'Docs', 1, true,
+      $title, '', '', true, 'Y', false, 1 );
+}
+
+function save_link_entry( $url='', $text='', $extra='' )
+{
+   global $f;
+
+   //save_new_faq_entry(dbgmsg,dbtable,tr_group,fid,is_cat,Q,A,Ref, append,translatable,log,chk_mode)
+   AdminFAQ::save_new_faq_entry( 'links.save_link_entry', 'Links', 'Docs', /*parent*/$f, false,
+      $text, $extra, $url, true, 'Y', false, 2 );
+}
 
 function load_links()
 { // show only faq-titles
+   global $TW_;
+
    $result = db_query( 'links.load_links',
-      "SELECT entry.*, Question.Text AS Q, Answer.Text AS A, " .
-      "IF(entry.Level=1, entry.SortOrder, parent.SortOrder) AS CatOrder " .
+      "SELECT entry.Level, entry.SortOrder, entry.Reference, " .
+         "Question.Text AS Q, Answer.Text AS A, " .
+         "IF(entry.Level=1, entry.SortOrder, parent.SortOrder) AS CatOrder " .
       "FROM Links AS entry " .
          "INNER JOIN Links AS parent ON parent.ID=entry.Parent " .
          "INNER JOIN TranslationTexts AS Question ON Question.ID=entry.Question " .
          "LEFT JOIN TranslationTexts AS Answer ON Answer.ID=entry.Answer " .
-      "WHERE entry.Level BETWEEN 1 AND 2 " .
+      "WHERE (entry.Level BETWEEN 1 AND 2) " .
+         "AND entry.Hidden='N' AND parent.Hidden='N'" . //need a viewable root
       "ORDER BY CatOrder, entry.Level, entry.SortOrder" );
 
    $last_level = 0;
@@ -159,9 +192,10 @@ function load_links()
          add_link_page_link();
 
       if( $row['Level'] == 1 ) // section
-         section( 'LinkTitle'.$row['SortOrder'], $row['Q'] );
+         section( 'LinkTitle'.$row['SortOrder'], $TW_($row['Q']) );
       elseif( $row['Level'] == 2 ) // link-entry
-         add_link_page_link( $row['Reference'], $row['Q'], $row['A'] );
+         add_link_page_link( $row['Reference'], $TW_($row['Q']), $TW_($row['A']) );
+
       $last_level = $row['Level'];
    }
    if( $last_level > 0 )
