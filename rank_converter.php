0e5dc2d8dae5075025850c5eb67e480ee6eb999c
Jens-Uwe Gaspar <juga@users.sf.net>
2011-04-17 10:10:54 +0000

    changed TranslateGroups: Common -> Game

--- rank_converter.php
+++ rank_converter.php
@@ -17,7 +17,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-$TranslateGroups[] = "Common";
+$TranslateGroups[] = "Game";
 
 require_once 'include/rank_converter.php';
 
