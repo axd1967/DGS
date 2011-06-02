0c58cba80b222708f7144d92965d79008b9b75f4
Jens-Uwe Gaspar <juga@users.sf.net>
2011-06-02 21:02:14 +0000

    fix for static-classes: extended output by STATIC|OBJ-column

--- scripts/find-classes.pl
+++ scripts/find-classes.pl
@@ -3,7 +3,7 @@
 # Usage: scripts/find-classes.pl ` find -type f -name "*.php" ` >classes.txt
 #
 #    show list of all PHP-classes in DGS-code
-#    output-line-format: "prefix <TAB> classname <TAB> line-count <TAB> filename"
+#    output-line-format: "prefix <TAB> classname <TAB> line-count <TAB> STATIC|OBJ <TAB> filename"
 
 my $prefix = "CHECK\t";
 
@@ -12,20 +12,25 @@ foreach my $filename (@ARGV) {
 
   if (open(IN, "<$filename")) {
     my $class = '';
+    my $class_line = 0;
     my $lcnt = 0;
     while (<IN>) {
       chomp;
       $lcnt++;
       if( /^class ([a-z0-9_]+)/i ) {
+         print "$prefix$class\t$class_line\tSTATIC\t$filename\n" if $class;
          $class = $1;
+         $class_line = $lcnt;
       }
       elsif( $class && /function $class\b/i ) {
-         print "$prefix$class\t$lcnt\t$filename\n";
+         print "$prefix$class\t$lcnt\tOBJ\t$filename\n";
          $class = '';
       }
     }
+    print "$prefix$class\t$class_line\tSTATIC\t$filename\n" if $class;
     close IN;
   }
+
 }
 
 exit 0;
