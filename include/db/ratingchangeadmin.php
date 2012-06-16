b7d5d546fb49190dad57e926f2474cf45a418b34
Jens-Uwe Gaspar <juga@users.sf.net>
2012-06-16 12:11:26 +0000

    removed/commented-out translations for admin-pages

--- include/db/ratingchangeadmin.php
+++ include/db/ratingchangeadmin.php
@@ -19,8 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
  /* Author: Jens-Uwe Gaspar */
 
-$TranslateGroups[] = "Admin";
-
 require_once 'include/globals.php';
 require_once 'include/db_classes.php';
 require_once 'include/std_classes.php';
