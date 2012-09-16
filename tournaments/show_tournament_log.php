adf05a1299a670829515ecdd705ee5cbd56ee008
Jens-Uwe Gaspar <juga@users.sf.net>
2012-09-16 16:49:28 +0000

    changed translation-group: Admin -> Tournament

--- tournaments/show_tournament_log.php
+++ tournaments/show_tournament_log.php
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * - needs to be tournament-owner or tournament-director to see entries of THEIR tournament
  */
 
-$TranslateGroups[] = "Admin";
+$TranslateGroups[] = "Tournament";
 
 chdir('..');
 require_once 'include/std_functions.php';
