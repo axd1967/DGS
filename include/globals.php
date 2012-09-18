30e3770913c0b0ff1902a6b78dac7b87cb59b8f6
Jens-Uwe Gaspar <juga@users.sf.net>
2012-09-18 15:27:12 +0000

    updated CSS-version

--- include/globals.php
+++ include/globals.php
@@ -26,7 +26,7 @@ require_once 'include/config-local.php';
  */
 
 define('DGS_VERSION', '1.0.15');
-define('CSS_VERSION', '20120910174100'); // take UTC-timestamp of skins/dragon/screen.css
+define('CSS_VERSION', '20120918150000'); // take UTC-timestamp of skins/dragon/screen.css
 
 // global version of quick-suite: increased with each release
 define('QUICK_VERSION', 2);
