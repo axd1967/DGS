0cda9809d02d4e7527bf007ecd601e0707395c1e
Jens-Uwe Gaspar <juga@users.sf.net>
2012-09-20 15:02:41 +0000

    added link to goodies-page, re-structured goodies-page to use standard DGS-layout

--- goodies/index.php
+++ goodies/index.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Dragon Go Server
-Copyright (C) 2001-2012  Erik Ouchterlony, Rod Ival
+Copyright (C) 2001-2012  Erik Ouchterlony, Jens-Uwe Gaspar
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
@@ -17,61 +17,46 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+$TranslateGroups[] = "Docs";
+
+chdir('..');
+require_once 'include/std_functions.php';
+
+$GLOBALS['ThePage'] = new Page('Goodies');
+
+
 {
-   $NOW = time();
-   header('Expires: ' . gmdate('D, d M Y H:i:s',$NOW) . ' GMT');
-   header('Last-Modified: ' . gmdate('D, d M Y H:i:s',$NOW-30) . ' GMT');
-   header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0'); // HTTP/1.1
-   header('Pragma: no-cache');                                              // HTTP/1.0
-}
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-                      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+   connect2mysql();
+   $logged_in = who_is_logged($player_row);
 
-<head>
- <title>DGS goodies</title>
- <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+   $page = "goodies/index.php";
 
- <style type="text/css">
-   li {
-      margin-top: 2em;
-      border-top: 1px solid gray;
-   }
-   pre {
-      margin: 0.5em 0px;
-      background-color: #f0f0f0;
-      overflow: visible;
-   }
- </style>
-</head>
-
-<body>
- <h3>Dragon Go goodies</h3>
- <div>
-  <h4>GreaseMonkey scripts</h4>
-  <em>
-   Depending of your GreaseMonkey version, either Click or Right-Click
-   a desired name link to install it:
-  </em>
-  <ul>
-<?php
-{
    $files = array();
-   if( $fh = opendir('.') )
+   $path_goodies = 'goodies';
+   if( $fh = opendir($path_goodies) )
    {
       while( false !== ($file = readdir($fh)) )
       {
          if( substr($file,-8) == '.user.js' )
-            $files[] = $file;
+            $files[] = "$path_goodies/$file";
       }
       closedir($fh);
-   } else echo "Error: open dir fails<br />";
+   }
+   else
+      echo "Error: open dir fails<br />";
    asort( $files);
+
+
+   start_page( T_('Goodies'), true, $logged_in, $player_row );
+
+   echo '<h3>', T_('Dragon Go Goodies'), "</h3>\n",
+      "<div>\n",
+      "<h4>", T_('GreaseMonkey Scripts'), "</h4>\n",
+      "<em>", T_('Depending of your GreaseMonkey version, either Click or Right-Click on a desired name-link to install it:'), "</em>\n",
+      "<ul>\n";
 
    foreach( $files as $file )
    {
-      //echo "<p>$file</p>\n";
       $fh = fopen($file, 'r');
       $txt = fread($fh, filesize($file));
       fclose($fh);
@@ -93,36 +78,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
          $infos);
 
       $txt = '';
-
       $str = "- $description\n";
       if( $infos )
          $str.= "<br />- Script infos:\n";
       $txt.= "<dt>\n$str</dt>\n";
 
-      if( !$infos )
-         $str = '';
-      else
-         $str = "<pre>$infos</pre>\n";
+      $str = ( $infos ) ? "<pre>$infos</pre>\n" : '';
       $txt.= "<dd>\n$str</dd>\n";
-
       $txt = "<dl>\n$txt</dl>\n";
 
       //disabling the caches while installing the script cause me some problems.
       //the ?date=$NOW ensure to reload the file (fake no-cache)
       //the $NOW.user.js give a fake extension to feed the GreaseMonkey install
       $str = "<p><strong>Name: <a href='./$file?date=$NOW.user.js'>$name</a></strong></p>\n";
-      //if the .htaccess modules could accept:
-      //ExpiresActive On
-      //ExpiresDefault A1
-      //use the simple:
-      //$str = "<p><strong>Name: <a href='./$file'>$name</a></strong></p>\n";
 
-      $txt = "<li>$str$txt</li>\n";
-
-      echo $txt;
+      echo "<li>$str$txt</li>\n";
    } //$files
+
+   echo "</ul>\n",
+      "</div>\n";
+
+   end_page();
 }
+
 ?>
-  </ul>
- </div>
-</body></html>
