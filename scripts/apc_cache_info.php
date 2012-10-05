b3c8e96c4a216927589c480b36a458f60c909599
Jens-Uwe Gaspar <juga@users.sf.net>
2012-10-05 17:42:59 +0000

    added output of user-cache-entries

--- scripts/apc_cache_info.php
+++ scripts/apc_cache_info.php
@@ -4,5 +4,8 @@ echo "<html><body bgcolor=white>\n";
 echo "<p><b>APC Cache Info (System):</b><br>\n";
 echo '<pre>', print_r(apc_cache_info(), true), "</pre>\n";
 
+echo "<p><b>APC Cache Info (User):</b><br>\n";
+echo '<pre>', print_r(apc_cache_info('user'), true), "</pre>\n";
+
 echo "</body></html>\n";
 ?>
