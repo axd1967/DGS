4fe58eea6c672b35dcbf786ad33dec65ca68a38a
Jens-Uwe Gaspar <juga@users.sf.net>
2012-11-19 13:43:49 +0000

    limiting waiting-room entries a user can offer also for starting MPGs even if wanting to invite-only-MPG

--- include/game_functions.php
+++ include/game_functions.php
@@ -3176,14 +3176,15 @@ class GameSetupChecker
    /*! \brief Checks max. count of existing waiting-room-entries. */
    function check_wroom_count( $view, $uid, &$errors )
    {
-      if( $view == GSETVIEW_SIMPLE || $view == GSETVIEW_EXPERT || $view == GSETVIEW_FAIRKOMI )
+      if( $view == GSETVIEW_SIMPLE || $view == GSETVIEW_EXPERT || $view == GSETVIEW_FAIRKOMI || $view == GSETVIEW_MPGAME )
       {
          $row = mysql_single_fetch( "GameSetupChecker::check_wroom_count.count_wr($uid,$view)",
                "SELECT COUNT(*) AS X_Count FROM Waitingroom WHERE uid=$uid" );
          if( $row && (int)@$row['X_Count'] >= WROOM_MAX_ENTRIES )
             $errors[] = sprintf( T_('Max. number of own waiting-room entries [%s] has been reached.'), WROOM_MAX_ENTRIES ) . "\n" .
                T_('You can save the form as template for later, delete one waiting-room offer or wait till they are joined.') . "\n" .
-               T_('Number of games for same offer can be set with \'Number of games to add\'.');
+               ( $view != GSETVIEW_MPGAME ?
+                  T_('Number of games for same offer can be set with \'Number of games to add\'.') : '' );
       }
    }//check_wroom_count
 
