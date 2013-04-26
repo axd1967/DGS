aadf7da1219b9a135d015c902d0faf3fe7ee2dda
Jens-Uwe Gaspar <juga@users.sf.net>
2013-04-26 08:34:34 +0000

    - added consistency-checks/fixes on Players-table for multi-player-games - cleanup: more debug-output, more comments, small coding changes

--- scripts/player_consistency.php
+++ scripts/player_consistency.php
@@ -30,9 +30,11 @@ define('DEBUG',0);
 
 //---------------
 require_once( "include/table_columns.php" );
-function echo_query( $query, $rowhdr=20, $colsize=80, $colwrap='cut' )
+
+
+function echo_query( $dbgmsg, $query, $rowhdr=20, $colsize=80, $colwrap='cut' )
 {
-   $result = mysql_query( $query );
+   $result = db_query( "player_consistency.echo_query.$dbgmsg", $query );
 
    $mysqlerror = @mysql_error();
    if( $mysqlerror )
@@ -99,16 +101,16 @@ function echo_query( $query, $rowhdr=20, $colsize=80, $colwrap='cut' )
    echo "\n</table><br>\n";
 
    return $numrows;
-}
+}//echo_query
 
-function explain_query($s) {
+function explain_query( $dbgmsg, $s ) {
    if(DEBUG)
    {
      echo "<BR>EXPLAIN $s;<BR>";
-     echo_query( "EXPLAIN ".$s);
-     echo_query( $s); // show contents
+     echo_query( $dbgmsg, "EXPLAIN $s" );
+     echo_query( $dbgmsg, $s); // show contents
    }
-   return mysql_query( $s); // return query-results
+   return db_query( "player_consistency.explain_query.$dbgmsg", $s); // return query-results
 }
 //---------------
 
@@ -125,6 +127,17 @@ function uid_clause( $fld, $oper)
 }
 
 
+/*!
+ * \brief Counts and returns player-ids with incorrect counts for given query-restrictions.
+ * \param $pfld Players-table field to check count for
+ * \param $gwhr game-WHERE clause restricting query (e.g. on Status/Rated or other field)
+ * \param $gwhrB game-WHERE clause restricting query for Black-player
+ * \param $gwhrW game-WHERE clause restricting query for White-player
+ * \note multi-player-games are counted in automatically for Players-field $pfld 'Running' and 'Finished' only,
+ *       because MPG is not a rated games and 'RatedGames/Won/Lost' are only counted for rated games!
+ * \return entries only for players with incorrect counts found:
+ *       arr( Players.ID => arry( Players.<$pfld> count, real games-count )
+ */
 function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
 {
    $tstart = getmicrotime();
@@ -132,58 +145,74 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
 
    global $limit, $sqlbuf;
 
-   $query = "SELECT $sqlbuf Black_ID as idB, COUNT(*) as cntB"
+   $query = "SELECT $sqlbuf Black_ID AS idB, COUNT(*) AS cntB"
           . " FROM Games"
           . " WHERE ".$gwhr.$gwhrB.uid_clause( 'Black_ID', 'AND')
+            . " AND GameType='".GAMETYPE_GO."'" // no MPG
           . " GROUP BY Black_ID"
           ;
-   $resB = explain_query( $query)
-      or die( $nam.".B: " . mysql_error());
+   $resB = explain_query( "$nam.B1", $query)
+      or die( "$nam.B2: " . mysql_error());
 
-   $plB = array();
+   $plB = array(); // Players.ID => games-count
    while( $rowB = mysql_fetch_assoc($resB) )
-   {
       $plB[$rowB['idB']] = $rowB['cntB'];
-   }
    mysql_free_result($resB);
 
 
-   $query = "SELECT $sqlbuf White_ID as idW, COUNT(*) as cntW"
+   $query = "SELECT $sqlbuf White_ID AS idW, COUNT(*) AS cntW"
           . " FROM Games"
           . " WHERE ".$gwhr.$gwhrW.uid_clause( 'White_ID', 'AND')
+            . " AND GameType='".GAMETYPE_GO."'" // no MPG
           . " GROUP BY White_ID"
           ;
-   $resW = explain_query( $query)
-      or die( $nam.".W: " . mysql_error());
+   $resW = explain_query( "$name.W1", $query)
+      or die( "$nam.W2: " . mysql_error());
 
-   $plW = array();
+   $plW = array(); // Players.ID => games-count
    while( $rowW = mysql_fetch_assoc($resW) )
-   {
       $plW[$rowW['idW']] = $rowW['cntW'];
-   }
    mysql_free_result($resW);
 
 
-   $query = "SELECT $sqlbuf ID as idP, $pfld as cntP"
-          . " FROM Players".uid_clause( 'ID', 'WHERE');
-   $resP = explain_query( $query)
-      or die( $nam.".P: " . mysql_error());
+   $plMPG = array(); // Players.ID => games-count
+   if( $pfld == 'Running' || $pfld == 'Finished' ) // count MPGs
+   {
+      $query = "SELECT $sqlbuf GP.uid, COUNT(*) AS cntMPG"
+             . " FROM GamePlayers AS GP  INNER JOIN Games AS G ON G.ID=GP.gid"
+             . " WHERE ".$gwhr.uid_clause( 'GP.uid', 'AND')
+             . " GROUP BY GP.uid"
+             ;
+      $resMPG = explain_query( "$name.MPG1", $query)
+         or die( "$nam.MPG2: " . mysql_error());
+
+      while( $rowMPG = mysql_fetch_assoc($resMPG) )
+         $plMPG[$rowMPG['uid']] = $rowW['cntMPG'];
+      mysql_free_result($resMPG);
+   }
+
+
+   $query = "SELECT $sqlbuf ID AS idP, $pfld AS cntP FROM Players".uid_clause( 'ID', 'WHERE');
+   $resP = explain_query( "$nam.P1", $query)
+      or die( "$nam.P2: " . mysql_error());
 
    while( $rowP = mysql_fetch_assoc($resP) )
    {
       extract($rowP);
-      $sum = @$plB[$idP] + @$plW[$idP];
+      $sum = @$plB[$idP] + @$plW[$idP] + @$plMPG[$idP];
       if(DEBUG)
-         echo "\n<br>P:$idP/$cntP/$sum  B:".@$plB[$idP]." W:".@$plW[$idP];
+         echo "\n<br>P:$idP/$cntP/$sum  B:".@$plB[$idP]." W:".@$plW[$idP]." MPG:".@$plMPG[$idP];;
       if( $cntP != $sum )
-         $diff[$idP] = array( $cntP, $sum);
+         $diff[$idP] = array( $cntP, $sum );
    }
    mysql_free_result($resP);
+
    krsort($diff, SORT_NUMERIC);
 
    echo "\n<br>Needed ($nam): " . sprintf("%1.3fs", (getmicrotime() - $tstart));
    return $diff;
-}
+}//cnt_diff
+
 
 
 
@@ -203,31 +232,24 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
    if( !(@$player_row['admin_level'] & ADMIN_DATABASE) )
       error('adminlevel_too_low', 'scripts.player_consistency');
 
+/*
+   URL-args:
+      uid    = '' | 123 | 123,456      ; x,y = range x..y
+      limit  = '' | 10
+      buffer = '' | no
+*/
    //uid could be '12,27' meaning from player=12 to player=27
    @list( $uid1, $uid2) = explode( ',', @$_REQUEST['uid']);
 
    //limit could be '55,10'
-   if( ($lim=@$_REQUEST['limit']) > '' )
-      $limit = " LIMIT $lim";
-   else
-      $limit = "";
-
-
-   if( @$_REQUEST['buffer'] == 'no' )
-      $sqlbuf = "";
-   else
-      $sqlbuf = "SQL_BUFFER_RESULT";
+   $limit = ( ($lim=@$_REQUEST['limit']) > '' ) ? " LIMIT $lim" : '';
+   $sqlbuf = ( @$_REQUEST['buffer'] == 'no' ) ? '' : 'SQL_BUFFER_RESULT';
 
 
    $page = $_SERVER['PHP_SELF'];
    $page_args = array();
    if( $uid1 > '' )
-   {
-      if( $uid2 > '' )
-         $page_args['uid'] = $uid1.','.$uid2;
-      else
-         $page_args['uid'] = $uid1;
-   }
+      $page_args['uid'] = ( $uid2 > '' ) ? $uid1.','.$uid2 : $uid1;
    if( $lim > '' )
       $page_args['limit'] = $lim;
    if( $sqlbuf == '' )
@@ -269,6 +291,7 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
 
    echo "<br>On ", date(DATE_FMT, $NOW), ' GMT<br>';
 
+
 //-----------------
 
    $begin = getmicrotime();
@@ -279,8 +302,8 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
             . " AND (White_ID<=0 OR Black_ID<=0 OR White_ID=Black_ID)"
           . " ORDER BY ID DESC"
           ;
-   $result = explain_query( $query)
-      or die("pID.A: " . mysql_error());
+   $result = explain_query( "pID.find_bad_players1", $query)
+      or die("pID.find_bad_players2: " . mysql_error());
 
    $err = 0;
    while( $row = mysql_fetch_assoc($result) )
@@ -299,7 +322,7 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
 
 //-----------------
 
-   $diff = cnt_diff( 'Run', 'Running', 'Status'.IS_STARTED_GAME, "", "");
+   $diff = cnt_diff( 'Run', 'Running', 'Status'.IS_STARTED_GAME);
    foreach( $diff as $ID => $ary )
    {
       list( $cnt, $sum) = $ary;
@@ -310,7 +333,7 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
    echo "\n<br>Running Done.";
 
 
-   $diff = cnt_diff( 'Fin', 'Finished', "Status='".GAME_STATUS_FINISHED."'", "", "");
+   $diff = cnt_diff( 'Fin', 'Finished', "Status='".GAME_STATUS_FINISHED."'");
    foreach( $diff as $ID => $ary )
    {
       list( $cnt, $sum) = $ary;
@@ -321,7 +344,7 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
    echo "\n<br>Finished Done.";
 
 
-   $diff = cnt_diff( 'Rat', 'RatedGames', "Status='".GAME_STATUS_FINISHED."'$is_rated", "", "");
+   $diff = cnt_diff( 'Rat', 'RatedGames', "Status='".GAME_STATUS_FINISHED."' $is_rated");
    foreach( $diff as $ID => $ary )
    {
       list( $cnt, $sum) = $ary;
@@ -332,7 +355,7 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
    echo "\n<br>RatedGames Done.";
 
 
-   $diff = cnt_diff( 'Won', 'Won', "Status='".GAME_STATUS_FINISHED."'$is_rated", " AND Score<0", " AND Score>0");
+   $diff = cnt_diff( 'Won', 'Won', "Status='".GAME_STATUS_FINISHED."' $is_rated", " AND Score<0", " AND Score>0");
    foreach( $diff as $ID => $ary )
    {
       list( $cnt, $sum) = $ary;
@@ -373,14 +396,14 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
 
 
    $begin = getmicrotime();
-   //RatedGames && Ratinglog consistency
+   //check consistency: RatedGames && Ratinglog
    $query = "SELECT $sqlbuf Players.ID, COUNT(Ratinglog.ID) AS Log, RatedGames " .
             "FROM Players LEFT JOIN Ratinglog ON Ratinglog.uid=Players.ID " .
             "GROUP BY Players.ID HAVING Log!=RatedGames"
             .uid_clause( 'Players.ID', 'AND')
             ." $limit";
-   $result = explain_query( $query)
-      or die("Log.A: " . mysql_error());
+   $result = explain_query( "Ratinglog1", $query)
+      or die("Ratinglog2: " . mysql_error());
 
    $err = 0;
    while( $row = mysql_fetch_assoc($result) )
@@ -398,7 +421,7 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
 
 
    $begin = getmicrotime();
-   //Various checks
+   //Various checks: Rating2 within boundaries of RatingMin/Max (if user RATED), valid ClockUsed
    $query = "SELECT $sqlbuf " .
             "Players.ID, ClockUsed, RatingStatus, Rating2, RatingMin, RatingMax " .
             "FROM Players " .
@@ -414,8 +437,8 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
             .uid_clause( 'Players.ID', 'AND')
             ." ORDER BY Players.ID$limit";
    //echo "\n<br>MiscQry=".$query;
-   $result = explain_query( $query)
-      or die("Mis.A: " . mysql_error());
+   $result = explain_query( "Misc1", $query)
+      or die("Misc2: " . mysql_error());
 
    $err = 0;
    while( $row = mysql_fetch_assoc($result) )
@@ -434,11 +457,37 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
 
 //-----------------
 
-   //TODO TODO fix Players.GamesMPG
-   //loop over result of:
-   //  SELECT GP.uid, COUNT(*) AS X_Count FROM GamePlayers AS GP INNER JOIN Games AS G ON G.ID=GP.gid WHERE G.Status='SETUP' AND ((GP.Flags & ".GPFLAG_JOINED.") or (GP.Flags & ".GPFLAGS_RESERVED_INVITATION.")=".GPFLAGS_RESERVED_INVITATION.") GROUP BY GP.uid
-   //  UPDATE Players SET GamesMPG=$X_Count WHERE ID=$uid LIMIT 1
+   $begin = getmicrotime();
+   echo "\n<br>";
 
+   // check Players.GamesMPG
+   $query = "SELECT $sqlbuf GP.uid, COUNT(*) AS X_Count, P.GamesMPG "
+      . "FROM GamePlayers AS GP "
+         . "INNER JOIN Games AS G ON G.ID=GP.gid "
+         . "INNER JOIN Players AS P ON P.ID=GP.uid "
+      . "WHERE G.Status='".GAME_STATUS_SETUP."' "
+         . "AND ((GP.Flags & ".GPFLAG_JOINED.") OR (GP.Flags & ".GPFLAGS_RESERVED_INVITATION.")=".GPFLAGS_RESERVED_INVITATION.") "
+         . uid_clause( 'GP.uid', 'AND' )
+      . "GROUP BY GP.uid $limit";
+   $result = explain_query( "Players.GamesMPG1", $query)
+      or die("Players.GamesMPG2: " . mysql_error());
+   $err = 0;
+   while( $row = mysql_fetch_assoc($result) )
+   {
+      $uid = $row['uid'];
+      $cntG = $row['X_Count'];
+      $cntP = $row['GamesMPG'];
+      if( $cntG != $cntP )
+      {
+         $err++;
+         echo "\n<br>ID: $uid  GamesMPG: $cntP  Should be: $cntG";
+         dbg_query("UPDATE Players SET GamesMPG=$cntG WHERE ID=$uid LIMIT 1");
+      }
+   }
+   if( $err )
+      echo "\n<br>--- $err error(s) found.";
+
+   echo "\n<br>Players.GamesMPG Done.";
 
 
 //----------------- Player-related tables (ConfigBoard, ConfigPages, UserQuota)
@@ -447,7 +496,7 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
    echo "\n<br>";
 
    // check missing ConfigBoard/ConfigPages/UserQuota
-   $query = "SELECT P.ID, IFNULL(CB.User_ID,0) AS XCB_uid, " .
+   $query = "SELECT $sqlbuf P.ID, IFNULL(CB.User_ID,0) AS XCB_uid, " .
                "IFNULL(CP.User_ID,0) AS XCP_uid, IFNULL(UQ.uid,0) AS XUQ_uid " .
             "FROM Players AS P " .
                "LEFT JOIN ConfigBoard AS CB ON CB.User_ID=P.ID " .
@@ -456,8 +505,8 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
             uid_clause( 'P.ID', 'AND' ) .
             "HAVING (XCB_uid=0 OR XCP_uid=0 OR XUQ_uid=0) " .
             "ORDER BY P.ID $limit";
-   $result = explain_query($query)
-      or die("PlayersFK.A: " . mysql_error());
+   $result = explain_query( "PlayersFK1", $query)
+      or die("PlayersFK2: " . mysql_error());
    $err = 0;
    while( $row = mysql_fetch_assoc($result) )
    {
@@ -500,8 +549,8 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
    $query = "SELECT ID, CountMsgNew FROM Players WHERE CountMsgNew>=0 " .
             uid_clause( 'ID', 'AND' ) .
             "ORDER BY ID $limit";
-   $result = explain_query($query)
-      or die("CountMsgNew.A: " . mysql_error());
+   $result = explain_query( "CountMsgNew1", $query)
+      or die("CountMsgNew2: " . mysql_error());
    $err = 0;
    while( $row = mysql_fetch_assoc($result) )
    {
@@ -528,8 +577,8 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
    $query = "SELECT ID, CountFeatNew FROM Players WHERE CountFeatNew>=0 " .
             uid_clause( 'ID', 'AND' ) .
             "ORDER BY ID $limit";
-   $result = explain_query($query)
-      or die("CountFeatNew.A: " . mysql_error());
+   $result = explain_query( "CountFeatNew1", $query)
+      or die("CountFeatNew2: " . mysql_error());
    $err = 0;
    while( $row = mysql_fetch_assoc($result) )
    {
@@ -559,8 +608,8 @@ function cnt_diff( $nam, $pfld, $gwhr, $gwhrB='', $gwhrW='')
    $query = "SELECT ID, CountBulletinNew FROM Players WHERE CountBulletinNew>=0 " .
             uid_clause( 'ID', 'AND' ) .
             "ORDER BY ID $limit";
-   $result = explain_query($query)
-      or die("CountBulletinNew.A: " . mysql_error());
+   $result = explain_query("CountBulletinNew1", $query)
+      or die("CountBulletinNew2: " . mysql_error());
    $err = 0;
    while( $row = mysql_fetch_assoc($result) )
    {
