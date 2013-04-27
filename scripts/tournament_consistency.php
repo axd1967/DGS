329af7bdd1acf5780acff77219e129498dfddc78
Jens-Uwe Gaspar <juga@users.sf.net>
2013-04-27 11:58:38 +0000

    - add form to enter mandatory tournament-id - basic cleanup

--- scripts/tournament_consistency.php
+++ scripts/tournament_consistency.php
@@ -18,13 +18,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
-// Checks and fixes errors in Forum fields in the database.
-// see section 'Calculated database fields' in 'specs/forums.txt' for necessary forum-updates !!
+// Checks and fixes errors for tournaments in the database.
 
 chdir('..');
 require_once 'include/std_functions.php';
 require_once 'include/gui_functions.php';
 require_once 'include/table_columns.php';
+require_once 'include/form_functions.php';
 require_once 'tournaments/include/tournament.php';
 
 define('SEPLINE', "\n<p><hr>\n");
@@ -34,7 +34,7 @@ define('SEPLINE', "\n<p><hr>\n");
    $beginall = getmicrotime();
    disable_cache();
    connect2mysql();
-   set_time_limit(300); //max. 5min
+   set_time_limit(0); // don't want script-break during "transaction" with multi-db-queries
 
    $logged_in = who_is_logged( $player_row);
    if( !$logged_in )
@@ -44,9 +44,8 @@ define('SEPLINE', "\n<p><hr>\n");
    if( !(@$player_row['admin_level'] & ADMIN_DATABASE) )
       error('adminlevel_too_low', 'scripts.tournament_consistency');
 
-   $page = $_SERVER['PHP_SELF'];
-   $page_args = array();
-   //$page_args['limit'] = $lim;
+   $tid = (int)@$_REQUEST['tid'];
+   $do_it = @$_REQUEST['do_it'];
 
    start_html( 'tournament_consistency', 0, '',
       "  table.Table { border:0; background: #c0c0c0; }\n" .
@@ -54,66 +53,43 @@ define('SEPLINE', "\n<p><hr>\n");
       "  tr.Row2 { background: #dddddd; }\n" .
       "  tr.hil { background: #ffb010; }" );
 
-   if( $do_it = @$_REQUEST['do_it'] )
-   {
-      echo "<p>*** Fixes errors ***"
-         ."<br>".anchor(make_url($page, $page_args), 'Just show it')
-         ."</p>";
-   }
-   else
-   {
-      $tmp = array_merge($page_args,array('do_it' => 1));
-      $tmp2 = array_merge($page_args,array('do_it' => 1, 'withnew' => 1 ));
-      echo "<p>(just show needed queries)"
-         ."<br>".anchor(make_url($page, $page_args), 'Show it again')
-         ."<br>".anchor(make_url($page, $tmp), '[Validate it]')
-         ."</p>";
-   }
-   $debug = !$do_it;
-   $cnt_err = 0;
+   $page = "tournament_consistency.php";
+   $form = new Form( 'tourneyconsistency', $page, FORM_GET );
 
-   echo "On ", date(DATE_FMT5, $NOW);
+   $form->add_row( array(
+         'DESCRIPTION', 'tid',
+         'TEXTINPUT',   'tid', 12, 12, $tid,
+         'TEXT',        'tournament-id required!', ));
+   $form->add_empty_row();
+   $form->add_row( array(
+         'SUBMITBUTTON', 'check_it', 'Check Only',
+         'TEXT', SMALL_SPACING,
+         'SUBMITBUTTON', 'do_it', 'Check and Fix it!', ));
 
-
-//----------------- Fix Tournament.RegisteredTP
-
-   $begin = getmicrotime();
-   echo SEPLINE;
-   echo "Fix Tournament.RegisteredTP ...<br>\n";
-
-   // note: join slightly faster than using subquery: Posts where User_ID not in (select ID from Players)
-   $result = db_query( 'tournament_consistency.check_authors',
-      "SELECT TP.tid, T.RegisteredTP, COUNT(*) AS X_Count " .
-      "FROM TournamentParticipant AS TP INNER JOIN Tournament AS T ON T.ID=TP.tid " .
-      "WHERE TP.Status='".TP_STATUS_REGISTER."' GROUP BY TP.tid HAVING T.RegisteredTP <> X_Count" );
-   $upd_arr = array();
-   while( $row = mysql_fetch_array( $result ) )
-   {
-      $cnt_err++;
-      $tid = $row['tid'];
-      $count = $row['X_Count'];
-      echo sprintf( "Tournament #%s: wrong RegisteredTP found: [%s] -> [%s]<br>\n",
-         $tid, $row['RegisteredTP'], $count );
-      $upd_arr[] = "UPDATE Tournament SET RegisteredTP=$count WHERE ID=$tid LIMIT 1";
-   }
-   mysql_free_result($result);
-   $cnt_err += count($upd_arr);
-
-   do_updates( 'tournament_consistency.fix_tournament.RegisteredTP', $upd_arr, $do_it );
-
-   echo "\n<br>Needed: " . sprintf("%1.3fs", (getmicrotime() - $begin))
-      , " - Tournament.RegisteredTP check Done.";
+   echo "<p><h3 class=center>Tournament Consistency:</h3>\n";
+   $form->echo_string();
 
 //-----------------
 
-   echo SEPLINE;
+   echo "On ", date(DATE_FMT5, $NOW);
 
+   $cnt_err = 0;
+   if( $tid > 0 )
+   {
+      if( $do_it )
+         echo "<p>*** Fixes errors ***</p>";
+
+      $cnt_err += fix_tournament_RegisteredTP( $do_it );
+   }
+
+   echo SEPLINE;
    echo sprintf( "<font color=red><b>Found %s errors (inconsistencies).</b></font><br>\n", $cnt_err );
 
    echo "\n<br>Needed (all): " . sprintf("%1.3fs", (getmicrotime() - $beginall));
    echo "\n<br>Done!!!\n";
    end_html();
 }//main
+
 
 
 function do_updates( $dbgmsg, $upd_arr, $do_it )
@@ -129,6 +105,40 @@ function do_updates( $dbgmsg, $upd_arr, $do_it )
          db_query( $dbgmsg, $query );
    }
    echo '</pre>';
-}
+}//do_updates
+
+
+// Fix Tournament.RegisteredTP
+function fix_tournament_RegisteredTP( $do_it )
+{
+   $begin = getmicrotime();
+   echo SEPLINE;
+   echo "Fix Tournament.RegisteredTP ...<br>\n";
+
+   // note: join slightly faster than using subquery: Posts where User_ID not in (select ID from Players)
+   $result = db_query( 'tournament_consistency.fix_tournament_registered_tp.check_authors',
+      "SELECT TP.tid, T.RegisteredTP, COUNT(*) AS X_Count " .
+      "FROM TournamentParticipant AS TP INNER JOIN Tournament AS T ON T.ID=TP.tid " .
+      "WHERE TP.Status='".TP_STATUS_REGISTER."' GROUP BY TP.tid HAVING T.RegisteredTP <> X_Count" );
+   $upd_arr = array();
+   $cnt_err = 0;
+   while( $row = mysql_fetch_array($result) )
+   {
+      $tid = $row['tid'];
+      $count = $row['X_Count'];
+      echo sprintf( "Tournament #%s: wrong RegisteredTP found: [%s] -> [%s]<br>\n",
+         $tid, $row['RegisteredTP'], $count );
+      $upd_arr[] = "UPDATE Tournament SET RegisteredTP=$count WHERE ID=$tid LIMIT 1";
+   }
+   mysql_free_result($result);
+   $cnt_err += count($upd_arr);
+
+   do_updates( 'tournament_consistency.fix_tournament.RegisteredTP', $upd_arr, $do_it );
+
+   echo "\n<br>Needed: " . sprintf("%1.3fs", (getmicrotime() - $begin))
+      , " - Tournament.RegisteredTP check Done.";
+
+   return $cnt_err;
+}//fix_tournament_registered_tp
 
 ?>
