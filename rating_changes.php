f2e452ad2dad84241873b39e5295bc7288a6f26d
Jens-Uwe Gaspar <juga@users.sf.net>
2013-10-06 02:06:43 +0200

    bugfix: correct translation-group 'User' -> 'Users'

--- rating_changes.php
+++ rating_changes.php
@@ -17,7 +17,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-$TranslateGroups[] = "User";
+$TranslateGroups[] = "Users";
 
 require_once 'include/classlib_user.php';
 require_once 'include/form_functions.php';
