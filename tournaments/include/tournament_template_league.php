a3f07f8a4a5fba889fbf71b5de819b189d6927e3
Jens-Uwe Gaspar <juga@users.sf.net>
2016-11-09 01:35:00 +0100

    extend rank-summary checks for correct relegations of pool-users for league-tournaments
    - check for double-ranks that may need tie-breaking
    - check for users with rank-based relegations that doesn't match the league-configuration
    - check for too many relegations with promoted, demoted or staying pool-users
    - reporting incorrect relegations as warnings to allow T-director to overwrite

--- tournaments/include/tournament_template_league.php
+++ tournaments/include/tournament_template_league.php
@@ -81,6 +81,7 @@ abstract class TournamentTemplateLeague extends TournamentTemplateRoundRobin
          $tpool_iterator = TournamentPool::load_tournament_pools( $tpool_iterator, $tid, 1, 0, TPOOL_LOADOPT_USER_HANDLE );
 
          $this->check_pools_invalid_ranks_flags( $tid, $tround, $tpool_iterator, $errors );
+         $this->check_pools_relegations( $tourney->Type, $tround, $tpool_iterator, $warnings );
       }
    }//check_pools_finish_tournament_type_specific
 
@@ -132,6 +133,105 @@ abstract class TournamentTemplateLeague extends TournamentTemplateRoundRobin
             implode(', ', $arr_users_invalid_relegations));
       }
    }//check_pools_invalid_ranks_flags
+
+   /*! \brief Checks that relegations are done correctly, but allow T-director to overwrite (so issue as warnings). */
+   protected function check_pools_relegations( $tourney_type, $tround, $tpool_iterator, &$warnings )
+   {
+      global $base_path;
+      $tid = $tround->tid;
+      $uri_edit_user = $base_path . "tournaments/roundrobin/edit_ranks.php?tid=$tid&uid=%s";
+
+      $cnt_double_ranks = array(); // [ tier-pool-key => [ Rank => COUNT, ... ] ]
+      $arr_double_ranks = array(); // [ pool-name => 1, ... ]
+      $arr_should_promote = array(); // [ user-edit-link, ... ]
+      $arr_should_demote = array(); // [ user-edit-link, ... ]
+      $arr_should_stay = array(); // [ user-edit-link, ... ]
+      $cnt_relegations = array(); // [ pool-name => [ TPOOL_FLAG_PROMOTE|DEMOTE|0 => COUNT, ... ] ]
+
+      $tpool_iterator->resetListIterator();
+      while ( list(,$arr_item) = $tpool_iterator->getListIterator() )
+      {
+         list( $tpool, $orow ) = $arr_item;
+
+         if ( $tpool->Rank > 0 )
+         {
+            $tier_pool_key = TournamentUtils::encode_tier_pool_key( $tpool->Tier, $tpool->Pool );
+            $pool_name = PoolViewer::format_tier_pool( $tourney_type, $tpool->Tier, $tpool->Pool, true );
+            $is_promote = ( $tpool->Flags & TPOOL_FLAG_PROMOTE );
+            $is_demote = ( $tpool->Flags & TPOOL_FLAG_DEMOTE );
+            $uid = $tpool->uid;
+            $handle = $tpool->User->Handle;
+
+            // check for double-ranks -> may need tie-breaking
+            if ( !isset($cnt_double_ranks[$tier_pool_key][$tpool->Rank]) )
+               $cnt_double_ranks[$tier_pool_key][$tpool->Rank] = 1;
+            else
+               $cnt_double_ranks[$tier_pool_key][$tpool->Rank]++;
+            if ( $cnt_double_ranks[$tier_pool_key][$tpool->Rank] == 2 ) // report only once
+               $arr_double_ranks[$pool_name] = 1;
+
+            // check for users with rank-based relegations that doesn't match league-configuration
+            $user_edit_link = anchor( sprintf($uri_edit_user, $uid), "$pool_name: $handle" );
+            if ( $tpool->Rank <= $tround->PromoteRanks && !$is_promote )
+               $arr_should_promote[] = $user_edit_link; // user should be promoted
+            if ( $tpool->Rank >= $tround->DemoteStartRank && !$is_demote )
+               $arr_should_demote[] = $user_edit_link; // user should be demoted
+            elseif ( $tpool->Rank > $tround->PromoteRanks && $tpool->Rank < $tround->DemoteStartRank && ($is_promote || $is_demote) )
+               $arr_should_stay[] = $user_edit_link; // user should stay in same league
+
+            // check for too many relegations (promote / demote / stay)
+            if ( !($is_promote && $is_demote) ) // skip bad relegation-state
+            {
+               if ( !isset($cnt_relegations[$pool_name]) )
+                  $cnt_relegations[$pool_name] = array( TPOOL_FLAG_PROMOTE => 0, TPOOL_FLAG_DEMOTE => 0, 0 => 0 );
+               $cnt_relegations[$pool_name][$tpool->Flags & TPOOL_FLAG_RELEGATIONS]++;
+            }
+         }
+      }
+
+      if ( count($arr_double_ranks) )
+         $warnings[] = sprintf( T_('Found pools [%s] with non-unique ranks, which may need tie-breaking.'),
+            implode(', ', array_keys($arr_double_ranks)) );
+
+      if ( count($arr_should_promote) )
+         $warnings[] = sprintf( T_('Found pool with users [%s], that should be promoted.'),
+            implode(', ', $arr_should_promote) );
+      if ( count($arr_should_demote) )
+         $warnings[] = sprintf( T_('Found pool with users [%s], that should be demoted.'),
+            implode(', ', $arr_should_demote) );
+      if ( count($arr_should_stay) )
+         $warnings[] = sprintf( T_('Found pool with users [%s], that should stay in the same league.'),
+            implode(', ', $arr_should_stay) );
+
+      $exp_promotes = $tround->PromoteRanks;
+      $exp_demotes = $tround->PoolSize - $tround->DemoteStartRank + 1;
+      $exp_stays = $tround->DemoteStartRank - $tround->PromoteRanks - 1;
+      $arr_too_many_promotes = array();
+      $arr_too_many_demotes = array();
+      $arr_too_many_stays = array();
+      foreach ( $cnt_relegations as $pool_name => $arr_cnt_flags )
+      {
+         $cnt_promote = (int)@$arr_cnt_flags[TPOOL_FLAG_PROMOTE];
+         $cnt_demote = (int)@$arr_cnt_flags[TPOOL_FLAG_DEMOTE];
+         $cnt_stay = (int)@$arr_cnt_flags[0];
+         if ( $cnt_promote > $exp_promotes )
+            $arr_too_many_promotes[] = $pool_name;
+         if ( $cnt_demote > $exp_demotes )
+            $arr_too_many_demotes[] = $pool_name;
+         if ( $cnt_stay > $exp_stays )
+            $arr_too_many_stays[] = $pool_name;
+      }
+
+      if ( count($arr_too_many_promotes) )
+         $warnings[] = sprintf( T_('Pools [%s] have too many promoted users (max. %s expected).'),
+            implode(', ', $arr_too_many_promotes), $exp_promotes );
+      if ( count($arr_too_many_demotes) )
+         $warnings[] = sprintf( T_('Pools [%s] have too many demoted users (max. %s expected).'),
+            implode(', ', $arr_too_many_demotes), $exp_demotes );
+      if ( count($arr_too_many_stays) )
+         $warnings[] = sprintf( T_('Pools [%s] have too many users that stay in the same league (max. %s expected).'),
+            implode(', ', $arr_too_many_stays), $exp_stays );
+   }//check_pools_relegations
 
 } // end of 'TournamentTemplateLeague'
 
